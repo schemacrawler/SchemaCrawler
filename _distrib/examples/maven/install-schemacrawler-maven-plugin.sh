293fa9ee8bf2ce0f7a1928df56384f641e29943c
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2006-09-18 11:39:41 +0000

    Moving maven plugin test project into examples
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code@56 3243f2e7-4e1b-42d2-9742-d984016aa316

---
+++ _distrib/examples/maven/install-schemacrawler-maven-plugin.sh
@@ -0,0 +1,1 @@
+mvn install:install-file -Dfile=schemacrawler-3.8.jar -DpomFile=schemacrawler-maven-plugin.pom -DgroupId=schemacrawler -DartifactId=schemacrawler-maven-plugin -Dversion=3.8 -Dpackaging=maven-plugin
--- test/maven-plugin-test-project/install-schemacrawler-maven-plugin.cmd
+++
@@ -1,3 +0,0 @@
-mvn install:install-file -Dfile=../../_distrib/schemacrawler-3.8.jar -DgroupId=schemacrawler -DartifactId=schemacrawler-maven-plugin -Dversion=3.8 -Dpackaging=maven-plugin -DgeneratePom=true
-
-mvn help:describe -DgroupId=schemacrawler -DartifactId=schemacrawler-maven-plugin
