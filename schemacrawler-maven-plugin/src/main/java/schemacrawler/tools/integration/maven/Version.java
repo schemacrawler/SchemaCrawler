72a9098156915128b06f6c65974f91fec9cb8110
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2006-10-12 17:05:20 +0000

    - Bumping version to 4.0
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code@89 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler-maven-plugin/src/main/java/schemacrawler/tools/integration/maven/Version.java
+++ schemacrawler-maven-plugin/src/main/java/schemacrawler/tools/integration/maven/Version.java
@@ -35,7 +35,7 @@ public final class Version
 
   private static final String PRODUCTNAME = "schemacrawler-maven-plugin";
 
-  private static final String VERSION = "3.9";
+  private static final String VERSION = "4.0";
 
   private Version()
   {
