b0708e7463393e97d845f7dfcc480f6c246e7800
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2007-01-06 05:06:21 +0000

    Added schema grep functionality
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code@191 3243f2e7-4e1b-42d2-9742-d984016aa316

---
+++ schemacrawler/src/main/java/schemacrawler/main/Options.java
@@ -0,0 +1,195 @@
+/* 
+ *
+ * SchemaCrawler
+ * http://sourceforge.net/projects/schemacrawler
+ * Copyright (c) 2000-2007, Sualeh Fatehi.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+package schemacrawler.main;
+
+
+import java.util.Locale;
+import java.util.Properties;
+import java.util.logging.Level;
+
+import schemacrawler.crawl.SchemaCrawlerOptions;
+import schemacrawler.tools.OutputOptions;
+import schemacrawler.tools.ToolType;
+import schemacrawler.tools.datatext.DataTextFormatOptions;
+import schemacrawler.tools.operation.OperatorOptions;
+import schemacrawler.tools.schematext.SchemaTextOptions;
+
+/**
+ * Program options.
+ * 
+ * @author sfatehi
+ */
+public final class Options
+{
+
+  private static final String SCHEMACRAWLER_LOG_LEVEL = "schemacrawler.log_level";
+
+  private final Properties config;
+  private final SchemaCrawlerOptions schemaCrawlerOptions;
+  private final ToolType toolType;
+  private final SchemaTextOptions schemaTextOptions;
+  private final DataTextFormatOptions dataTextFormatOptions;
+  private final OperatorOptions operatorOptions;
+  private final String query;
+  private Level logLevel;
+
+  /**
+   * Parses options from a properties file.
+   * 
+   * @param config
+   *        Properties
+   * @param command
+   *        Properties
+   * @param pageOptions
+   *        Page options
+   */
+  Options(final Properties config,
+          final Command command,
+          final OutputOptions outputOptions)
+  {
+    if (config == null || command == null || outputOptions == null)
+    {
+      throw new IllegalArgumentException("All arguments need to be specified");
+    }
+
+    this.config = config;
+
+    schemaCrawlerOptions = new SchemaCrawlerOptions(config);
+
+    schemaTextOptions = new SchemaTextOptions(config, outputOptions, command
+      .getSchemaTextDetailType());
+    dataTextFormatOptions = new DataTextFormatOptions(config, outputOptions);
+    operatorOptions = new OperatorOptions(outputOptions,
+                                          command.getOperation(),
+                                          command.getQuery());
+
+    toolType = command.getToolType();
+    query = command.getQuery();
+
+    // Set additional options from the config
+    setLogLevel(config.getProperty(SCHEMACRAWLER_LOG_LEVEL, "OFF"));
+  }
+
+  void setLogLevel(final String logLevelString)
+  {
+    logLevel = Level.parse(logLevelString.toUpperCase(Locale.ENGLISH));
+  }
+
+  /**
+   * Gets the configuration properties.
+   * 
+   * @return Configuration properties
+   */
+  public Properties getConfig()
+  {
+    return config;
+  }
+
+  /**
+   * Gets the tool type.
+   * 
+   * @return Tool type
+   */
+  public ToolType getToolType()
+  {
+    return toolType;
+  }
+
+  /**
+   * Gets the operator options.
+   * 
+   * @return Operator options
+   */
+  public OperatorOptions getOperatorOptions()
+  {
+    return operatorOptions;
+  }
+
+  /**
+   * Gets the data text format options.
+   * 
+   * @return Data text format options
+   */
+  public DataTextFormatOptions getDataTextFormatOptions()
+  {
+    return dataTextFormatOptions;
+  }
+
+  /**
+   * Gets the schema crawler options.
+   * 
+   * @return S
+   */
+  public SchemaCrawlerOptions getSchemaCrawlerOptions()
+  {
+    return schemaCrawlerOptions;
+  }
+
+  /**
+   * Get schema text options.
+   * 
+   * @return Schema text options
+   */
+  public SchemaTextOptions getSchemaTextOptions()
+  {
+    return schemaTextOptions;
+  }
+
+  /**
+   * Query.
+   * 
+   * @return Query
+   */
+  public String getQuery()
+  {
+    return query;
+  }
+
+  /**
+   * Log level.
+   * 
+   * @return Log level
+   */
+  public Level getLogLevel()
+  {
+    return logLevel;
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see Object#toString()
+   */
+  public String toString()
+  {
+    final StringBuffer buffer = new StringBuffer();
+    buffer.append("Options[");
+    buffer.append("query=").append(query);
+    buffer.append("; log level=").append(logLevel.getName());
+    buffer.append("; ").append(schemaCrawlerOptions);
+    buffer.append("; ").append(schemaTextOptions);
+    buffer.append("; ").append(dataTextFormatOptions);
+    buffer.append("; ").append(operatorOptions);
+    buffer.append("]");
+    return buffer.toString();
+  }
+
+}
---
+++ schemacrawler/src/main/java/schemacrawler/main/OptionsParser.java
@@ -0,0 +1,246 @@
+/* 
+ *
+ * SchemaCrawler
+ * http://sourceforge.net/projects/schemacrawler
+ * Copyright (c) 2000-2007, Sualeh Fatehi.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+package schemacrawler.main;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+import schemacrawler.crawl.SchemaCrawlerException;
+import schemacrawler.tools.OutputFormat;
+import schemacrawler.tools.OutputOptions;
+import schemacrawler.tools.operation.Operation;
+import schemacrawler.tools.schematext.SchemaTextDetailType;
+import sf.util.CommandLineParser;
+import sf.util.Utilities;
+
+/**
+ * Parses the command line.
+ * 
+ * @author sfatehi
+ */
+public final class OptionsParser
+{
+
+  private static final String OPTION_CONFIGFILE = "configfile";
+  private static final String OPTION_CONFIGOVERRIDEFILE = "configoverridefile";
+  private static final String OPTION_NOINFO = "noinfo";
+  private static final String OPTION_NOFOOTER = "nofooter";
+  private static final String OPTION_NOHEADER = "noheader";
+  private static final String OPTION_COMMAND = "command";
+  private static final String OPTION_OUTPUT_FORMAT = "outputformat";
+  private static final String OPTION_OUTPUT_FILE = "outputfile";
+  private static final String OPTION_OUTPUT_APPEND = "append";
+
+  public static boolean getBooleanOption(final CommandLineParser.BaseOption option)
+  {
+    if (option == null || option.getValue() == null)
+    {
+      return false;
+    }
+    return Boolean.valueOf(option.getValue().toString()).booleanValue();
+  }
+
+  public static String getStringOption(final CommandLineParser.BaseOption option,
+                                       final String defaultValue)
+  {
+    if (option == null || option.getValue() == null)
+    {
+      return defaultValue;
+    }
+    return option.getValue().toString();
+  }
+
+  public static Properties loadConfig(final String configfilename,
+                                      final String configoverridefilename)
+  {
+    Properties config = new Properties();
+    config = Utilities.loadProperties(config, configfilename);
+    config = Utilities.loadProperties(config, configoverridefilename);
+    return config;
+  }
+
+  /**
+   * Parses the command line.
+   * 
+   * @param args
+   *        Command line arguments
+   * @return Command line options
+   * @throws SchemaCrawlerException
+   */
+  static Options[] parseCommandLine(final String[] args)
+    throws SchemaCrawlerException
+  {
+
+    final CommandLineParser parser = new CommandLineParser();
+    parser
+      .addOption(new CommandLineParser.StringOption('g', OPTION_CONFIGFILE));
+    parser
+      .addOption(new CommandLineParser.StringOption('p',
+                                                    OPTION_CONFIGOVERRIDEFILE));
+    parser.addOption(new CommandLineParser.StringOption(OPTION_COMMAND));
+    parser.addOption(new CommandLineParser.StringOption(OPTION_OUTPUT_FORMAT));
+    parser.addOption(new CommandLineParser.StringOption(OPTION_OUTPUT_FILE));
+    parser.addOption(new CommandLineParser.StringOption(OPTION_OUTPUT_APPEND));
+    parser.addOption(new CommandLineParser.StringOption(OPTION_NOHEADER));
+    parser.addOption(new CommandLineParser.StringOption(OPTION_NOFOOTER));
+    parser.addOption(new CommandLineParser.StringOption(OPTION_NOINFO));
+    parser.parse(args);
+
+    final String cfgFile = getStringOption(parser.getOption(OPTION_CONFIGFILE),
+                                           "schemacrawler.config.properties");
+    final String cfgOverrideFile = getStringOption(parser
+                                                     .getOption(OPTION_CONFIGOVERRIDEFILE),
+                                                   "schemacrawler.config.override.properties");
+    final Properties config = loadConfig(cfgFile, cfgOverrideFile);
+
+    final String outputFormatValue = getStringOption(parser
+      .getOption(OPTION_OUTPUT_FORMAT), OutputFormat.TEXT.toString());
+
+    final String outputFile = getStringOption(parser
+      .getOption(OPTION_OUTPUT_FILE), "");
+
+    final boolean appendOutput = getBooleanOption(parser
+      .getOption(OPTION_OUTPUT_APPEND));
+
+    final boolean noHeader = getBooleanOption(parser.getOption(OPTION_NOHEADER));
+    final boolean noFooter = getBooleanOption(parser.getOption(OPTION_NOFOOTER));
+    final boolean noInfo = getBooleanOption(parser.getOption(OPTION_NOINFO));
+
+    final OutputOptions masterOutputOptions = new OutputOptions(outputFormatValue,
+                                                                outputFile);
+    masterOutputOptions.setAppendOutput(appendOutput);
+    masterOutputOptions.setNoHeader(noHeader);
+    masterOutputOptions.setNoFooter(noFooter);
+    masterOutputOptions.setNoInfo(noInfo);
+
+    final CommandLineParser.BaseOption commandOption = parser
+      .getOption(OPTION_COMMAND);
+    String commandOptionValue = getStringOption(commandOption, "");
+    if (Utilities.isBlank(commandOptionValue))
+    {
+      throw new SchemaCrawlerException("No SchemaCrawler command specified");
+    }
+    final String[] commandStrings = commandOptionValue.split(",");
+    final Options[] optionCommands = createOptionsPerCommand(commandStrings,
+                                                             config,
+                                                             masterOutputOptions);
+
+    return optionCommands;
+
+  }
+
+  private static Options[] createOptionsPerCommand(final String[] commandStrings,
+                                                   final Properties config,
+                                                   final OutputOptions masterOutputOptions)
+  {
+    final List optionCommandsList = new ArrayList();
+    for (int i = 0; i < commandStrings.length; i++)
+    {
+      final Command command = parseCommand(config, commandStrings[i]);
+      //
+      final OutputOptions outputOptions = masterOutputOptions.duplicate();
+      if (i == 0)
+      {
+        // First command - no footer
+        outputOptions.setNoFooter(true);
+      }
+      else if (i == commandStrings.length - 1)
+      {
+        // Last command - no header, or info
+        outputOptions.setNoHeader(true);
+        outputOptions.setNoInfo(true);
+
+        outputOptions.setAppendOutput(true);
+      }
+      else
+      {
+        // Middle command - no header, footer, or info
+        outputOptions.setNoHeader(true);
+        outputOptions.setNoInfo(true);
+        outputOptions.setNoFooter(true);
+
+        outputOptions.setAppendOutput(true);
+      }
+      //
+      final Options options = new Options(config, command, outputOptions);
+      //
+      optionCommandsList.add(options);
+    }
+    final Options[] optionCommands = (Options[]) optionCommandsList
+      .toArray(new Options[optionCommandsList.size()]);
+
+    return optionCommands;
+  }
+
+  private static boolean isQueryOver(final String query)
+  {
+    boolean isQueryOver = false;
+    final Set keys = Utilities.extractTemplateVariables(query);
+    final String[] queryOverKeys = {
+        "table", "table_type"
+    };
+    for (int i = 0; i < queryOverKeys.length; i++)
+    {
+      if (keys.contains(queryOverKeys[i]))
+      {
+        isQueryOver = true;
+        break;
+      }
+    }
+    return isQueryOver;
+  }
+
+  private static Command parseCommand(final Properties config,
+                                      final String commandString)
+  {
+    final SchemaTextDetailType schemaTextDetailType = SchemaTextDetailType
+      .valueOf(commandString);
+    Operation operation = Operation.valueOf(commandString);
+    String query = "";
+    if (schemaTextDetailType == null && operation == null)
+    {
+      // Assume that the command is a query
+      query = config.getProperty(commandString);
+      if (query == null)
+      {
+        throw new IllegalArgumentException("Invalid command - " + commandString);
+      }
+      if (isQueryOver(query))
+      {
+        operation = Operation.queryOverOperation();
+      }
+    }
+
+    final Command command = Command.createCommand(schemaTextDetailType,
+                                                  operation,
+                                                  query);
+    return command;
+  }
+
+  private OptionsParser()
+  {
+
+  }
+
+}
