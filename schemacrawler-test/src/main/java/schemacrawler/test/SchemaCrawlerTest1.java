f510e6e54b9f0a0788be1f66990a6ef54b396483
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2007-02-16 21:12:26 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code@198 3243f2e7-4e1b-42d2-9742-d984016aa316

---
+++ schemacrawler-test/src/main/java/schemacrawler/test/SchemaCrawlerTest1.java
@@ -0,0 +1,61 @@
+package schemacrawler.test;
+
+
+import java.util.Arrays;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import schemacrawler.crawl.SchemaCrawler;
+import schemacrawler.crawl.SchemaCrawlerOptions;
+import schemacrawler.crawl.SchemaInfoLevel;
+import schemacrawler.schema.Schema;
+import schemacrawler.schema.Table;
+import dbconnector.datasource.PropertiesDataSource;
+import dbconnector.datasource.PropertiesDataSourceException;
+
+public class SchemaCrawlerTest1
+{
+
+  public static void main(String[] args)
+    throws Exception
+  {
+    DataSource dataSource = makeDataSource();
+
+    Properties properties = new Properties();
+    properties.setProperty("schemacrawler.table_types", "TABLE");
+    properties.setProperty("schemacrawler.show_stored_procedures", "false");
+    properties.setProperty("schemacrawler.table.pattern.include", ".*");
+    properties.setProperty("schemacrawler.table.pattern.exclude", "");
+    
+    SchemaCrawlerOptions options = new SchemaCrawlerOptions(properties);
+
+    Schema schema = SchemaCrawler.getSchema(dataSource,
+                                            properties,
+                                            SchemaInfoLevel.BASIC,
+                                            options);
+    Table[] tableArray = schema.getTables();
+
+    System.out.println(Arrays.asList(tableArray));
+    System.out.println(Arrays.asList(schema.getProcedures()));
+
+  }
+
+  private static DataSource makeDataSource()
+    throws PropertiesDataSourceException
+  {
+    final String datasourceName = "schemacrawler";
+
+    final Properties connectionProperties = new Properties();
+    connectionProperties.setProperty(datasourceName + ".driver",
+                                     "org.hsqldb.jdbcDriver");
+    connectionProperties
+      .setProperty(datasourceName + ".url",
+                   "jdbc:hsqldb:hsql://localhost:9001/schemacrawler");
+    connectionProperties.setProperty(datasourceName + ".user", "sa");
+    connectionProperties.setProperty(datasourceName + ".password", "");
+
+    return new PropertiesDataSource(connectionProperties, datasourceName);
+  }
+
+}
