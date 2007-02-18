57bbee2359b032694a247fd564968414f00df9df
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2007-02-18 05:06:32 +0000

    1660040  NPE when running SchemaCrawler with options
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code@203 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler/src/main/java/schemacrawler/crawl/InclusionRule.java
+++ schemacrawler/src/main/java/schemacrawler/crawl/InclusionRule.java
@@ -26,6 +26,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
+import sf.util.Utilities;
+
 /**
  * Specifies inclusion and exclusion patterns that can be applied to the
  * names of database objects.
@@ -103,7 +105,7 @@ public final class InclusionRule
   public boolean include(final String name)
   {
     boolean include = false;
-    if (name == null && name.length() > 0)
+    if (!Utilities.isBlank(name))
     {
       if (!patternInclude.matcher(name).matches())
       {
