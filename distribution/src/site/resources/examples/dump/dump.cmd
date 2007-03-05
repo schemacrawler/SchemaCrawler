368f48c213dab9401292d8e4b79eb81d63b6aa27
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2007-03-05 22:03:42 +0000

    Changing version number
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code@226 3243f2e7-4e1b-42d2-9742-d984016aa316

--- distribution/src/site/resources/examples/dump/dump.cmd
+++ distribution/src/site/resources/examples/dump/dump.cmd
@@ -1,4 +1,4 @@
 @echo off
 del /q /f database-dump.html
-java -jar schemacrawler-4.1.jar -c hsqldb -command=count,dump -outputformat=html -outputfile=database-dump.html
+java -jar schemacrawler-4.2.jar -c hsqldb -command=count,dump -outputformat=html -outputfile=database-dump.html
 echo Database dump is in database-dump.html
