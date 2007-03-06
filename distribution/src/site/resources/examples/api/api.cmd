c2fea76304ca582358e32183d4b572de14f500d0
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2007-03-06 18:13:45 +0000

    Fixing examples
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code@227 3243f2e7-4e1b-42d2-9742-d984016aa316

--- distribution/src/site/resources/examples/api/api.cmd
+++ distribution/src/site/resources/examples/api/api.cmd
@@ -1,4 +1,4 @@
 @echo off
 del /f /q *.class
 javac -classpath schemacrawler-4.2.jar ApiExample.java
-java -classpath schemacrawler-4.2.jar;. ApiExample
+java -classpath schemacrawler-4.2.jar;../../dbserver/hsqldb.jar;. ApiExample
--- distribution/src/site/resources/examples/api/api.sh
+++ distribution/src/site/resources/examples/api/api.sh
@@ -1,3 +1,3 @@
 rm -f *.class
 javac -classpath schemacrawler-4.2.jar ApiExample.java
-java -classpath .:schemacrawler-4.2.jar ApiExample
+java -classpath schemacrawler-4.2.jar:../../dbserver/hsqldb.jar:. ApiExample
