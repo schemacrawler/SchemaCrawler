613e8a1212906390fbe5df2356f773db900e2416
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2007-10-03 13:27:13 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code@399 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler/src/main/java/schemacrawler/tools/Query.java
+++ schemacrawler/src/main/java/schemacrawler/tools/Query.java
@@ -1,3 +1,22 @@
+/* 
+ *
+ * SchemaCrawler
+ * http://sourceforge.net/projects/schemacrawler
+ * Copyright (c) 2000-2007, Sualeh Fatehi.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
 package schemacrawler.tools;
 
 
@@ -6,6 +25,11 @@ import java.util.Set;
 
 import sf.util.Utilities;
 
+/**
+ * A SQL query. May be parameterized with ant-like variable references.
+ * 
+ * @author sfatehi
+ */
 public class Query
   implements Serializable
 {
@@ -15,6 +39,15 @@ public class Query
   private final String name;
   private final String query;
 
+  /**
+   * Definition of a query, including a name, and parameterized or
+   * regular SQL.
+   * 
+   * @param name
+   *        Query name.
+   * @param query
+   *        Query SQL.
+   */
   public Query(final String name, final String query)
   {
     if (name == null || name.length() == 0)
@@ -45,7 +78,7 @@ public class Query
     return query;
   }
 
-  public boolean isQueryOver()
+  boolean isQueryOver()
   {
     boolean isQueryOver = false;
     final Set<String> keys = Utilities.extractTemplateVariables(query);
