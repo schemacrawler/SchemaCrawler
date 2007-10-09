93dfdff48e600c744c527044919563a82768ce18
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2007-10-09 19:11:20 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code@431 3243f2e7-4e1b-42d2-9742-d984016aa316

--- integrations/src/test/java/schemacrawler/integration/test/SpringIntegrationTest.java
+++ integrations/src/test/java/schemacrawler/integration/test/SpringIntegrationTest.java
@@ -6,10 +6,10 @@ import static org.junit.Assert.fail;
 
 import java.io.File;
 
-import javax.sql.DataSource;
-
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -22,10 +22,28 @@ import schemacrawler.tools.Executable;
 import schemacrawler.tools.datatext.DataTextFormatOptions;
 import schemacrawler.tools.operation.OperationOptions;
 import schemacrawler.tools.schematext.SchemaTextOptions;
+import dbconnector.datasource.PropertiesDataSourceException;
 import dbconnector.test.TestUtility;
 
 public class SpringIntegrationTest
 {
+
+  private static TestUtility testUtility = new TestUtility();
+
+  @AfterClass
+  public static void afterAllTests()
+    throws PropertiesDataSourceException, ClassNotFoundException
+  {
+    testUtility.shutdownDatabase();
+  }
+
+  @BeforeClass
+  public static void beforeAllTests()
+    throws PropertiesDataSourceException, ClassNotFoundException
+  {
+    testUtility.setApplicationLogLevel();
+    testUtility.createMemoryDatabase();
+  }
 
   private ApplicationContext appContext;
 
@@ -33,9 +51,6 @@ public class SpringIntegrationTest
   public void setupDatabase()
   {
     appContext = new ClassPathXmlApplicationContext("context.xml");
-
-    final DataSource dataSource = (DataSource) appContext.getBean("dataSource");
-    TestUtility.setupSchema(dataSource);
   }
 
   @Test
@@ -47,20 +62,10 @@ public class SpringIntegrationTest
 
     final Executable<OperationOptions> executable = (Executable<OperationOptions>) appContext
       .getBean("executableForCount");
-    final DataSource dataSource = (DataSource) appContext.getBean("dataSource");
-
     executable.getToolOptions().getOutputOptions()
       .setOutputFileName(outputFilename);
 
-    executable.execute(dataSource);
-
-    final File outputFile = new File(outputFilename);
-    Assert.assertTrue(outputFile.exists());
-    Assert.assertTrue(outputFile.length() > 0);
-    if (!outputFile.delete())
-    {
-      fail("Cannot delete output file");
-    }
+    executeAndCheckForOutputFile(executable, outputFilename);
   }
 
   @Test
@@ -72,20 +77,10 @@ public class SpringIntegrationTest
 
     final Executable<SchemaTextOptions> executable = (Executable<SchemaTextOptions>) appContext
       .getBean("executableForFreeMarker");
-    final DataSource dataSource = (DataSource) appContext.getBean("dataSource");
-
     executable.getToolOptions().getOutputOptions()
       .setOutputFileName(outputFilename);
 
-    executable.execute(dataSource);
-
-    final File outputFile = new File(outputFilename);
-    Assert.assertTrue(outputFile.exists());
-    Assert.assertTrue(outputFile.length() > 0);
-    if (!outputFile.delete())
-    {
-      fail("Cannot delete output file");
-    }
+    executeAndCheckForOutputFile(executable, outputFilename);
   }
 
   @Test
@@ -97,20 +92,10 @@ public class SpringIntegrationTest
 
     final Executable<DataTextFormatOptions> executable = (Executable<DataTextFormatOptions>) appContext
       .getBean("executableForQuery");
-    final DataSource dataSource = (DataSource) appContext.getBean("dataSource");
-
     executable.getToolOptions().getOutputOptions()
       .setOutputFileName(outputFilename);
 
-    executable.execute(dataSource);
-
-    final File outputFile = new File(outputFilename);
-    Assert.assertTrue(outputFile.exists());
-    Assert.assertTrue(outputFile.length() > 0);
-    if (!outputFile.delete())
-    {
-      fail("Cannot delete output file");
-    }
+    executeAndCheckForOutputFile(executable, outputFilename);
   }
 
   @Test
@@ -122,20 +107,10 @@ public class SpringIntegrationTest
 
     final Executable<SchemaTextOptions> executable = (Executable<SchemaTextOptions>) appContext
       .getBean("executableForSchema");
-    final DataSource dataSource = (DataSource) appContext.getBean("dataSource");
-
     executable.getToolOptions().getOutputOptions()
       .setOutputFileName(outputFilename);
 
-    executable.execute(dataSource);
-
-    final File outputFile = new File(outputFilename);
-    Assert.assertTrue(outputFile.exists());
-    Assert.assertTrue(outputFile.length() > 0);
-    if (!outputFile.delete())
-    {
-      fail("Cannot delete output file");
-    }
+    executeAndCheckForOutputFile(executable, outputFilename);
   }
 
   @Test
@@ -147,12 +122,28 @@ public class SpringIntegrationTest
 
     final Executable<SchemaTextOptions> executable = (Executable<SchemaTextOptions>) appContext
       .getBean("executableForVelocity");
-    final DataSource dataSource = (DataSource) appContext.getBean("dataSource");
-
     executable.getToolOptions().getOutputOptions()
       .setOutputFileName(outputFilename);
 
-    executable.execute(dataSource);
+    executeAndCheckForOutputFile(executable, outputFilename);
+  }
+
+  @Test
+  public void testSchema()
+  {
+    final SchemaCrawlerOptions schemaCrawlerOptions = (SchemaCrawlerOptions) appContext
+      .getBean("schemaCrawlerOptions");
+    final Schema schema = SchemaCrawler.getSchema(testUtility.getDataSource(),
+                                                  SchemaInfoLevel.maximum,
+                                                  schemaCrawlerOptions);
+    assertEquals(6, schema.getTables().length);
+  }
+
+  private void executeAndCheckForOutputFile(final Executable<?> executable,
+                                            final String outputFilename)
+    throws Exception
+  {
+    executable.execute(testUtility.getDataSource());
 
     final File outputFile = new File(outputFilename);
     Assert.assertTrue(outputFile.exists());
@@ -161,18 +152,6 @@ public class SpringIntegrationTest
     {
       fail("Cannot delete output file");
     }
-  }
-
-  @Test
-  public void testSchema()
-  {
-    final SchemaCrawlerOptions schemaCrawlerOptions = (SchemaCrawlerOptions) appContext
-      .getBean("schemaCrawlerOptions");
-    final DataSource dataSource = (DataSource) appContext.getBean("dataSource");
-    final Schema schema = SchemaCrawler.getSchema(dataSource,
-                                                  SchemaInfoLevel.maximum,
-                                                  schemaCrawlerOptions);
-    assertEquals(6, schema.getTables().length);
   }
 
 }
