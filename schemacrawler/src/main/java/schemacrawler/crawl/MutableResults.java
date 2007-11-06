ba085caaf5b8b885ceba59208ff64000451fcb19
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2007-11-06 18:27:22 +0000

    Preparing for resultset metadata
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code@491 3243f2e7-4e1b-42d2-9742-d984016aa316

---
+++ schemacrawler/src/main/java/schemacrawler/crawl/MutableResults.java
@@ -0,0 +1,114 @@
+/* 
+ *
+ * SchemaCrawler
+ * http://sourceforge.net/projects/schemacrawler
+ * Copyright (c) 2000-2007, Sualeh Fatehi.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+package schemacrawler.crawl;
+
+
+import schemacrawler.crawl.NamedObjectList.NamedObjectSort;
+import schemacrawler.schema.Results;
+import schemacrawler.schema.ResultsColumn;
+
+/**
+ * {@inheritDoc}
+ * 
+ * @author Sualeh Fatehi
+ */
+class MutableResults
+  extends AbstractNamedObject
+  implements Results
+{
+
+  private static final long serialVersionUID = 5204766782914559188L;
+
+  private final NamedObjectList<MutableResultsColumn> columns = new NamedObjectList<MutableResultsColumn>(NamedObjectSort.natural);
+
+  MutableResults(final String name)
+  {
+    super(name);
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see schemacrawler.schema.Table#getColumn(java.lang.String)
+   */
+  public ResultsColumn getColumn(final String name)
+  {
+    return columns.lookup(name);
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see Table#getColumns()
+   */
+  public ResultsColumn[] getColumns()
+  {
+    return columns.getAll().toArray(new ResultsColumn[columns.size()]);
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see schemacrawler.schema.Table#getColumnsListAsString()
+   */
+  public String getColumnsListAsString()
+  {
+    String columnsList = "";
+    final ResultsColumn[] columnsArray = getColumns();
+    if (columnsArray != null && columnsArray.length > 0)
+    {
+      final StringBuffer buffer = new StringBuffer();
+      for (int i = 0; i < columnsArray.length; i++)
+      {
+        if (i > 0)
+        {
+          buffer.append(", ");
+        }
+        final ResultsColumn column = columnsArray[i];
+        buffer.append(column.getFullName());
+      }
+      columnsList = buffer.toString();
+    }
+    return columnsList;
+  }
+
+  /**
+   * Adds a column.
+   * 
+   * @param column
+   *        Column
+   */
+  void addColumn(final MutableResultsColumn column)
+  {
+    columns.add(column);
+  }
+
+  NamedObjectList<MutableResultsColumn> getColumnsList()
+  {
+    return columns;
+  }
+
+  void setColumnComparator(final NamedObjectSort comparator)
+  {
+    columns.setSortOrder(comparator);
+  }
+
+}
---
+++ schemacrawler/src/main/java/schemacrawler/schema/ResultsColumn.java
@@ -0,0 +1,96 @@
+/* 
+ *
+ * SchemaCrawler
+ * http://sourceforge.net/projects/schemacrawler
+ * Copyright (c) 2000-2007, Sualeh Fatehi.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+package schemacrawler.schema;
+
+
+/**
+ * A table or procedure column.
+ * 
+ * @author Sualeh Fatehi
+ */
+public interface ResultsColumn
+  extends BaseColumn
+{
+
+  /**
+   * True if this column is auto-incremented.
+   * 
+   * @return If the column is auto-incremented
+   */
+  boolean isAutoIncrement();
+
+  /**
+   * True if this column is case-sensitive.
+   * 
+   * @return If the column is case-sensitive
+   */
+  boolean isCaseSensitive();
+
+  /**
+   * True if this column is a cash value.
+   * 
+   * @return If the column is a cash value
+   */
+  boolean isCurrency();
+
+  /**
+   * Indicates whether a write on the designated column will definitely
+   * succeed.
+   * 
+   * @return Whether a write on the designated column will definitely
+   *         succeed
+   */
+  boolean isDefinitelyWritable();
+
+  /**
+   * Indicates whether the designated column is definitely not writable.
+   * 
+   * @return Whether the designated column is definitely not writable.
+   */
+  boolean isReadOnly();
+
+  /**
+   * Indicates whether the designated column can be used in a where
+   * clause.
+   * 
+   * @return Whether the designated column can be used in a where
+   *         clause.
+   */
+  boolean isSearchable();
+
+  /**
+   * Indicates whether values in the designated column are signed
+   * numbers.
+   * 
+   * @return Whether values in the designated column are signed numbers.
+   */
+  boolean isSigned();
+
+  /**
+   * Indicates whether it is possible for a write on the designated
+   * column to succeed.
+   * 
+   * @return Whether it is possible for a write on the designated column
+   *         to succeed.
+   */
+  boolean isWritable();
+
+}
