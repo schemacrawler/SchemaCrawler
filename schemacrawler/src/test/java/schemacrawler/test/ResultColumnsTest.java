79e4e964c8bea0d5bcab47498767f10275b80e87
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2007-11-11 22:12:55 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code@505 3243f2e7-4e1b-42d2-9742-d984016aa316

---
+++ schemacrawler/src/test/java/schemacrawler/test/ResultColumnsTest.java
@@ -0,0 +1,115 @@
+/* 
+ *
+ * SchemaCrawler
+ * http://sourceforge.net/projects/schemacrawler
+ * Copyright (c) 2000-2007, Sualeh Fatehi.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+package schemacrawler.test;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import schemacrawler.crawl.SchemaCrawler;
+import schemacrawler.schema.ResultColumns;
+import schemacrawler.schema.ResultsColumn;
+import dbconnector.datasource.PropertiesDataSourceException;
+import dbconnector.test.TestUtility;
+
+public class ResultColumnsTest
+{
+
+  private static final Logger LOGGER = Logger.getLogger(ResultColumnsTest.class
+    .getName());
+
+  private static TestUtility testUtility = new TestUtility();
+
+  @AfterClass
+  public static void afterAllTests()
+    throws PropertiesDataSourceException, ClassNotFoundException
+  {
+    testUtility.shutdownDatabase();
+  }
+
+  @BeforeClass
+  public static void beforeAllTests()
+    throws PropertiesDataSourceException, ClassNotFoundException
+  {
+    testUtility.setApplicationLogLevel();
+    testUtility.createMemoryDatabase();
+  }
+
+  @Test
+  public void columns()
+    throws SQLException
+  {
+
+    final String[] columnNames = {
+        "", "FIRSTNAME", "LASTNAME",
+    };
+
+    final String[] columnDataTypes = {
+        "DOUBLE", "VARCHAR", "VARCHAR",
+    };
+
+    final String sql = "SELECT " + "  CUSTOMER.FIRSTNAME, "
+                       + "  CUSTOMER.LASTNAME, " + "  SUM(INVOICE.TOTAL) "
+                       + "FROM " + "  CUSTOMER " + "  INNER JOIN INVOICE "
+                       + "  ON INVOICE.CUSTOMERID = CUSTOMER.ID " + "GROUP BY "
+                       + "  CUSTOMER.FIRSTNAME, " + "  CUSTOMER.LASTNAME "
+                       + "ORDER BY " + "  SUM(INVOICE.TOTAL) DESC";
+    Connection connection = testUtility.getDataSource().getConnection();
+    Statement statement = connection.createStatement();
+    ResultSet resultSet = statement.executeQuery(sql);
+
+    final ResultColumns resultColumns = SchemaCrawler
+      .getResultColumns(resultSet);
+    connection.close();
+
+    assertNotNull("Could not obtain result columns", resultColumns);
+    ResultsColumn[] columns = resultColumns.getColumns();
+    assertEquals("Column count does not match", 3, columns.length);
+    for (int columnIdx = 0; columnIdx < columns.length; columnIdx++)
+    {
+      ResultsColumn column = columns[columnIdx];
+      LOGGER.log(Level.FINE, column.toString());
+      assertEquals("Column full name does not match",
+                   columnNames[columnIdx],
+                   column.getFullName());
+      assertEquals("Column name does not match", columnNames[columnIdx], column
+        .getName());
+      assertEquals("Column type does not match",
+                   columnDataTypes[columnIdx],
+                   column.getType().getDatabaseSpecificTypeName());
+      assertEquals("Column JDBC type does not match",
+                   columnDataTypes[columnIdx],
+                   column.getType().getTypeName());
+    }
+
+  }
+}
