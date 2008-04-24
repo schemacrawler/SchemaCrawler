f28ee9cc77235bc8d76ce3b1e044580da311c0c4
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2008-04-24 12:14:03 +0000

    Slimmer API for XML serialization and deserialization.
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code@655 3243f2e7-4e1b-42d2-9742-d984016aa316

--- integrations/src/main/java/schemacrawler/tools/integration/xml/XmlSchemaCrawler.java
+++ integrations/src/main/java/schemacrawler/tools/integration/xml/XmlSchemaCrawler.java
@@ -5,87 +5,24 @@ import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
 
-import schemacrawler.schema.Procedure;
+import schemacrawler.crawl.CachedSchemaCrawler;
 import schemacrawler.schema.Schema;
-import schemacrawler.schema.Table;
-import schemacrawler.schemacrawler.CrawlHandler;
-import schemacrawler.schemacrawler.SchemaCrawler;
 import schemacrawler.schemacrawler.SchemaCrawlerException;
-import schemacrawler.schemacrawler.SchemaCrawlerOptions;
 
 import com.thoughtworks.xstream.XStream;
 
 public final class XmlSchemaCrawler
-  implements SchemaCrawler
+  extends CachedSchemaCrawler
 {
 
-  private Schema schema;
-  private Reader reader;
+  public XmlSchemaCrawler(Schema schema)
+  {
+    super(schema);
+  }
 
   public XmlSchemaCrawler(final Reader reader)
-    throws SchemaCrawlerException
   {
-    load(reader);
-  }
-
-  public XmlSchemaCrawler()
-  {
-  }
-
-  public XmlSchemaCrawler(final Schema schema)
-  {
-    this.schema = schema;
-  }
-
-  public void crawl(final SchemaCrawlerOptions options,
-                    final CrawlHandler handler)
-    throws SchemaCrawlerException
-  {
-    if (handler == null)
-    {
-      throw new SchemaCrawlerException("No crawl handler specified");
-    }
-    if (schema == null)
-    {
-      throw new SchemaCrawlerException("No schema loaded");
-    }
-
-    handler.begin();
-    handler.handle(schema.getJdbcDriverInfo());
-    handler.handle(schema.getDatabaseInfo());
-    for (final Table table: schema.getTables())
-    {
-      handler.handle(table);
-    }
-    for (final Procedure procedure: schema.getProcedures())
-    {
-      handler.handle(procedure);
-    }
-    handler.end();
-  }
-
-  public Schema getSchema()
-  {
-    return schema;
-  }
-
-  public Schema load(final SchemaCrawlerOptions options)
-    throws SchemaCrawlerException
-  {
-    if (reader == null)
-    {
-      throw new SchemaCrawlerException("Reader not provided");
-    }
-    final XStream xStream = new XStream();
-    schema = (Schema) xStream.fromXML(reader);
-    return schema;
-  }
-
-  public Schema load(final Reader reader)
-    throws SchemaCrawlerException
-  {
-    setReader(reader);
-    return load((SchemaCrawlerOptions) null);
+    super((Schema) new XStream().fromXML(reader));
   }
 
   public void save(final Writer writer)
@@ -97,7 +34,7 @@ public final class XmlSchemaCrawler
     }
     if (schema == null)
     {
-      throw new SchemaCrawlerException("No schema loaded");
+      throw new SchemaCrawlerException("No cached schema");
     }
     final XStream xStream = new XStream();
     xStream.toXML(schema, writer);
@@ -105,15 +42,10 @@ public final class XmlSchemaCrawler
     {
       writer.flush();
     }
-    catch (IOException e)
+    catch (final IOException e)
     {
       throw new SchemaCrawlerException("Could not flush writer", e);
     }
-  }
-
-  public void setReader(final Reader reader)
-  {
-    this.reader = reader;
   }
 
 }
