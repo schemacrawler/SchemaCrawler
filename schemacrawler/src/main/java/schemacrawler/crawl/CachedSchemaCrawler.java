ae2adf06a5ab1ef96200a816c53d6f55c048621d
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2008-04-24 12:14:10 +0000

    Slimmer API for XML serialization and deserialization.
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code@656 3243f2e7-4e1b-42d2-9742-d984016aa316

---
+++ schemacrawler/src/main/java/schemacrawler/crawl/CachedSchemaCrawler.java
@@ -0,0 +1,60 @@
+package schemacrawler.crawl;
+
+
+import schemacrawler.schema.Procedure;
+import schemacrawler.schema.Schema;
+import schemacrawler.schema.Table;
+import schemacrawler.schemacrawler.CrawlHandler;
+import schemacrawler.schemacrawler.SchemaCrawler;
+import schemacrawler.schemacrawler.SchemaCrawlerException;
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+
+public class CachedSchemaCrawler
+  implements SchemaCrawler
+{
+
+  protected final Schema schema;
+
+  public CachedSchemaCrawler(final Schema schema)
+  {
+    this.schema = schema;
+  }
+
+  public void crawl(final SchemaCrawlerOptions options,
+                    final CrawlHandler handler)
+    throws SchemaCrawlerException
+  {
+    if (handler == null)
+    {
+      throw new SchemaCrawlerException("No crawl handler specified");
+    }
+    if (schema == null)
+    {
+      throw new SchemaCrawlerException("No schema loaded");
+    }
+
+    handler.begin();
+    handler.handle(schema.getJdbcDriverInfo());
+    handler.handle(schema.getDatabaseInfo());
+    for (final Table table: schema.getTables())
+    {
+      handler.handle(table);
+    }
+    for (final Procedure procedure: schema.getProcedures())
+    {
+      handler.handle(procedure);
+    }
+    handler.end();
+  }
+
+  public Schema load(final SchemaCrawlerOptions options)
+    throws SchemaCrawlerException
+  {
+    if (schema == null)
+    {
+      throw new SchemaCrawlerException("No cached schema");
+    }
+    return schema;
+  }
+
+}
