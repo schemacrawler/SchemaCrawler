201de492024fef6c72320e0e580f33583590a73d
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2008-10-29 13:07:28 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code@791 3243f2e7-4e1b-42d2-9742-d984016aa316

--- distribution/src/site/resources/examples/javascript/tables.js
+++ distribution/src/site/resources/examples/javascript/tables.js
@@ -1,17 +1,17 @@
-print(catalog.getDatabaseInfo() + "\n");
-
-var schemas = catalog.getSchemas();
-for (i = 0; i < schemas.length; i++)
-{
-  print(schemas[i].getName() + "\n");
-  var tables = schemas[i].getTables();
-  for (j = 0; j < tables.length; j++)
-  {
-    print("o--> " + tables[j].getName() + "\n");
-    var columns = tables[j].getColumns();
-    for (k = 0; k < columns.length; k++)
-    {
-      print("     o--> " + columns[k].getName() + "\n");
+var printCatalog = function() {
+  print(catalog.getDatabaseInfo() + "\n");  
+  var schemas = catalog.getSchemas();
+  for (var i = 0; i < schemas.length; i++) {
+    print(schemas[i].getName() + "\n");
+    var tables = schemas[i].getTables();
+    for (var j = 0; j < tables.length; j++) {
+      print("o--> " + tables[j].getName() + "\n");
+      var columns = tables[j].getColumns();
+      for (var k = 0; k < columns.length; k++) {
+        print("     o--> " + columns[k].getName() + "\n");
+      }
     }
   }
-}
+};
+
+printCatalog();
