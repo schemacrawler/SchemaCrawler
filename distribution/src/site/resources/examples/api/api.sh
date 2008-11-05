2c9851f6bc3add514ba7151e571f35386225ae84
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2008-11-05 13:49:27 +0000

    Changing version to 6.0.1, for minor bug-fix
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code@792 3243f2e7-4e1b-42d2-9742-d984016aa316

--- distribution/src/site/resources/examples/api/api.sh
+++ distribution/src/site/resources/examples/api/api.sh
@@ -1,3 +1,3 @@
 rm -f *.class
-javac -classpath ../schemacrawler-6.1.jar ApiExample.java
-java -classpath ../schemacrawler-6.1.jar:../hsqldb.jar:. ApiExample
+javac -classpath ../schemacrawler-6.0.1.jar ApiExample.java
+java -classpath ../schemacrawler-6.0.1.jar:../hsqldb.jar:. ApiExample
