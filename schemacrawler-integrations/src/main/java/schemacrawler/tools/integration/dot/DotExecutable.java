f72c84b37dda9b2fcc850fda9b4164db27b2db8a
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2009-04-01 23:52:23 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code@871 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler-integrations/src/main/java/schemacrawler/tools/integration/dot/DotExecutable.java
+++ schemacrawler-integrations/src/main/java/schemacrawler/tools/integration/dot/DotExecutable.java
@@ -57,19 +57,19 @@ public final class DotExecutable
     .getName());
   public static final String NEWLINE = System.getProperty("line.separator");
 
+  private static String dotError()
+  {
+    final byte[] text = Utilities.readFully(HtmlFormattingHelper.class
+      .getResourceAsStream("/dot.error.txt"));
+    return new String(text);
+  }
+
   private static String dotHeader(final String name)
   {
     final byte[] text = Utilities.readFully(HtmlFormattingHelper.class
       .getResourceAsStream("/dot.header.txt"));
     final String dotHeader = new String(text);
     return String.format(dotHeader, name);
-  }
-
-  private static String dotError()
-  {
-    final byte[] text = Utilities.readFully(HtmlFormattingHelper.class
-      .getResourceAsStream("/dot.error.txt"));
-    return new String(text);
   }
 
   /**
@@ -85,6 +85,34 @@ public final class DotExecutable
     throws Exception
   {
     executeOnSchema(args, "/schemacrawler-dot-readme.txt");
+  }
+
+  @Override
+  protected void doExecute(final DataSource dataSource)
+    throws Exception
+  {
+    final Catalog catalog = getCatalog(dataSource);
+    final OutputOptions outputOptions = toolOptions.getOutputOptions();
+    final File outputFile = outputOptions.getOutputFile();
+
+    try
+    {
+      final File dotFile = File.createTempFile("schemacrawler_"
+                                                   + catalog.getName() + "_",
+                                               ".dot");
+      dotFile.deleteOnExit();
+
+      final String outputFormat = outputOptions.getOutputFormatValue();
+      final Dot dot = new Dot();
+      writeDotFile(catalog, dotFile);
+      dot.generateDiagram(dotFile, outputFormat, outputFile);
+    }
+    catch (final Exception e)
+    {
+      LOGGER.log(Level.WARNING, "Could not write diagram", e);
+      writeDotFile(catalog, Utilities.changeFileExtension(outputFile, ".dot"));
+      System.out.println(dotError());
+    }
   }
 
   private int colorValue()
@@ -211,34 +239,6 @@ public final class DotExecutable
     writer.flush();
     writer.close();
     LOGGER.log(Level.INFO, "Wrote DOT file, " + dotFile.getAbsolutePath());
-  }
-
-  @Override
-  protected void doExecute(final DataSource dataSource)
-    throws Exception
-  {
-    final Catalog catalog = getCatalog(dataSource);
-    final OutputOptions outputOptions = toolOptions.getOutputOptions();
-    final File outputFile = outputOptions.getOutputFile();
-
-    try
-    {
-      final File dotFile = File.createTempFile("schemacrawler_"
-                                                   + catalog.getName() + "_",
-                                               ".dot");
-      dotFile.deleteOnExit();
-
-      final String outputFormat = outputOptions.getOutputFormatValue();
-      final Dot dot = new Dot();
-      writeDotFile(catalog, dotFile);
-      dot.generateDiagram(dotFile, outputFormat, outputFile);
-    }
-    catch (final Exception e)
-    {
-      LOGGER.log(Level.WARNING, "Could not write diagram", e);
-      writeDotFile(catalog, Utilities.changeFileExtension(outputFile, ".dot"));
-      System.out.println(dotError());
-    }
   }
 
 }
