68b24cda2b0ba11174f6f4cd54b993526042e5d3
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2009-05-08 00:46:43 +0000

    6.3 to 6.4
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@962 3243f2e7-4e1b-42d2-9742-d984016aa316

--- distribution/src/assembly/StartDatabaseServer.sh
+++ distribution/src/assembly/StartDatabaseServer.sh
@@ -1,1 +1,1 @@
-java -cp hsqldb.jar;schemacrawler-6.3.jar schemacrawler.utility.test.TestUtility
+java -cp hsqldb.jar;schemacrawler-6.4.jar schemacrawler.utility.test.TestUtility
--- distribution/src/site/resources/examples/api/api.sh
+++ distribution/src/site/resources/examples/api/api.sh
@@ -1,3 +1,3 @@
 rm -f *.class
-javac -classpath ../schemacrawler-6.3.jar ApiExample.java
-java -classpath ../schemacrawler-6.3.jar:../hsqldb.jar:. ApiExample
+javac -classpath ../schemacrawler-6.4.jar ApiExample.java
+java -classpath ../schemacrawler-6.4.jar:../hsqldb.jar:. ApiExample
