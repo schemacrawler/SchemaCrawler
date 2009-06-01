cabc68e49c083208226bcc9be62658d8fdbdb865
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2009-06-01 12:02:45 +0000

    Changing version number to 6.5
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@977 3243f2e7-4e1b-42d2-9742-d984016aa316

--- distribution/src/site/resources/examples/api/api.cmd
+++ distribution/src/site/resources/examples/api/api.cmd
@@ -1,4 +1,4 @@
 @echo off
 del /f /q *.class
-javac -classpath ../schemacrawler-6.4.jar ApiExample.java
-java -classpath ../schemacrawler-6.4.jar;../hsqldb.jar;. ApiExample
+javac -classpath ../schemacrawler-6.5.jar ApiExample.java
+java -classpath ../schemacrawler-6.5.jar;../hsqldb.jar;. ApiExample
--- distribution/src/site/resources/examples/commandline/sc.cmd
+++ distribution/src/site/resources/examples/commandline/sc.cmd
@@ -1,1 +1,1 @@
-@java -classpath ../schemacrawler-6.4.jar;../hsqldb.jar schemacrawler.Main %*
+@java -classpath ../schemacrawler-6.5.jar;../hsqldb.jar schemacrawler.Main %*
