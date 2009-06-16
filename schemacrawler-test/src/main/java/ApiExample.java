2a0862901a13fb3c21817bc0bf6d7c7c3f51f4ee
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2009-06-16 13:40:56 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1009 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler-test/src/main/java/ApiExample.java
+++ schemacrawler-test/src/main/java/ApiExample.java
@@ -9,6 +9,7 @@ import schemacrawler.schema.Schema;
 import schemacrawler.schema.Table;
 import schemacrawler.schema.View;
 import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+import schemacrawler.schemacrawler.SchemaInfoLevel;
 import schemacrawler.utility.SchemaCrawlerUtility;
 import schemacrawler.utility.datasource.PropertiesDataSource;
 
@@ -23,7 +24,11 @@ public final class ApiExample
 
     // Create the options
     final SchemaCrawlerOptions options = new SchemaCrawlerOptions();
+    // Set what details are required in the schema - this affects the
+    // time taken to crawl the schema
+    options.setSchemaInfoLevel(SchemaInfoLevel.standard());
     options.setShowStoredProcedures(false);
+    // Sorting options
     options.setAlphabeticalSortForTableColumns(true);
 
     // Get the schema definition
@@ -34,7 +39,7 @@ public final class ApiExample
     {
       for (final Schema schema: catalog.getSchemas())
       {
-        System.out.println(catalog + "." + schema);
+        System.out.println(schema);
         for (final Table table: schema.getTables())
         {
           System.out.print("o--> " + table);
