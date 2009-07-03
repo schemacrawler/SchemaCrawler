4bbccd9f3585c6119ba79960a0f514087fe331ca
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2009-07-03 02:16:29 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1053 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler-tools/src/test/java/schemacrawler/test/TestUtility.java
+++ schemacrawler-tools/src/test/java/schemacrawler/test/TestUtility.java
@@ -27,7 +27,7 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.List;
 
-public class Utility
+public class TestUtility
 {
 
   public static void compareOutput(final String referenceFile,
@@ -85,7 +85,7 @@ public class Utility
     return line2 == null;
   }
 
-  private Utility()
+  private TestUtility()
   {
     // Prevent instantiation
   }
