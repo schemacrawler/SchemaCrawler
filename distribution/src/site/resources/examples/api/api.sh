47cd792db76b7452cc76535e98cb13fad839552e
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2009-09-24 20:01:19 +0000

    Change version to 7.3.1 for minor release
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1163 3243f2e7-4e1b-42d2-9742-d984016aa316

--- distribution/src/site/resources/examples/api/api.sh
+++ distribution/src/site/resources/examples/api/api.sh
@@ -1,3 +1,3 @@
 rm -f *.class
-javac -classpath ../schemacrawler-7.4.jar ApiExample.java
-java -classpath ../schemacrawler-7.4.jar:../hsqldb.jar:. ApiExample
+javac -classpath ../schemacrawler-7.3.1.jar ApiExample.java
+java -classpath ../schemacrawler-7.3.1.jar:../hsqldb.jar:. ApiExample
--- schemacrawler-tools/src/main/java/schemacrawler/Version.java
+++ schemacrawler-tools/src/main/java/schemacrawler/Version.java
@@ -34,7 +34,7 @@ public final class Version
 {
 
   private static final String PRODUCTNAME = "SchemaCrawler";
-  private static final String VERSION = "7.4";
+  private static final String VERSION = "7.3.1";
   private static final String ABOUT = Utility.readFully(Version.class
     .getResourceAsStream("/help/SchemaCrawler.txt"));
 
