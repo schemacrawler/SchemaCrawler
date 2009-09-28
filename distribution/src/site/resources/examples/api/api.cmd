e910581af7277a4c79eb01e76d22e25864c7e2e2
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2009-09-28 15:14:05 +0000

    Changing version to 7.4
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1166 3243f2e7-4e1b-42d2-9742-d984016aa316

--- distribution/src/site/resources/examples/api/api.cmd
+++ distribution/src/site/resources/examples/api/api.cmd
@@ -1,4 +1,4 @@
 @echo off
 del /f /q *.class
-javac -classpath ../schemacrawler-7.3.1.jar ApiExample.java
-java -classpath ../schemacrawler-7.3.1.jar;../hsqldb.jar;. ApiExample
+javac -classpath ../schemacrawler-7.4.jar ApiExample.java
+java -classpath ../schemacrawler-7.4.jar;../hsqldb.jar;. ApiExample
