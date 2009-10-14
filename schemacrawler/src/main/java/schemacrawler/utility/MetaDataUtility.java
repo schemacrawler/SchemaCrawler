471cb0a83c70c04f25cb13c64480a0e4429bb1df
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2009-10-14 21:57:06 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1174 3243f2e7-4e1b-42d2-9742-d984016aa316

---
+++ schemacrawler/src/main/java/schemacrawler/utility/MetaDataUtility.java
@@ -0,0 +1,65 @@
+/* 
+ *
+ * SchemaCrawler
+ * http://sourceforge.net/projects/schemacrawler
+ * Copyright (c) 2000-2009, Sualeh Fatehi.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+package schemacrawler.utility;
+
+
+import schemacrawler.crawl.JavaSqlType;
+import schemacrawler.crawl.JavaSqlTypesUtility;
+
+/**
+ * SchemaCrawler utility methods.
+ * 
+ * @author sfatehi
+ */
+public class MetaDataUtility
+{
+  /**
+   * Lookup java.sql.Types type, and return more detailed information,
+   * including the mapped Java class.
+   * 
+   * @param type
+   *        java.sql.Types type
+   * @return JavaSqlType type
+   */
+  public static JavaSqlType lookupSqlDataType(final int type)
+  {
+    return JavaSqlTypesUtility.lookupSqlDataType(type);
+  }
+
+  /**
+   * Lookup java.sql.Types type, and return more detailed information,
+   * including the mapped Java class.
+   * 
+   * @param typeName
+   *        java.sql.Types type name
+   * @return JavaSqlType type
+   */
+  public static JavaSqlType lookupSqlDataType(final String typeName)
+  {
+    return JavaSqlTypesUtility.lookupSqlDataType(typeName);
+  }
+
+  private MetaDataUtility()
+  {
+    // Prevent instantiation
+  }
+
+}
