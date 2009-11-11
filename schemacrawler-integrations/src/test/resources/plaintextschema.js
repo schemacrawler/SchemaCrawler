3639906a3c0249b3f380a099c8b510796bd55c29
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2009-11-11 01:41:19 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1291 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler-integrations/src/test/resources/plaintextschema.js
+++ schemacrawler-integrations/src/test/resources/plaintextschema.js
@@ -1,16 +1,21 @@
-var printDb = function() {
-  print(database.getDatabaseInfo() + "\n");
-  var catalogs = database.getCatalogs();
-  for ( var c = 0; c < catalogs.length; c++) {
-    var schemas = catalogs[c].getSchemas();
-    for ( var i = 0; i < schemas.length; i++) {
+var printDb = function()
+{
+  print(database.databaseInfo + "\n");
+  var catalogs = database.catalogs;
+  for ( var c = 0; c < catalogs.length; c++)
+  {
+    var schemas = catalogs[c].schemas;
+    for ( var i = 0; i < schemas.length; i++)
+    {
       print(schemas[i].getFullName() + "\n");
-      var tables = schemas[i].getTables();
-      for ( var j = 0; j < tables.length; j++) {
-        print("o--> " + tables[j].getName() + "\n");
-        var columns = tables[j].getColumns();
-        for ( var k = 0; k < columns.length; k++) {
-          print("     o--> " + columns[k].getName() + "\n");
+      var tables = schemas[i].tables;
+      for ( var j = 0; j < tables.length; j++)
+      {
+        print("o--> " + tables[j].name + "\n");
+        var columns = tables[j].columns;
+        for ( var k = 0; k < columns.length; k++)
+        {
+          print("     o--> " + columns[k].name + "\n");
         }
       }
     }
