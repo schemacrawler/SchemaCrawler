a5d1a0219b4bf96188bf2845d167896913defc8e
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2009-11-11 01:45:16 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1292 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler-integrations/src/test/resources/plaintextschema.js
+++ schemacrawler-integrations/src/test/resources/plaintextschema.js
@@ -1,21 +1,21 @@
 var printDb = function()
 {
-  print(database.databaseInfo + "\n");
+  println(database.databaseInfo);
   var catalogs = database.catalogs;
   for ( var c = 0; c < catalogs.length; c++)
   {
     var schemas = catalogs[c].schemas;
     for ( var i = 0; i < schemas.length; i++)
     {
-      print(schemas[i].getFullName() + "\n");
+      println(schemas[i].fullName);
       var tables = schemas[i].tables;
       for ( var j = 0; j < tables.length; j++)
       {
-        print("o--> " + tables[j].name + "\n");
+        println("o--> " + tables[j].name);
         var columns = tables[j].columns;
         for ( var k = 0; k < columns.length; k++)
         {
-          print("     o--> " + columns[k].name + "\n");
+          println("     o--> " + columns[k].name);
         }
       }
     }
