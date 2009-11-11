babe757fe4d734b31c992642b9366de12c85ec9f
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2009-11-11 01:41:19 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1291 3243f2e7-4e1b-42d2-9742-d984016aa316

---
+++ distribution/src/site/resources/examples/javascript/droptables.js
@@ -0,0 +1,30 @@
+var dropTables = function()
+{
+  println(database.databaseInfo + "\n");
+  var statement = connection.createStatement();
+  var catalogs = database.catalogs;
+  for ( var c = 0; c < catalogs.length; c++)
+  {
+    var schemas = catalogs[c].schemas;
+    for ( var i = 0; i < schemas.length; i++)
+    {
+      var tables = schemas[i].tables;
+      for ( var j = 0; j < tables.length; j++)
+      {
+        try
+        {
+          println("Attempting to drop table: " + tables[j].fullName + "\n");
+          statement.executeUpdate("DROP TABLE " + tables[j].fullName);
+        }
+        catch (e)
+        {
+          println(e);
+          println("Not dropping any more tables\n");
+          break;
+        }
+      }
+    }
+  }
+};
+
+dropTables();
