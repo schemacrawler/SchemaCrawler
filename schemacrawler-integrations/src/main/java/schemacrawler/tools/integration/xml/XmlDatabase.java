01744b5ada6a71b6fea4c6a6c6db240261407390
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2009-12-21 19:49:20 +0000

    Creating a decorator for XML serialization
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1454 3243f2e7-4e1b-42d2-9742-d984016aa316

---
+++ schemacrawler-integrations/src/main/java/schemacrawler/tools/integration/xml/XmlDatabase.java
@@ -0,0 +1,210 @@
+package schemacrawler.tools.integration.xml;
+
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.Map;
+
+import schemacrawler.schema.ColumnDataType;
+import schemacrawler.schema.Database;
+import schemacrawler.schema.DatabaseInfo;
+import schemacrawler.schema.JdbcDriverInfo;
+import schemacrawler.schema.NamedObject;
+import schemacrawler.schema.Schema;
+import schemacrawler.schema.SchemaCrawlerInfo;
+import schemacrawler.schemacrawler.SchemaCrawlerException;
+
+import com.thoughtworks.xstream.XStream;
+
+/**
+ * Decorates a database to allow for serialization to and from XML.
+ * 
+ * @author sfatehi
+ */
+public final class XmlDatabase
+  implements Database
+{
+
+  private static final long serialVersionUID = 5314326260124511414L;
+
+  private final Database database;
+
+  public XmlDatabase(final Database database)
+  {
+    if (database == null)
+    {
+      throw new IllegalArgumentException("No database provided");
+    }
+    this.database = database;
+  }
+
+  public XmlDatabase(final Reader reader)
+  {
+    this((Database) new XStream().fromXML(reader));
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see java.lang.Comparable#compareTo(java.lang.Object)
+   */
+  public int compareTo(final NamedObject o)
+  {
+    return database.compareTo(o);
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see schemacrawler.schema.NamedObject#getAttribute(java.lang.String)
+   */
+  public Object getAttribute(final String name)
+  {
+    return database.getAttribute(name);
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see schemacrawler.schema.NamedObject#getAttributes()
+   */
+  public Map<String, Object> getAttributes()
+  {
+    return database.getAttributes();
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see schemacrawler.schema.Database#getDatabaseInfo()
+   */
+  public DatabaseInfo getDatabaseInfo()
+  {
+    return database.getDatabaseInfo();
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see schemacrawler.schema.NamedObject#getFullName()
+   */
+  public String getFullName()
+  {
+    return database.getFullName();
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see schemacrawler.schema.Database#getJdbcDriverInfo()
+   */
+  public JdbcDriverInfo getJdbcDriverInfo()
+  {
+    return database.getJdbcDriverInfo();
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see schemacrawler.schema.NamedObject#getName()
+   */
+  public String getName()
+  {
+    return database.getName();
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see schemacrawler.schema.NamedObject#getRemarks()
+   */
+  public String getRemarks()
+  {
+    return database.getRemarks();
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see schemacrawler.schema.Database#getSchema(java.lang.String)
+   */
+  public Schema getSchema(final String schemaName)
+  {
+    return database.getSchema(schemaName);
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see schemacrawler.schema.Database#getSchemaCrawlerInfo()
+   */
+  public SchemaCrawlerInfo getSchemaCrawlerInfo()
+  {
+    return database.getSchemaCrawlerInfo();
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see schemacrawler.schema.Database#getSchemas()
+   */
+  public Schema[] getSchemas()
+  {
+    return database.getSchemas();
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see schemacrawler.schema.Database#getSystemColumnDataType(java.lang.String)
+   */
+  public ColumnDataType getSystemColumnDataType(final String name)
+  {
+    return database.getSystemColumnDataType(name);
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see schemacrawler.schema.Database#getSystemColumnDataTypes()
+   */
+  public ColumnDataType[] getSystemColumnDataTypes()
+  {
+    return database.getSystemColumnDataTypes();
+  }
+
+  /**
+   * Serializes the database to the writer, as XML.
+   */
+  public void save(final Writer writer)
+    throws SchemaCrawlerException
+  {
+    if (writer == null)
+    {
+      throw new SchemaCrawlerException("Writer not provided");
+    }
+    final XStream xStream = new XStream();
+    xStream.toXML(database, writer);
+    try
+    {
+      writer.flush();
+    }
+    catch (final IOException e)
+    {
+      throw new SchemaCrawlerException("Could not flush writer", e);
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see schemacrawler.schema.NamedObject#setAttribute(java.lang.String,
+   *      java.lang.Object)
+   */
+  public void setAttribute(final String name, final Object value)
+  {
+    database.setAttribute(name, value);
+  }
+
+}
