0c70b51b3ffc5f0a6c5063dd3596e95fe5d69c2e
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2010-01-03 17:09:36 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1520 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler-commandline/src/main/java/schemacrawler/tools/executable/SchemaCrawlerExecutable.java
+++ schemacrawler-commandline/src/main/java/schemacrawler/tools/executable/SchemaCrawlerExecutable.java
@@ -6,9 +6,6 @@ import java.sql.Connection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.sql.DataSource;
-
-import schemacrawler.schemacrawler.ConnectionOptions;
 import schemacrawler.schemacrawler.SchemaCrawlerOptions;
 import schemacrawler.tools.options.OutputOptions;
 
