39c4ba864f2899b263a0ad896a7709fb34a2da84
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2010-01-27 18:10:12 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1599 3243f2e7-4e1b-42d2-9742-d984016aa316

--- distribution/src/site/resources/examples/javascript/javascript.sh
+++ distribution/src/site/resources/examples/javascript/javascript.sh
@@ -1,1 +1,1 @@
-java -classpath ../schemacrawler-8.0.jar:../hsqldb.jar schemacrawler.tools.integration.scripting.Main -g schemacrawler.config.properties -c hsqldb -command verbose_schema -outputformat $1
+java -classpath ../schemacrawler-8.0.jar:../hsqldb.jar schemacrawler.Main -g schemacrawler.config.properties -c hsqldb -command script -infolevel=maximum -sorttables=false -outputformat $1
