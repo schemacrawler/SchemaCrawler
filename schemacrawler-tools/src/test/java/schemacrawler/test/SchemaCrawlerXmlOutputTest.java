ffe0b4190092f9729e75f419512f466b7e9601dc
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2010-01-28 00:36:15 +0000

    More robust unit tests for XML output
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1608 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler-tools/src/test/java/schemacrawler/test/SchemaCrawlerXmlOutputTest.java
+++ schemacrawler-tools/src/test/java/schemacrawler/test/SchemaCrawlerXmlOutputTest.java
@@ -23,14 +23,12 @@ package schemacrawler.test;
 
 import static org.junit.Assert.fail;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.custommonkey.xmlunit.Validator;
 import org.custommonkey.xmlunit.XMLUnit;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -39,6 +37,7 @@ import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
+import schemacrawler.schemacrawler.SchemaCrawlerException;
 import schemacrawler.schemacrawler.SchemaCrawlerOptions;
 import schemacrawler.schemacrawler.SchemaInfoLevel;
 import schemacrawler.tools.executable.SchemaCrawlerExecutable;
@@ -47,6 +46,7 @@ import schemacrawler.tools.options.OutputOptions;
 import schemacrawler.tools.text.operation.Operation;
 import schemacrawler.tools.text.schema.SchemaTextDetailType;
 import schemacrawler.utility.TestDatabase;
+import sf.util.TestUtility;
 
 public class SchemaCrawlerXmlOutputTest
 {
@@ -91,21 +91,56 @@ public class SchemaCrawlerXmlOutputTest
   }
 
   @Test
-  public void brief_schemaValidXMLOutput()
+  public void countOperatorValidXMLOutput()
     throws Exception
   {
-    final String outputFilename = File.createTempFile("schemacrawler",
-                                                      ".test.html")
-      .getAbsolutePath();
+    checkValidXmlOutput(Operation.count.name());
+  }
+
+  @Test
+  public void dumpOperatorValidXMLOutput()
+    throws Exception
+  {
+    checkValidXmlOutput(Operation.dump.name());
+  }
+
+  @Test
+  public void list_objectsValidXMLOutput()
+    throws Exception
+  {
+    checkValidXmlOutput(SchemaTextDetailType.list_objects.name());
+  }
+
+  @Test
+  public void standard_schemaValidXMLOutput()
+    throws Exception
+  {
+    checkValidXmlOutput(SchemaTextDetailType.standard_schema.name());
+  }
+
+  @Test
+  public void verbose_schemaValidXMLOutput()
+    throws Exception
+  {
+    checkValidXmlOutput(SchemaTextDetailType.verbose_schema.name());
+  }
+
+  private void checkValidXmlOutput(final String command)
+    throws IOException, Exception, SchemaCrawlerException
+  {
+    final String referenceFile = command + ".xml";
+    final File testOutputFile = File.createTempFile("schemacrawler."
+                                                        + referenceFile + ".",
+                                                    ".test");
 
     final OutputOptions outputOptions = new OutputOptions(OutputFormat.html,
-                                                          outputFilename);
+                                                          testOutputFile
+                                                            .getAbsolutePath());
     outputOptions.setNoHeader(false);
     outputOptions.setNoFooter(false);
     outputOptions.setNoInfo(false);
 
-    final SchemaCrawlerExecutable executable = new SchemaCrawlerExecutable(SchemaTextDetailType.list_objects
-      .name());
+    final SchemaCrawlerExecutable executable = new SchemaCrawlerExecutable(command);
 
     final SchemaCrawlerOptions options = executable.getSchemaCrawlerOptions();
     options.setSchemaInfoLevel(SchemaInfoLevel.minimum());
@@ -113,126 +148,16 @@ public class SchemaCrawlerXmlOutputTest
     executable.setOutputOptions(outputOptions);
     executable.execute(testUtility.getConnection());
 
-    validateXml(outputFilename);
-  }
+    final List<String> failures = new ArrayList<String>();
 
-  @Test
-  public void countOperatorValidXMLOutput()
-    throws Exception
-  {
-    final String outputFilename = File.createTempFile("schemacrawler",
-                                                      ".test.html")
-      .getAbsolutePath();
+    TestUtility.compareOutput("xml_output/" + referenceFile,
+                              testOutputFile,
+                              OutputFormat.html,
+                              failures);
 
-    final OutputOptions outputOptions = new OutputOptions(OutputFormat.html,
-                                                          outputFilename);
-    outputOptions.setNoHeader(false);
-    outputOptions.setNoFooter(false);
-    outputOptions.setNoInfo(false);
-
-    final SchemaCrawlerExecutable executable = new SchemaCrawlerExecutable(Operation.count
-      .name());
-    executable.setOutputOptions(outputOptions);
-    executable.execute(testUtility.getConnection());
-
-    validateXml(outputFilename);
-  }
-
-  @Test
-  public void dumpOperatorValidXMLOutput()
-    throws Exception
-  {
-    final String outputFilename = File.createTempFile("schemacrawler",
-                                                      ".test.html")
-      .getAbsolutePath();
-
-    final OutputOptions outputOptions = new OutputOptions(OutputFormat.html,
-                                                          outputFilename);
-    outputOptions.setNoHeader(false);
-    outputOptions.setNoFooter(false);
-    outputOptions.setNoInfo(false);
-
-    final SchemaCrawlerExecutable executable = new SchemaCrawlerExecutable(Operation.dump
-      .name());
-    executable.setOutputOptions(outputOptions);
-    executable.execute(testUtility.getConnection());
-
-    validateXml(outputFilename);
-  }
-
-  @Test
-  public void standard_schemaValidXMLOutput()
-    throws Exception
-  {
-    final String outputFilename = File.createTempFile("schemacrawler",
-                                                      ".test.html")
-      .getAbsolutePath();
-
-    final OutputOptions outputOptions = new OutputOptions(OutputFormat.html,
-                                                          outputFilename);
-    outputOptions.setNoHeader(false);
-    outputOptions.setNoFooter(false);
-    outputOptions.setNoInfo(false);
-
-    final SchemaCrawlerExecutable executable = new SchemaCrawlerExecutable(SchemaTextDetailType.standard_schema
-      .name());
-    executable.getSchemaCrawlerOptions().setSchemaInfoLevel(SchemaInfoLevel
-      .detailed());
-    executable.setOutputOptions(outputOptions);
-    executable.execute(testUtility.getConnection());
-
-    validateXml(outputFilename);
-  }
-
-  @Test
-  public void verbose_schemaValidXMLOutput()
-    throws Exception
-  {
-    final String outputFilename = File.createTempFile("schemacrawler",
-                                                      ".test.html")
-      .getAbsolutePath();
-
-    final OutputOptions outputOptions = new OutputOptions(OutputFormat.html,
-                                                          outputFilename);
-    outputOptions.setNoHeader(false);
-    outputOptions.setNoFooter(false);
-    outputOptions.setNoInfo(false);
-
-    final SchemaCrawlerExecutable executable = new SchemaCrawlerExecutable(SchemaTextDetailType.verbose_schema
-      .name());
-    executable.getSchemaCrawlerOptions().setSchemaInfoLevel(SchemaInfoLevel
-      .maximum());
-    executable.setOutputOptions(outputOptions);
-    executable.execute(testUtility.getConnection());
-
-    validateXml(outputFilename);
-  }
-
-  private void validateXml(final String outputFilename)
-    throws Exception
-  {
-    final File outputFile = new File(outputFilename);
-    Reader reader = null;
-    try
+    if (failures.size() > 0)
     {
-      reader = new BufferedReader(new FileReader(outputFile));
-      final Validator validator = new Validator(reader);
-      final boolean isValid = validator.isValid();
-      if (!isValid)
-      {
-
-      }
-    }
-    finally
-    {
-      if (reader != null)
-      {
-        reader.close();
-      }
-      if (!outputFile.delete())
-      {
-        fail("Could not delete output file, " + outputFile.getAbsolutePath());
-      }
+      fail(failures.toString());
     }
   }
 
