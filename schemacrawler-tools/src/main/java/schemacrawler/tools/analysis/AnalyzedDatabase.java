7ad82d17d55cda328c9b74438a0349d5c67b4fe9
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2010-02-10 18:51:48 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1649 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler-tools/src/main/java/schemacrawler/tools/analysis/AnalyzedDatabase.java
+++ schemacrawler-tools/src/main/java/schemacrawler/tools/analysis/AnalyzedDatabase.java
@@ -2,9 +2,12 @@ package schemacrawler.tools.analysis;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import schemacrawler.schema.Column;
 import schemacrawler.schema.ColumnDataType;
@@ -26,6 +29,29 @@ public final class AnalyzedDatabase
     .getName());
 
   private static final String TABLE_WTH_SINGLE_COLUMN = "table with single column";
+  private static final String INCREMENTING_COLUMNS = "incrementing columns";
+
+  public static final Column[] getIncrementingColumns(final Table table)
+  {
+    if (table == null)
+    {
+      return null;
+    }
+    else
+    {
+      final Object incrementingColumns = table
+        .getAttribute(INCREMENTING_COLUMNS);
+      if (incrementingColumns == null
+          || !(incrementingColumns instanceof Column[]))
+      {
+        return new Column[0];
+      }
+      else
+      {
+        return (Column[]) incrementingColumns;
+      }
+    }
+  }
 
   public static final boolean isTableWithSingleColumn(final Table table)
   {
@@ -35,12 +61,71 @@ public final class AnalyzedDatabase
     }
     else
     {
-      final Column[] columns = table.getColumns();
-      return columns.length <= 1;
+      final Boolean isTableWithSingleColumn = (Boolean) table
+        .getAttribute(TABLE_WTH_SINGLE_COLUMN);
+      if (isTableWithSingleColumn == null)
+      {
+        return false;
+      }
+      else
+      {
+        return isTableWithSingleColumn;
+      }
     }
   }
 
+  private static Column[] findIncrementingColumns(final Table table,
+                                                  final Column[] columns)
+  {
+    final Pattern pattern = Pattern.compile("([^0-9]*)([0-9]+)");
+
+    final Map<String, Integer> incrementingColumnsMap = new HashMap<String, Integer>();
+    for (final Column column: columns)
+    {
+      final String columnName = column.getName();
+      incrementingColumnsMap.put(columnName, 1);
+      final Matcher matcher = pattern.matcher(columnName);
+      if (matcher.matches())
+      {
+        final String columnNameBase = matcher.group(1);
+        if (incrementingColumnsMap.containsKey(columnNameBase))
+        {
+          incrementingColumnsMap.put(columnNameBase, incrementingColumnsMap
+            .get(columnNameBase) + 1);
+        }
+        else
+        {
+          incrementingColumnsMap.put(columnNameBase, 1);
+        }
+      }
+    }
+    for (final String columnNameBase: incrementingColumnsMap.keySet())
+    {
+      if (incrementingColumnsMap.get(columnNameBase) == 1)
+      {
+        incrementingColumnsMap.remove(columnNameBase);
+      }
+    }
+
+    final List<Column> incrementingColumns = new ArrayList<Column>();
+    for (final Column column: columns)
+    {
+      final Matcher matcher = pattern.matcher(column.getName());
+      if (matcher.matches())
+      {
+        final String columnNameBase = matcher.group(1);
+        if (incrementingColumnsMap.containsKey(columnNameBase))
+        {
+          incrementingColumns.add(column);
+        }
+      }
+    }
+
+    return incrementingColumns.toArray(new Column[incrementingColumns.size()]);
+  }
+
   private final Database database;
+
   private final List<Table> tables;
 
   public AnalyzedDatabase(final Database database)
@@ -61,17 +146,6 @@ public final class AnalyzedDatabase
     }
 
     analyzeTables();
-  }
-
-  private void analyzeTables()
-  {
-    for (final Table table: tables)
-    {
-      if (isTableWithSingleColumn(table))
-      {
-        table.setAttribute(TABLE_WTH_SINGLE_COLUMN, Boolean.TRUE);
-      }
-    }
   }
 
   public int compareTo(final NamedObject o)
@@ -142,6 +216,24 @@ public final class AnalyzedDatabase
   public void setAttribute(final String name, final Object value)
   {
     database.setAttribute(name, value);
+  }
+
+  private void analyzeTables()
+  {
+    for (final Table table: tables)
+    {
+      final Column[] columns = table.getColumns();
+      if (columns.length <= 1)
+      {
+        table.setAttribute(TABLE_WTH_SINGLE_COLUMN, Boolean.TRUE);
+      }
+      final Column[] incrementingColumns = findIncrementingColumns(table,
+                                                                   columns);
+      if (incrementingColumns.length > 0)
+      {
+        table.setAttribute(INCREMENTING_COLUMNS, incrementingColumns);
+      }
+    }
   }
 
 }
