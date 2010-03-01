946979eb7360a9e58b51bb35a3dccf8a97362115
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2010-03-01 01:00:34 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1693 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler/src/main/java/sf/util/Utility.java
+++ schemacrawler/src/main/java/sf/util/Utility.java
@@ -26,7 +26,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
-import java.util.Enumeration;
+import java.util.Collections;
+import java.util.List;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
@@ -163,16 +164,19 @@ public final class Utility
   public static void setApplicationLogLevel(final Level logLevel)
   {
     final LogManager logManager = LogManager.getLogManager();
-    for (final Enumeration<String> loggerNames = logManager.getLoggerNames(); loggerNames
-      .hasMoreElements();)
+    final List<String> loggerNames = Collections.list(logManager
+      .getLoggerNames());
+    for (final String loggerName: loggerNames)
     {
-      final String loggerName = loggerNames.nextElement();
       final Logger logger = logManager.getLogger(loggerName);
-      logger.setLevel(null);
-      final Handler[] handlers = logger.getHandlers();
-      for (final Handler handler: handlers)
+      if (logger != null)
       {
-        handler.setLevel(logLevel);
+        logger.setLevel(null);
+        final Handler[] handlers = logger.getHandlers();
+        for (final Handler handler: handlers)
+        {
+          handler.setLevel(logLevel);
+        }
       }
     }
 
