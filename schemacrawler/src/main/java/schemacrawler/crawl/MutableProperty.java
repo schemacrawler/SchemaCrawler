6d324bb91de152fbd0b7300b4130d124224c06ec
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2010-03-04 02:02:21 +0000

    - Changing groupId to prepare for Maven repository upload - Refactored property classes so that JDBC driver properties can be sorted
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1705 3243f2e7-4e1b-42d2-9742-d984016aa316

---
+++ schemacrawler/src/main/java/schemacrawler/crawl/MutableProperty.java
@@ -0,0 +1,122 @@
+/* 
+ *
+ * SchemaCrawler
+ * http://sourceforge.net/projects/schemacrawler
+ * Copyright (c) 2000-2010, Sualeh Fatehi.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+package schemacrawler.crawl;
+
+
+import schemacrawler.schema.Property;
+import sf.util.Utility;
+
+abstract class MutableProperty
+  implements Property
+{
+
+  private static final long serialVersionUID = -7150431683440256142L;
+
+  private final String name;
+  private final Object value;
+
+  MutableProperty(final String name, final Object value)
+  {
+    if (Utility.isBlank(name))
+    {
+      throw new IllegalArgumentException("No property name provided");
+    }
+    this.name = name.trim();
+    this.value = value;
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  @Override
+  public final boolean equals(final Object obj)
+  {
+    if (this == obj)
+    {
+      return true;
+    }
+    if (obj == null)
+    {
+      return false;
+    }
+    if (!(obj instanceof MutableProperty))
+    {
+      return false;
+    }
+    final MutableProperty other = (MutableProperty) obj;
+    if (name == null)
+    {
+      if (other.name != null)
+      {
+        return false;
+      }
+    }
+    else if (!name.equals(other.name))
+    {
+      return false;
+    }
+    if (value == null)
+    {
+      if (other.value != null)
+      {
+        return false;
+      }
+    }
+    else if (!value.equals(other.value))
+    {
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public final String getName()
+  {
+    return name;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public Object getValue()
+  {
+    return value;
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public final int hashCode()
+  {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + (name == null? 0: name.hashCode());
+    result = prime * result + (value == null? 0: value.hashCode());
+    return result;
+  }
+
+}
