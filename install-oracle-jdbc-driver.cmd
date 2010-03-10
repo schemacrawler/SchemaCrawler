44e21f48e04defd70af3943e47f167f9f4683205
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2010-03-10 19:58:45 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1718 3243f2e7-4e1b-42d2-9742-d984016aa316

--- install-oracle-jdbc-driver.cmd
+++ install-oracle-jdbc-driver.cmd
@@ -1,1 +1,1 @@
-@mvn install:install-file -DgroupId=oracle -DartifactId=oracle-jdbc -Dversion=11.1.0.7.0 -Dfile=ojdbc6_g.jar -Dpackaging=jar -DgeneratePom=true
+@mvn install:install-file -DgroupId=oracle -DartifactId=oracle-jdbc -Dversion=11.2.0.1.0 -Dfile=ojdbc5_g.jar -Dpackaging=jar -DgeneratePom=true
