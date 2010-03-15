cafcc155a08ac69680b4bb365b226ce2464558d5
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2010-03-15 15:21:24 +0000

    Changing version to 8.2-SNAPSHOT
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1742 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler/src/assembly/install-schemacrawler-into-maven.cmd
+++ schemacrawler/src/assembly/install-schemacrawler-into-maven.cmd
@@ -1,1 +1,1 @@
-mvn install:install-file -DgroupId=schemacrawler -DartifactId=schemacrawler -Dversion=8.1 -Dpackaging=jar -Dfile=lib/schemacrawler-8.1.jar -DgeneratePom=true -DcreateChecksum=true
+mvn install:install-file -DgroupId=schemacrawler -DartifactId=schemacrawler -Dversion=8.2-SNAPSHOT -Dpackaging=jar -Dfile=lib/schemacrawler-8.2-SNAPSHOT.jar -DgeneratePom=true -DcreateChecksum=true
--- schemacrawler/src/site/resources/examples/api/api.cmd
+++ schemacrawler/src/site/resources/examples/api/api.cmd
@@ -1,4 +1,4 @@
 @echo off
 del /f /q *.class
-javac -classpath ../schemacrawler-8.1.jar ApiExample.java
-java -classpath ../schemacrawler-8.1.jar;../hsqldb.jar;. ApiExample
+javac -classpath ../schemacrawler-8.2-SNAPSHOT.jar ApiExample.java
+java -classpath ../schemacrawler-8.2-SNAPSHOT.jar;../hsqldb.jar;. ApiExample
