4da4755932eef919d761bfc223e987f85efac630
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2010-07-02 23:57:24 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1884 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler-api/src/main/resources/schemacrawler.test.post_schema.1.sql
+++ schemacrawler-api/src/main/resources/schemacrawler.test.post_schema.1.sql
@@ -3,7 +3,3 @@ CREATE TRIGGER TRG_Authors BEFORE DELETE ON Authors FOR EACH ROW CALL "org.hsqld
 
 -- Synonyms?
 CREATE ALIAS ABS FOR "java.lang.Math.abs"
-
--- Create the next schema
-CREATE SCHEMA SALES AUTHORIZATION DBA;
-SET SCHEMA SALES;
