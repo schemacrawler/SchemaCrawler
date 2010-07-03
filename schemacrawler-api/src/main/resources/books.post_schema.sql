d91b6b7258e69d8a6a7045a1ff2b349d329ec243
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2010-07-03 18:30:19 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1911 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler-api/src/main/resources/books.post_schema.sql
+++ schemacrawler-api/src/main/resources/books.post_schema.sql
@@ -1,3 +1,13 @@
+-- Remarks
+COMMENT ON TABLE Publishers IS 'List of book publishers'
+;
+COMMENT ON TABLE Authors IS 'Contact details for book authors'
+;
+COMMENT ON TABLE Books IS 'Details for published books'
+;
+COMMENT ON TABLE BookAuthors IS 'Relationship between books and their authors, along with the latest updated information'
+;
+
 -- Stored procedures
 CREATE PROCEDURE New_Publisher(OUT Publisher VARCHAR(50))
   SET Publisher = 'New Publisher'
@@ -6,4 +16,3 @@ CREATE PROCEDURE New_Publisher(OUT Publisher VARCHAR(50))
 -- Triggers
 CREATE TRIGGER TRG_Authors AFTER DELETE ON Authors FOR EACH ROW UPDATE Publishers SET Publisher = 'Jacob' WHERE Publisher = 'John'
 ;
-  
