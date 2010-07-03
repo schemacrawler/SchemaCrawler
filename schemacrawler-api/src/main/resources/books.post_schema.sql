668a31a38b7735ae9445dcdeb10c0243c35939d2
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2010-07-03 07:34:41 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1904 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler-api/src/main/resources/books.post_schema.sql
+++ schemacrawler-api/src/main/resources/books.post_schema.sql
@@ -1,9 +1,9 @@
+-- Stored procedures
+CREATE PROCEDURE New_Publisher(OUT Publisher VARCHAR(50))
+  SET Publisher = 'New Publisher'
+;
+  
 -- Triggers
-CREATE TRIGGER TRG_Authors BEFORE DELETE ON Authors FOR EACH ROW CALL "org.hsqldb.sample.TriggerSample";
-
--- Synonyms?
--- CREATE ALIAS ABS FOR "java.lang.Math.abs"
-
-CREATE DOMAIN VALUE_STRING AS VARCHAR(20) 
-  DEFAULT 'NO VALUE' 
-  CHECK (VALUE IS NOT NULL AND CHARACTER_LENGTH(VALUE) > 2);
+CREATE TRIGGER TRG_Authors AFTER DELETE ON Authors FOR EACH ROW UPDATE Publishers SET Publisher = 'Jacob' WHERE Publisher = 'John'
+;
+  
