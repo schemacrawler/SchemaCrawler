5dc55b34a153e89e476a5d39829696340bec8bb7
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2010-07-13 01:37:53 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1947 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler/src/site/resources/examples/api/api.sh
+++ schemacrawler/src/site/resources/examples/api/api.sh
@@ -1,3 +1,3 @@
 rm -f *.class
 javac -classpath ../_schemacrawler/lib/_schemacrawler/lib/schemacrawler-8.3.jar ApiExample.java
-java -classpath ../_schemacrawler/lib/_schemacrawler/lib/schemacrawler-8.3.jar:../_schemacrawler/lib/_schemacrawler/lib/hsqldb.jar:. ApiExample
+java -classpath ../_schemacrawler/lib/_schemacrawler/lib/schemacrawler-8.3.jar:../_schemacrawler/lib/_schemacrawler/lib/hsqldb-2.0.0.jar:. ApiExample
--- schemacrawler/src/site/resources/examples/commandline/sc.cmd
+++ schemacrawler/src/site/resources/examples/commandline/sc.cmd
@@ -1,1 +1,1 @@
-@java -classpath ..\_schemacrawler\lib\schemacrawler-8.3.jar;..\_schemacrawler\lib\hsqldb.jar schemacrawler.Main %*
+@java -classpath ..\_schemacrawler\lib\schemacrawler-8.3.jar;..\_schemacrawler\lib\hsqldb-2.0.0.jar schemacrawler.Main %*
