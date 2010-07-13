51e14d8649c5357b8fbeafc91cb7cbdf4f86b551
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2010-07-13 01:37:53 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1947 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler/src/site/resources/examples/javascript/javascript.sh
+++ schemacrawler/src/site/resources/examples/javascript/javascript.sh
@@ -1,1 +1,1 @@
-java -classpath ../_schemacrawler/lib/schemacrawler-8.3.jar:../_schemacrawler/lib/hsqldb.jar schemacrawler.Main -g schemacrawler.config.properties -c hsqldb -command script -infolevel=maximum -sorttables=false -outputformat $1
+java -classpath ../_schemacrawler/lib/schemacrawler-8.3.jar:../_schemacrawler/lib/hsqldb-2.0.0.jar schemacrawler.Main -g schemacrawler.config.properties -c hsqldb -command script -infolevel=maximum -sorttables=false -outputformat $1
