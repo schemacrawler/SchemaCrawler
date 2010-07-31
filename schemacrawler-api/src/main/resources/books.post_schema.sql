676b854275dd6a9d2a0b4e334f51523aac422dd2
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2010-07-31 19:38:47 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@1982 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler-api/src/main/resources/books.post_schema.sql
+++ schemacrawler-api/src/main/resources/books.post_schema.sql
@@ -1,11 +1,30 @@
 -- Remarks
-COMMENT ON TABLE Publishers IS 'List of book publishers'
+COMMENT ON TABLE  Publishers IS 'List of book publishers'
 ;
-COMMENT ON TABLE Authors IS 'Contact details for book authors'
+COMMENT ON COLUMN Publishers.Id IS 'Unique (internal) id for book publisher'
 ;
-COMMENT ON TABLE Books IS 'Details for published books'
+COMMENT ON COLUMN Publishers.Publisher IS 'Name of book publisher'
 ;
-COMMENT ON TABLE BookAuthors IS 'Relationship between books and their authors, along with the latest updated information'
+
+COMMENT ON TABLE  Authors IS 'Contact details for book authors'
+;
+
+COMMENT ON TABLE  Books IS 'Details for published books'
+;
+COMMENT ON COLUMN Books.Id IS 'Unique (internal) id for book'
+;
+COMMENT ON COLUMN Books.Title IS 'Book title'
+;
+COMMENT ON COLUMN Books.Description IS 'Book description'
+;
+COMMENT ON COLUMN Books.PublisherId IS 'Foreign key to the book publisher'
+;
+COMMENT ON COLUMN Books.PublicationDate IS 'Book publication date'
+;
+COMMENT ON COLUMN Books.Price IS 'Current price for the book'
+;
+
+COMMENT ON TABLE  BookAuthors IS 'Relationship between books and their authors, along with the latest updated information'
 ;
 
 -- Stored procedures
