4900168380c5d3d66850107d15237b28b7e72c82
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2011-02-10 01:07:35 +0000

    Changing version to 8.6
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@2084 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler/src/site/resources/examples/diagram/diagram.sh
+++ schemacrawler/src/site/resources/examples/diagram/diagram.sh
@@ -1,3 +1,3 @@
 rm -f database-diagram.pdf
-java -classpath ../_schemacrawler/lib/schemacrawler-8.5.1.jar:../_schemacrawler/lib/hsqldb-2.0.0.jar schemacrawler.Main -c hsqldb -infolevel=lint -command=diagram -outputformat=pdf -outputfile=database-diagram.pdf
+java -classpath ../_schemacrawler/lib/schemacrawler-8.6.jar:../_schemacrawler/lib/hsqldb-2.0.0.jar schemacrawler.Main -c hsqldb -infolevel=lint -command=diagram -outputformat=pdf -outputfile=database-diagram.pdf
 echo Database diagram is in database-diagram.pdf
