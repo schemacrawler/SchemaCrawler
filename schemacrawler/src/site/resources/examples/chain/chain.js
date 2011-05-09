164562d2bb599b0957d02b0f481d6ee5acc57849
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2011-05-09 00:54:29 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@2112 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler/src/site/resources/examples/chain/chain.js
+++ schemacrawler/src/site/resources/examples/chain/chain.js
@@ -3,6 +3,7 @@ var scCommands = function()
 	chain.addNext("schema", "text", "schema.txt");
 	chain.addNext("graph", "png", "schema.png");
 	chain.execute();
+	print('Created files "schema.txt" and "schema.png"');
 };
 
 scCommands();
