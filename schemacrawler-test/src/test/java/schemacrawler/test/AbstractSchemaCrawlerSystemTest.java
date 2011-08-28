049550c0bffb605b8727ba56ae857cbf4a77e07b
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2011-08-28 16:34:54 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@2134 3243f2e7-4e1b-42d2-9742-d984016aa316

---
+++ schemacrawler-test/src/test/java/schemacrawler/test/AbstractSchemaCrawlerSystemTest.java
@@ -0,0 +1,156 @@
+/*
+ * SchemaCrawler
+ * Copyright (c) 2000-2011, Sualeh Fatehi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package schemacrawler.test;
+
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import schemacrawler.schema.Database;
+import schemacrawler.schema.Schema;
+import schemacrawler.schemacrawler.ConnectionOptions;
+import schemacrawler.schemacrawler.InclusionRule;
+import schemacrawler.schemacrawler.SchemaCrawlerException;
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+import schemacrawler.schemacrawler.SchemaInfoLevel;
+import schemacrawler.tools.options.InfoLevel;
+import schemacrawler.utility.SchemaCrawlerUtility;
+import sf.util.ObjectToString;
+
+public abstract class AbstractSchemaCrawlerSystemTest
+{
+
+  private final ApplicationContext appContext = new ClassPathXmlApplicationContext("datasources.xml");
+  protected final String[] dataSources = {
+      "MicrosoftSQLServer",
+      "Oracle",
+      "IBM_DB2",
+      "MySQL",
+      "PostgreSQL",
+      "SQLite",
+  };
+
+  @Test
+  public void connections()
+    throws Exception
+  {
+    final List<String> connectionErrors = new ArrayList<String>();
+    for (final String dataSource: dataSources)
+    {
+      try
+      {
+        connect(dataSource);
+      }
+      catch (final Exception e)
+      {
+        final String message = dataSource + ": " + e.getMessage();
+        System.out.println(message);
+        connectionErrors.add(message);
+      }
+    }
+    if (!connectionErrors.isEmpty())
+    {
+      final String error = ObjectToString.toString(connectionErrors);
+      System.out.println(error);
+      fail(error);
+    }
+  }
+
+  protected Connection connect(final String dataSourceName)
+    throws Exception
+  {
+    final ConnectionOptions connectionOptions = (ConnectionOptions) appContext
+      .getBean(dataSourceName);
+    final Connection connection = connectionOptions.getConnection();
+    return connection;
+  }
+
+  protected SchemaCrawlerOptions createOptions(final String schemaInclusion)
+  {
+    final SchemaCrawlerOptions schemaCrawlerOptions = new SchemaCrawlerOptions();
+    schemaCrawlerOptions.setSchemaInfoLevel(InfoLevel.lint.getSchemaInfoLevel());
+    if (schemaInclusion != null)
+    {
+      schemaCrawlerOptions
+        .setSchemaInclusionRule(new InclusionRule(schemaInclusion,
+                                                  InclusionRule.NONE));
+    }
+    return schemaCrawlerOptions;
+  }
+
+  protected Database retrieveDatabase(final String dataSourceName,
+                                    final SchemaCrawlerOptions schemaCrawlerOptions)
+    throws Exception
+  {
+    final Connection connection = connect(dataSourceName);
+    try
+    {
+      final Database database = SchemaCrawlerUtility
+        .getDatabase(connection, schemaCrawlerOptions);
+      return database;
+    }
+    catch (final Exception e)
+    {
+      throw new SchemaCrawlerException(dataSourceName, e);
+    }
+  }
+
+  protected Schema retrieveSchema(final String dataSourceName,
+                                final String schemaInclusion)
+    throws Exception
+  {
+    final SchemaCrawlerOptions schemaCrawlerOptions = createOptions(schemaInclusion);
+    final Database database = retrieveDatabase(dataSourceName,
+                                               schemaCrawlerOptions);
+
+    final Schema[] schemas = database.getSchemas();
+    final Schema schema;
+    if (schemas == null || schemas.length == 0)
+    {
+      schema = null;
+    }
+    else if (schemas.length == 1)
+    {
+      schema = schemas[0];
+    }
+    else
+    {
+      final Pattern schemaPattern = Pattern.compile(".*books",
+                                                    Pattern.CASE_INSENSITIVE);
+      Schema scSchema = null;
+      for (final Schema currSchema: schemas)
+      {
+        if (schemaPattern.matcher(currSchema.getFullName()).matches())
+        {
+          scSchema = currSchema;
+          break;
+        }
+      }
+      schema = scSchema;
+    }
+    return schema;
+  }
+
+}
---
+++ schemacrawler-test/src/test/java/schemacrawler/test/SchemaCrawlerSystemOutputTest.java
@@ -0,0 +1,125 @@
+/*
+ * SchemaCrawler
+ * Copyright (c) 2000-2011, Sualeh Fatehi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package schemacrawler.test;
+
+
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+import schemacrawler.tools.executable.SchemaCrawlerExecutable;
+import schemacrawler.tools.options.OutputFormat;
+import schemacrawler.tools.options.OutputOptions;
+import sf.util.ObjectToString;
+
+public class SchemaCrawlerSystemOutputTest
+  extends AbstractSchemaCrawlerSystemTest
+{
+
+  private static final String COMMAND_OUTPUT = "results/";
+
+  @Test
+  public void outputs()
+    throws Exception
+  {
+    final List<String> messages = new ArrayList<String>();
+    String message;
+    message = output("MicrosoftSQLServer", "Books.dbo");
+    if (message != null)
+    {
+      messages.add(message);
+    }
+
+    message = output("Oracle", "BOOKS");
+    if (message != null)
+    {
+      messages.add(message);
+    }
+
+    message = output("IBM_DB2", "BOOKS");
+    if (message != null)
+    {
+      messages.add(message);
+    }
+
+    message = output("MySQL", null);
+    if (message != null)
+    {
+      messages.add(message);
+    }
+
+    message = output("PostgreSQL", "books");
+    if (message != null)
+    {
+      messages.add(message);
+    }
+
+    // message = tablesAndCounts("SQLite", null, "\"");
+    // if (message != null)
+    // {
+    // messages.add(message);
+    // }
+
+    if (!messages.isEmpty())
+    {
+      final String error = ObjectToString.toString(messages);
+      System.out.println(error);
+      fail(error);
+    }
+  }
+
+  private String output(final String dataSourceName,
+                        final String schemaInclusion)
+    throws Exception
+  {
+    final String referenceFile = dataSourceName + ".txt";
+    final File testOutputFile = File.createTempFile("schemacrawler."
+                                                        + referenceFile + ".",
+                                                    ".test");
+    testOutputFile.delete();
+
+    final Connection connection = connect(dataSourceName);
+
+    final SchemaCrawlerOptions schemaCrawlerOptions = createOptions(schemaInclusion);
+
+    final OutputOptions outputOptions = new OutputOptions(OutputFormat.text.name(),
+                                                          testOutputFile);
+
+    final SchemaCrawlerExecutable executable = new SchemaCrawlerExecutable("details");
+    executable.setSchemaCrawlerOptions(schemaCrawlerOptions);
+    executable.setOutputOptions(outputOptions);
+    executable.execute(connection);
+
+    final List<String> failures = TestUtility
+      .compareOutput(COMMAND_OUTPUT + referenceFile,
+                     testOutputFile,
+                     outputOptions.getOutputFormat());
+    if (failures.size() > 0)
+    {
+      return failures.toString();
+    }
+
+    return null;
+  }
+
+}
