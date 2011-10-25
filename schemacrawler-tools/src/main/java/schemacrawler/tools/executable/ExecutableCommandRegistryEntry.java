51708c2b8c100e152169861c5d7a0e81d8da90a1
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2011-10-25 03:57:32 +0000

    Add executable providers
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@2195 3243f2e7-4e1b-42d2-9742-d984016aa316

---
+++ schemacrawler-tools/src/main/java/schemacrawler/tools/executable/ExecutableCommandRegistryEntry.java
@@ -0,0 +1,82 @@
+package schemacrawler.tools.executable;
+
+
+import java.lang.reflect.Constructor;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import schemacrawler.schemacrawler.SchemaCrawlerException;
+
+class ExecutableCommandRegistryEntry
+  implements CommandRegistryEntry
+{
+
+  private static final Logger LOGGER = Logger
+    .getLogger(ExecutableCommandRegistryEntry.class.getName());
+
+  private final String command;
+  private final String executableClassName;
+
+  ExecutableCommandRegistryEntry(final String command,
+                                 final String executableClassName)
+  {
+    this.command = command;
+    this.executableClassName = executableClassName;
+  }
+
+  public String getCommand()
+  {
+    return command;
+  }
+
+  public Executable newExecutable()
+    throws SchemaCrawlerException
+  {
+
+    Class<? extends Executable> commandExecutableClass;
+    try
+    {
+      commandExecutableClass = (Class<? extends Executable>) Class
+        .forName(executableClassName);
+    }
+    catch (final ClassNotFoundException e)
+    {
+      throw new SchemaCrawlerException("Could not load class "
+                                       + executableClassName, e);
+    }
+
+    Executable executable;
+    try
+    {
+      executable = commandExecutableClass.newInstance();
+    }
+    catch (final Exception e)
+    {
+      LOGGER.log(Level.FINE, "Could not instantiate " + executableClassName
+                             + " using the default constructor");
+      try
+      {
+        final Constructor<? extends Executable> constructor = commandExecutableClass
+          .getConstructor(new Class[] {
+            String.class
+          });
+        executable = constructor.newInstance(command);
+      }
+      catch (final Exception e1)
+      {
+        throw new SchemaCrawlerException("Could not instantiate executable for command '"
+                                             + command + "'",
+                                         e1);
+      }
+    }
+
+    return executable;
+  }
+
+  @Override
+  public String toString()
+  {
+    return executableClassName;
+  }
+
+}
