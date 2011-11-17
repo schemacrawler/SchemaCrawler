263a6488674f150ee1390af2f95b33461c815430
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2011-11-17 05:40:57 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@2223 3243f2e7-4e1b-42d2-9742-d984016aa316

---
+++ schemacrawler-lint/src/main/java/schemacrawler/tools/lint/executable/LintOptions.java
@@ -0,0 +1,17 @@
+package schemacrawler.tools.lint.executable;
+
+
+import schemacrawler.schemacrawler.Config;
+import schemacrawler.schemacrawler.Options;
+
+public class LintOptions
+  implements Options
+{
+
+  private static final long serialVersionUID = -5917925090616219096L;
+
+  public LintOptions(final Config additionalConfiguration)
+  {
+  }
+
+}
