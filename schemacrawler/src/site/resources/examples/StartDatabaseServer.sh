85e668e2cf85a64a4ebe36415691cee2408b0b1e
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2011-11-22 06:37:30 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@2232 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler/src/site/resources/examples/StartDatabaseServer.sh
+++ schemacrawler/src/site/resources/examples/StartDatabaseServer.sh
@@ -1,1 +1,1 @@
-java -cp _schemacrawler/lib/hsqldb-2.2.4.jar;_schemacrawler/lib/schemacrawler-8.9.jar schemacrawler.utility.TestDatabase
+java -cp _schemacrawler/lib/hsqldb-2.2.6.jar;_schemacrawler/lib/schemacrawler-8.9.jar schemacrawler.utility.TestDatabase
