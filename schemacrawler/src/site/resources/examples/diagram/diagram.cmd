a758ce7a18ceb12d6d15208b27d2336d3881b912
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2011-12-06 04:52:27 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@2268 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler/src/site/resources/examples/diagram/diagram.cmd
+++ schemacrawler/src/site/resources/examples/diagram/diagram.cmd
@@ -1,4 +1,4 @@
 @echo off
 del /q /f database-diagram.pdf
-java -classpath ..\_schemacrawler\lib\schemacrawler-8.9.jar;..\_schemacrawler\lib\hsqldb-2.2.6.jar schemacrawler.Main -c hsqldb -infolevel=maximum -command=graph -outputformat=pdf -outputfile=database-diagram.pdf
+..\_schemacrawler\sc.cmd -database=schemacrawler -user=sa -password= -infolevel=maximum -command=graph -outputformat=pdf -outputfile=database-diagram.pdf
 echo Database diagram is in database-diagram.pdf
