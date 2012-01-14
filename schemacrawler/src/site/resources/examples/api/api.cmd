9cffd861787f081b2d3db8104f30e0db41f606c1
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2012-01-14 05:48:02 +0000

    Version number
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@2290 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler/src/site/resources/examples/api/api.cmd
+++ schemacrawler/src/site/resources/examples/api/api.cmd
@@ -1,4 +1,4 @@
 @echo off
 del /f /q *.class
-javac -classpath ..\_schemacrawler\lib\schemacrawler-8.9.jar ApiExample.java
-java -classpath ..\_schemacrawler\lib\schemacrawler-8.9.jar;..\_schemacrawler\lib\hsqldb-2.2.6.jar;. ApiExample
+javac -classpath ..\_schemacrawler\lib\schemacrawler-8-10.jar ApiExample.java
+java -classpath ..\_schemacrawler\lib\schemacrawler-8-10.jar;..\_schemacrawler\lib\hsqldb-2.2.6.jar;. ApiExample
