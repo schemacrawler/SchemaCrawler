53ab007e9b614ec64be599bfbfae5e9424d82f8d
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2012-02-06 02:10:49 +0000

    Changing version to 8.11
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@2305 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler/src/site/resources/examples/db-specific-query/db-specific-query.sh
+++ schemacrawler/src/site/resources/examples/db-specific-query/db-specific-query.sh
@@ -1,1 +1,1 @@
-java -classpath ../_schemacrawler/lib/schemacrawler-8.10.jar:../_schemacrawler/lib/hsqldb-2.2.6.jar schemacrawler.Main -c hsqldb -infolevel=standard -command=hsqldb.tables
+java -classpath ../_schemacrawler/lib/schemacrawler-8.11.jar:../_schemacrawler/lib/hsqldb-2.2.6.jar schemacrawler.Main -c hsqldb -infolevel=standard -command=hsqldb.tables
--- schemacrawler/src/site/resources/examples/user-defined-query/user-defined-query.cmd
+++ schemacrawler/src/site/resources/examples/user-defined-query/user-defined-query.cmd
@@ -1,1 +1,1 @@
-@java -classpath ..\_schemacrawler\lib\schemacrawler-8.10.jar;..\_schemacrawler\lib\hsqldb-2.2.6.jar schemacrawler.Main -c hsqldb -infolevel=standard -command=tables.select -outputformat=csv
+@java -classpath ..\_schemacrawler\lib\schemacrawler-8.11.jar;..\_schemacrawler\lib\hsqldb-2.2.6.jar schemacrawler.Main -c hsqldb -infolevel=standard -command=tables.select -outputformat=csv
