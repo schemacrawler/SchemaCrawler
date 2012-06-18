cf2ee1544367403677d43d8ae89bebbb9fbc9bec
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2012-06-18 01:33:16 +0000

    Changing version to 8.16
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@2374 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler/src/site/resources/examples/api/api.sh
+++ schemacrawler/src/site/resources/examples/api/api.sh
@@ -1,3 +1,3 @@
 rm -f *.class
-javac -classpath ../_schemacrawler/lib/schemacrawler-8.15.jar ApiExample.java
-java -classpath ../_schemacrawler/lib/schemacrawler-8.15.jar:../_schemacrawler/lib/hsqldb-2.2.8.jar:. ApiExample
+javac -classpath ../_schemacrawler/lib/schemacrawler-8.16.jar ApiExample.java
+java -classpath ../_schemacrawler/lib/schemacrawler-8.16.jar:../_schemacrawler/lib/hsqldb-2.2.8.jar:. ApiExample
