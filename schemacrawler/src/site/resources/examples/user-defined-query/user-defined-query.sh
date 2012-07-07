e50c23a886a2c1c2de0155e87ff1ddc1739e9a79
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2012-07-07 15:00:20 +0000

    Changing version to 8.17
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@2388 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler/src/site/resources/examples/user-defined-query/user-defined-query.sh
+++ schemacrawler/src/site/resources/examples/user-defined-query/user-defined-query.sh
@@ -1,1 +1,1 @@
-java -classpath ../_schemacrawler/lib/schemacrawler-8.16.jar:../_schemacrawler/lib/hsqldb-2.2.8.jar schemacrawler.Main -c hsqldb -infolevel=standard -command=tables.select -outputformat=csv
+java -classpath ../_schemacrawler/lib/schemacrawler-8.17.jar:../_schemacrawler/lib/hsqldb-2.2.8.jar schemacrawler.Main -c hsqldb -infolevel=standard -command=tables.select -outputformat=csv
