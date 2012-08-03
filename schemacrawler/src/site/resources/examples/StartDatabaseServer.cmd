58660bc9429ec8e39cde417e95ba9ae8c2f318ec
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2012-08-03 02:30:48 +0000

    - Version # - 9.0 - Updating sample output
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@2400 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler/src/site/resources/examples/StartDatabaseServer.cmd
+++ schemacrawler/src/site/resources/examples/StartDatabaseServer.cmd
@@ -1,3 +1,3 @@
 @echo off
 color 1F
-java -cp _schemacrawler\lib\hsqldb-2.2.8.jar;_schemacrawler\lib\schemacrawler-8.17.jar;_schemacrawler\lib\schemacrawler-api-8.17-tests.jar schemacrawler.test.utility.TestDatabase
+java -cp _schemacrawler\lib\hsqldb-2.2.8.jar;_schemacrawler\lib\schemacrawler-9.0.jar;_schemacrawler\lib\schemacrawler-api-9.0-tests.jar schemacrawler.test.utility.TestDatabase
