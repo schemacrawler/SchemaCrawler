3ed15b2418965180f63847928281b7e0e57a3b0d
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2012-08-06 03:25:33 +0000

    Fixing examples
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@2407 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler/src/site/resources/examples/db-specific-query/db-specific-query.cmd
+++ schemacrawler/src/site/resources/examples/db-specific-query/db-specific-query.cmd
@@ -1,1 +1,2 @@
-@java -classpath ..\_schemacrawler\lib\schemacrawler-9.0.jar;..\_schemacrawler\lib\hsqldb-2.2.8.jar schemacrawler.Main -c hsqldb -infolevel=standard -command=hsqldb.tables
+@echo off
+..\_schemacrawler\sc.cmd -database=schemacrawler -user=sa -password= -infolevel=standard -command=hsqldb.tables
--- schemacrawler/src/site/resources/examples/dump/dump.cmd
+++ schemacrawler/src/site/resources/examples/dump/dump.cmd
@@ -1,4 +1,4 @@
 @echo off
 del /q /f database-dump.html
-..\_schemacrawler\sc.cmd -database=schemacrawler -user=sa -password= -infolevel=standard -command=count,dump -outputformat=html -outputfile=database-dump.html
+..\_schemacrawler\sc.cmd -database=schemacrawler -user=sa -password= -infolevel=standard -command=count,dump -outputformat=html -outputfile=database-dump.html %1
 echo Database dump is in database-dump.html
