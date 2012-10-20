6cfe35b7e6cce01bf1537fca27375e74ae473e24
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2012-10-20 00:22:29 +0000

    Changing version number to 9.2
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@2486 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler/src/site/resources/examples/StartDatabaseServer.cmd
+++ schemacrawler/src/site/resources/examples/StartDatabaseServer.cmd
@@ -1,3 +1,3 @@
 @echo off
 color 1F
-java -cp _schemacrawler\lib\hsqldb-2.2.8.jar;_schemacrawler\lib\schemacrawler-9.1.jar;_schemacrawler\lib\schemacrawler-api-9.1-tests.jar schemacrawler.test.utility.TestDatabase
+java -cp _schemacrawler/lib/*;. schemacrawler.test.utility.TestDatabase
--- schemacrawler/src/site/resources/examples/StartDatabaseServer.sh
+++ schemacrawler/src/site/resources/examples/StartDatabaseServer.sh
@@ -1,1 +1,1 @@
-java -cp _schemacrawler/lib/hsqldb-2.2.8.jar:_schemacrawler/lib/schemacrawler-9.1.jar:_schemacrawler/lib/schemacrawler-api-9.1-tests.jar schemacrawler.test.utility.TestDatabase
+java -cp _schemacrawler/lib/*:. schemacrawler.test.utility.TestDatabase
