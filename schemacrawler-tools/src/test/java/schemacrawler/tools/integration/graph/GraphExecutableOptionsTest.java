d8294ff8a2317a1c9528f7798a9eb5b9419c03c0
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2013-01-31 04:45:12 +0000

    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@2569 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler-tools/src/test/java/schemacrawler/tools/integration/graph/GraphExecutableOptionsTest.java
+++ schemacrawler-tools/src/test/java/schemacrawler/tools/integration/graph/GraphExecutableOptionsTest.java
@@ -159,7 +159,8 @@ public class GraphExecutableOptionsTest
     final File referenceDotFile = TestUtility
       .copyResourceToTempFile("/" + GRAPH_OPTIONS_OUTPUT + referenceFileName
                               + ".dot");
-    final GraphGenerator graphGenerator = new GraphGenerator(referenceDotFile,
+    final GraphGenerator graphGenerator = new GraphGenerator(graphOptions.getGraphVizOpts(),
+                                                             referenceDotFile,
                                                              "png",
                                                              testDiagramFile);
     graphGenerator.generateDiagram();
