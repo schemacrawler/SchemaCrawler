602cf776e0fd815955b6505ef7b6fe3e8052f39d
sfatehi <sfatehi@3243f2e7-4e1b-42d2-9742-d984016aa316>
2013-03-24 18:42:33 +0000

    - Taking show qualified options into account when showing graph - Muted SchemaCrawler information box in graph
    git-svn-id: http://svn.code.sf.net/p/schemacrawler/code/trunk@2645 3243f2e7-4e1b-42d2-9742-d984016aa316

--- schemacrawler-tools/src/main/java/schemacrawler/tools/text/base/BaseDotFormatter.java
+++ schemacrawler-tools/src/main/java/schemacrawler/tools/text/base/BaseDotFormatter.java
@@ -1,21 +1,22 @@
-/* 
- *
+/*
  * SchemaCrawler
  * http://sourceforge.net/projects/schemacrawler
  * Copyright (c) 2000-2013, Sualeh Fatehi.
- *
- * This library is free software; you can redistribute it and/or modify it under the terms
- * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms
+ * of the GNU Lesser General Public License as published by the Free Software
+ * Foundation;
  * either version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along with this
- * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330,
  * Boston, MA 02111-1307, USA.
- *
  */
 
 package schemacrawler.tools.text.base;
@@ -150,9 +151,12 @@ public abstract class BaseDotFormatter<O extends BaseTextOptions>
     {
       return;
     }
-    out.append("  graph [").append(NEWLINE).append("    label=<")
+    out
+      .append("  graph [fontcolor=\"#555555\", ")
       .append(NEWLINE)
-      .append("<table border=\"1\" cellborder=\"0\" cellspacing=\"0\">")
+      .append("    label=<")
+      .append(NEWLINE)
+      .append("<table color=\"#555555\" border=\"1\" cellborder=\"0\" cellspacing=\"0\">")
       .append(NEWLINE);
   }
 }
