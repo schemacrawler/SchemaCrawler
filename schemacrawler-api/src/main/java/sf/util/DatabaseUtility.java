4ba7653ca7b009bbdc55945a944adb2fc409776e
Sualeh Fatehi <sualeh@hotmail.com>
2014-03-31 23:09:34 -0400

    Centralizing code to execute SQL

---
+++ schemacrawler-api/src/main/java/sf/util/DatabaseUtility.java
@@ -0,0 +1,141 @@
+/*
+ *
+ * SchemaCrawler
+ * http://sourceforge.net/projects/schemacrawler
+ * Copyright (c) 2000-2014, Sualeh Fatehi.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+package sf.util;
+
+
+import static sf.util.Utility.isBlank;
+import static sf.util.Utility.readResourceFully;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLWarning;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import schemacrawler.schemacrawler.SchemaCrawlerException;
+
+/**
+ * Utility methods.
+ *
+ * @author Sualeh Fatehi
+ */
+public final class DatabaseUtility
+{
+
+  private static final Logger LOGGER = Logger.getLogger(DatabaseUtility.class
+    .getName());
+
+  public static Statement createStatement(final Connection connection)
+    throws SchemaCrawlerException, SQLException
+  {
+    if (connection == null)
+    {
+      throw new SchemaCrawlerException("No connection provided");
+    }
+    if (connection.isClosed())
+    {
+      throw new SchemaCrawlerException("Connection is closed");
+    }
+
+    return connection.createStatement();
+  }
+
+  public static void executeScriptFromResource(final String scriptResource,
+                                               final Connection connection)
+    throws SchemaCrawlerException
+  {
+    try (final Statement statement = createStatement(connection);)
+    {
+      final String sqlScript = readResourceFully(scriptResource);
+      if (!isBlank(sqlScript))
+      {
+        for (final String sql: sqlScript.split(";"))
+        {
+          final ResultSet resultSet = executeSql(statement, sql);
+          if (resultSet != null)
+          {
+            LOGGER.log(Level.WARNING, "Ignoring results from query: " + sql);
+            resultSet.close();
+          }
+        }
+      }
+    }
+    catch (final SQLException e)
+    {
+      System.err.println(e.getMessage());
+      LOGGER.log(Level.WARNING, e.getMessage(), e);
+    }
+  }
+
+  public static ResultSet executeSql(final Statement statement, final String sql)
+    throws SchemaCrawlerException
+  {
+    ResultSet results = null;
+    if (statement == null)
+    {
+      return results;
+    }
+    if (isBlank(sql))
+    {
+      LOGGER.log(Level.FINE, "No SQL provided", new RuntimeException());
+      return results;
+    }
+
+    try
+    {
+      statement.clearWarnings();
+
+      final boolean hasResults = statement.execute(sql);
+      if (hasResults)
+      {
+        results = statement.getResultSet();
+      }
+      else
+      {
+        final int updateCount = statement.getUpdateCount();
+        LOGGER.log(Level.FINE, String
+          .format("No results. Update count of %d for query: ",
+                  updateCount,
+                  sql));
+      }
+
+      SQLWarning sqlWarning = statement.getWarnings();
+      while (sqlWarning != null)
+      {
+        LOGGER.log(Level.INFO, sqlWarning.getMessage(), sqlWarning);
+        sqlWarning = sqlWarning.getNextWarning();
+      }
+
+      return results;
+    }
+    catch (final SQLException e)
+    {
+      LOGGER.log(Level.WARNING, "Error executing: " + sql, e);
+      return null;
+    }
+  }
+
+  private DatabaseUtility()
+  { // Prevent instantiation
+  }
+
+}
