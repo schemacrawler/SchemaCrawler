6f56750e6c2d872cb29af595d620907f66a30dd2
Sualeh Fatehi <sualeh@hotmail.com>
2014-04-12 17:03:58 -0400

    Adding offline snapshot options

---
+++ schemacrawler-offline/src/main/java/schemacrawler/tools/offline/OfflineSnapshotOptionsParser.java
@@ -0,0 +1,72 @@
+/* 
+ *
+ * SchemaCrawler
+ * http://sourceforge.net/projects/schemacrawler
+ * Copyright (c) 2000-2014, Sualeh Fatehi.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+package schemacrawler.tools.offline;
+
+
+import java.io.File;
+
+import schemacrawler.schemacrawler.Config;
+import schemacrawler.tools.commandline.BaseOptionsParser;
+import schemacrawler.tools.options.OutputFormat;
+import sf.util.Utility;
+import sf.util.clparser.StringOption;
+
+/**
+ * Parses the command line.
+ * 
+ * @author Sualeh Fatehi
+ */
+public final class OfflineSnapshotOptionsParser
+  extends BaseOptionsParser<OfflineSnapshotOptions>
+{
+
+  final OfflineSnapshotOptions outputOptions;
+
+  public OfflineSnapshotOptionsParser(final Config config)
+  {
+    super(new StringOption("outputformat", OutputFormat.text.toString()),
+          new StringOption('o', "outputfile", ""));
+
+    outputOptions = new OfflineSnapshotOptions(config);
+  }
+
+  @Override
+  public OfflineSnapshotOptions getOptions()
+  {
+    final String outputFormatValue = getStringValue("outputformat");
+    outputOptions.setOutputFormatValue(outputFormatValue);
+
+    final String outputFileName = getStringValue("outputfile");
+    final File outputFile;
+    if (Utility.isBlank(outputFileName))
+    {
+      outputFile = null;
+    }
+    else
+    {
+      outputFile = new File(outputFileName);
+    }
+    outputOptions.setOutputFile(outputFile);
+
+    return outputOptions;
+  }
+
+}
