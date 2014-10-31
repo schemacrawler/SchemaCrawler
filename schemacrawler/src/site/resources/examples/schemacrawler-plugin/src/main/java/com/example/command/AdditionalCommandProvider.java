31f8fc1f3d1701e57615b600b64a64e1f55a19ff
Sualeh Fatehi <sualeh@hotmail.com>
2014-10-30 20:13:28 -0400

    Fix plugin sample code

--- schemacrawler/src/site/resources/examples/schemacrawler-plugin/src/main/java/com/example/command/AdditionalCommandProvider.java
+++ schemacrawler/src/site/resources/examples/schemacrawler-plugin/src/main/java/com/example/command/AdditionalCommandProvider.java
@@ -1,12 +1,30 @@
 package com.example.command;
 
 
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
 import schemacrawler.tools.executable.CommandProvider;
 import schemacrawler.tools.executable.Executable;
+import schemacrawler.tools.options.OutputOptions;
 
 public class AdditionalCommandProvider
   implements CommandProvider
 {
+
+  @Override
+  public Executable configureNewExecutable(final SchemaCrawlerOptions schemaCrawlerOptions,
+                                           final OutputOptions outputOptions)
+  {
+    final AdditionalExecutable executable = new AdditionalExecutable();
+    if (schemaCrawlerOptions != null)
+    {
+      executable.setSchemaCrawlerOptions(schemaCrawlerOptions);
+    }
+    if (outputOptions != null)
+    {
+      executable.setOutputOptions(outputOptions);
+    }
+    return executable;
+  }
 
   @Override
   public String getCommand()
@@ -18,12 +36,6 @@ public class AdditionalCommandProvider
   public String getHelpResource()
   {
     return "/help/AdditionalCommandProvider.txt";
-  }
-
-  @Override
-  public Executable newExecutable()
-  {
-    return new AdditionalExecutable();
   }
 
 }
--- schemacrawler/src/site/resources/examples/schemacrawler-plugin/src/main/java/com/example/command/AdditionalExecutable.java
+++ schemacrawler/src/site/resources/examples/schemacrawler-plugin/src/main/java/com/example/command/AdditionalExecutable.java
@@ -1,33 +1,37 @@
 package com.example.command;
 
+
 import java.io.PrintWriter;
 import java.sql.Connection;
 
+import schemacrawler.schema.Catalog;
 import schemacrawler.schema.Column;
-import schemacrawler.schema.Database;
 import schemacrawler.schema.Schema;
 import schemacrawler.schema.Table;
-import schemacrawler.tools.executable.BaseExecutable;
+import schemacrawler.tools.executable.BaseStagedExecutable;
 import schemacrawler.tools.options.OutputWriter;
 
-public class AdditionalExecutable extends BaseExecutable {
+public class AdditionalExecutable
+  extends BaseStagedExecutable
+{
 
-	static final String COMMAND = "additional";
+  static final String COMMAND = "additional";
 
-	protected AdditionalExecutable() {
-		super(COMMAND);
-	}
+  protected AdditionalExecutable()
+  {
+    super(COMMAND);
+  }
 
-	@Override
-  protected void executeOn(final Database database, final Connection connection)
+  @Override
+  public void executeOn(final Catalog catalog, final Connection connection)
     throws Exception
   {
     try (final PrintWriter writer = new PrintWriter(new OutputWriter(outputOptions));)
     {
-      for (final Schema schema: database.getSchemas())
+      for (final Schema schema: catalog.getSchemas())
       {
         System.out.println(schema);
-        for (final Table table: schema.getTables())
+        for (final Table table: catalog.getTables(schema))
         {
           writer.println("o--> " + table);
           for (final Column column: table.getColumns())
@@ -38,4 +42,5 @@ public class AdditionalExecutable extends BaseExecutable {
       }
     }
   }
+
 }
