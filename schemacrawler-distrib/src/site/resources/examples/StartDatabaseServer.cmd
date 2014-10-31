038c6a717e0a0cb865ba626bd578f2183f1bc196
Sualeh Fatehi <sualeh@hotmail.com>
2014-10-30 21:22:49 -0400

    Renaming distribution project, so the artifact id does not clash with the constructed superjar

---
+++ schemacrawler-distrib/src/site/resources/examples/StartDatabaseServer.cmd
@@ -0,0 +1,3 @@
+@echo off
+color 1F
+java -cp _schemacrawler/lib/*;. schemacrawler.test.utility.TestDatabase
---
+++ schemacrawler-distrib/src/site/resources/examples/ruby/ruby.cmd
@@ -0,0 +1,2 @@
+@echo off
+java -classpath ../_schemacrawler/lib/*;lib/* schemacrawler.tools.hsqldb.Main -database=schemacrawler -user=sa -password= -infolevel=standard -command script -infolevel=maximum -sorttables=false -outputformat %1
