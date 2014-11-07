1d2b6704fc02ae6d179483ad2fa528330714d4df
Sualeh Fatehi <sualeh@hotmail.com>
2014-11-07 16:21:26 -0500

    Completing single distribution

--- schemacrawler-distrib/src/site/resources/examples/commandline/sc.sh
+++ schemacrawler-distrib/src/site/resources/examples/commandline/sc.sh
@@ -1,1 +1,1 @@
-java -classpath ../_schemacrawler/lib/*:lib/* schemacrawler.tools.hsqldb.Main -server=hsqldb -server=hsqldb -database=schemacrawler -user=sa -password= $*
+java -classpath ../_schemacrawler/lib/*:lib/* schemacrawler.Main -server=hsqldb -server=hsqldb -database=schemacrawler -user=sa -password= $*
