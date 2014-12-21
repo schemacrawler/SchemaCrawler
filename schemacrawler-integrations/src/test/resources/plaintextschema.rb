f294c207fd6723d12d5b004f93370565039085fd
Sualeh Fatehi <sualeh@hotmail.com>
2014-12-21 12:24:59 -0500

    Fixing Ruby tests

--- schemacrawler-integrations/src/test/resources/plaintextschema.rb
+++ schemacrawler-integrations/src/test/resources/plaintextschema.rb
@@ -6,6 +6,11 @@ puts ''
 puts $catalog.jdbcDriverInfo
 
 for table in $catalog.tables
+  $stderr.puts table.schema
+  if table.schema.to_s == "PUBLIC.FOR_LINT"
+    next
+  end
+  $stderr.puts table.fullName
   puts ''
   puts table.fullName
   for column in table.columns
