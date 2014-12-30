d9f2fb35788bb52bab10771a84109eccaa1a106a
Sualeh Fatehi <sualeh@hotmail.com>
2014-12-30 16:52:30 -0500

    Adding foreign language table name test for H2 and Derby

---
+++ schemacrawler-hsqldb/src/test/java/schemacrawler/integration/test/HsqldbTest.java
@@ -0,0 +1,76 @@
+package schemacrawler.integration.test;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import schemacrawler.schemacrawler.RegularExpressionInclusionRule;
+import schemacrawler.schemacrawler.SchemaCrawlerException;
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+import schemacrawler.test.utility.BaseExecutableTest;
+import schemacrawler.tools.executable.SchemaCrawlerExecutable;
+import schemacrawler.tools.text.schema.SchemaTextOptions;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:hsqldb-context.xml")
+public class HsqldbTest
+  extends BaseExecutableTest
+{
+
+  @Autowired
+  private DataSource dataSource;
+
+  @Test
+  public void testDataSource()
+    throws Exception
+  {
+    assertNotNull(dataSource);
+    final Connection connection = getConnection();
+    assertNotNull(connection);
+    assertEquals("org.hsqldb.jdbc.JDBCConnection", connection.getClass()
+      .getName());
+  }
+
+  @Test
+  public void testHsqldbWithConnection()
+    throws Exception
+  {
+    final SchemaCrawlerOptions options = new SchemaCrawlerOptions();
+    options
+      .setSchemaInclusionRule(new RegularExpressionInclusionRule("PUBLIC\\.BOOKS"));
+    final SchemaTextOptions textOptions = new SchemaTextOptions();
+    textOptions.setHideIndexNames(true);
+
+    final SchemaCrawlerExecutable executable = new SchemaCrawlerExecutable("details");
+    executable.setSchemaCrawlerOptions(options);
+    executable.setAdditionalConfiguration(textOptions.toConfig());
+
+    executeExecutable(executable, "text", "testHsqldbWithConnection.txt");
+  }
+
+  @Override
+  protected Connection getConnection()
+    throws SchemaCrawlerException
+  {
+    try
+    {
+      return dataSource.getConnection();
+    }
+    catch (final SQLException e)
+    {
+      throw new SchemaCrawlerException(e.getMessage(), e);
+    }
+  }
+
+}
