9efd67b6e6d69aa7264fc40bc3ff4db39c6c8e84
Sualeh Fatehi <sualeh@hotmail.com>
2015-03-13 18:07:36 -0400

    Weak associations

---
+++ schemacrawler-tools/src/test/java/schemacrawler/test/WeakAssociationsTest.java
@@ -0,0 +1,82 @@
+/*
+ * SchemaCrawler
+ * Copyright (c) 2000-2015, Sualeh Fatehi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package schemacrawler.test;
+
+
+import static org.junit.Assert.assertEquals;
+import static schemacrawler.test.utility.TestUtility.currentMethodFullName;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+
+import schemacrawler.schema.Catalog;
+import schemacrawler.schema.ColumnReference;
+import schemacrawler.schema.Schema;
+import schemacrawler.schema.Table;
+import schemacrawler.schemacrawler.RegularExpressionExclusionRule;
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+import schemacrawler.schemacrawler.SchemaInfoLevel;
+import schemacrawler.test.utility.BaseDatabaseTest;
+import schemacrawler.test.utility.TestWriter;
+import schemacrawler.tools.analysis.associations.CatalogWithAssociations;
+import schemacrawler.utility.NamedObjectSort;
+
+public class WeakAssociationsTest
+  extends BaseDatabaseTest
+{
+
+  @Test
+  public void weakAssociations()
+    throws Exception
+  {
+    try (final TestWriter out = new TestWriter("text");)
+    {
+
+      final SchemaCrawlerOptions schemaCrawlerOptions = new SchemaCrawlerOptions();
+      schemaCrawlerOptions.setSchemaInfoLevel(SchemaInfoLevel.maximum());
+      schemaCrawlerOptions
+        .setSchemaInclusionRule(new RegularExpressionExclusionRule(".*\\.FOR_LINT"));
+
+      final Catalog baseCatalog = getCatalog(schemaCrawlerOptions);
+      final CatalogWithAssociations catalog = new CatalogWithAssociations(baseCatalog);
+      final Schema[] schemas = catalog.getSchemas().toArray(new Schema[0]);
+      assertEquals("Schema count does not match", 5, schemas.length);
+      for (final Schema schema: schemas)
+      {
+        out.println("schema: " + schema.getFullName());
+        final Table[] tables = catalog.getTables(schema).toArray(new Table[0]);
+        Arrays.sort(tables, NamedObjectSort.alphabetical);
+        for (final Table table: tables)
+        {
+          out.println("  table: " + table.getFullName());
+          final Collection<ColumnReference> weakAssociations = CatalogWithAssociations
+            .getWeakAssociations(table);
+          for (final ColumnReference weakAssociation: weakAssociations)
+          {
+            out.println("    weak association: " + weakAssociation);
+          }
+        }
+      }
+
+      out.assertEquals(currentMethodFullName());
+    }
+  }
+
+}
