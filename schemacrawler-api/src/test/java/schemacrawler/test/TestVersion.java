af5dc48bdcca4a026af60b29709efe70a1c8ec6a
Sualeh Fatehi <sualeh@hotmail.com>
2015-08-05 22:44:40 -0400

    Removing unecessary code

---
+++ schemacrawler-api/src/test/java/schemacrawler/test/TestVersion.java
@@ -0,0 +1,41 @@
+package schemacrawler.test;
+
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import schemacrawler.Version;
+
+public class TestVersion
+{
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+  @Before
+  public void setUpStreams()
+  {
+    System.setOut(new PrintStream(outContent));
+    System.setErr(new PrintStream(errContent));
+  }
+
+  @After
+  public void cleanUpStreams()
+  {
+    System.setOut(null);
+    System.setErr(null);
+  }
+
+  @Test
+  public void version()
+  {
+    Version.main(new String[0]);
+    assertTrue(outContent.toString().startsWith("SchemaCrawler 14.01.02"));
+  }
+
+}
