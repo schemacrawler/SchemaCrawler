045f0967266b42ce60a18201e8adada8702a1924
Sualeh Fatehi <sualeh@hotmail.com>
2015-08-11 22:51:52 -0400

    Adding linter for "table with all nullable columns"

---
+++ schemacrawler-lint/src/main/java/schemacrawler/tools/linter/LinterTableAllNullableColumns.java
@@ -0,0 +1,68 @@
+/*
+ *
+ * SchemaCrawler
+ * http://www.schemacrawler.com
+ * Copyright (c) 2000-2015, Sualeh Fatehi.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+package schemacrawler.tools.linter;
+
+
+import static java.util.Objects.requireNonNull;
+
+import java.sql.Connection;
+import java.util.Collection;
+
+import schemacrawler.schema.Column;
+import schemacrawler.schema.Table;
+import schemacrawler.schema.View;
+import schemacrawler.tools.lint.BaseLinter;
+
+public class LinterTableAllNullableColumns
+  extends BaseLinter
+{
+
+  @Override
+  public String getSummary()
+  {
+    return "table with all nullable columns";
+  }
+
+  @Override
+  protected void lint(final Table table, final Connection connection)
+  {
+    requireNonNull(table, "No table provided");
+
+    if (!(table instanceof View) && hasAllNullableColumns(table.getColumns()))
+    {
+      addLint(table, getSummary(), table);
+    }
+  }
+
+  private boolean hasAllNullableColumns(final Collection<Column> columns)
+  {
+    boolean hasAllNullableColumns = true;
+    for (final Column column: columns)
+    {
+      if (!column.isPartOfPrimaryKey() && !column.isNullable())
+      {
+        hasAllNullableColumns = false;
+        break;
+      }
+    }
+    return hasAllNullableColumns;
+  }
+
+}
