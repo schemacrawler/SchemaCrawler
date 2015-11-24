efc838e9a191a146515e5c3f0039868dcd1bc6fa
Sualeh Fatehi <sualeh@hotmail.com>
2015-11-23 20:44:58 -0500

    Splitting out reserved words class

---
+++ schemacrawler-api/src/main/java/schemacrawler/utility/ReservedWords.java
@@ -0,0 +1,95 @@
+/*
+ *
+ * SchemaCrawler
+ * http://www.schemacrawler.com
+ * Copyright (c) 2000-2015, Sualeh Fatehi.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+package schemacrawler.utility;
+
+
+import static java.util.Objects.requireNonNull;
+import static sf.util.Utility.isBlank;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public final class ReservedWords
+{
+
+  private static final Logger LOGGER = Logger
+    .getLogger(ReservedWords.class.getName());
+
+  private static final Predicate<? super String> filter = word -> !isBlank(word);
+  private static final Function<String, String> map = word -> word.trim()
+    .toUpperCase();
+
+  private static Collection<String> loadSql2003ReservedWords()
+  {
+    final BufferedReader reader = new BufferedReader(new InputStreamReader(ReservedWords.class
+      .getResourceAsStream("/sql2003_reserved_words.txt")));
+
+    return reader.lines().filter(filter).map(map).collect(Collectors.toSet());
+  }
+
+  private static Collection<String> lookupReservedWords(final DatabaseMetaData metaData)
+  {
+    String sqlKeywords = "";
+    try
+    {
+      sqlKeywords = metaData.getSQLKeywords();
+    }
+    catch (final Exception e)
+    {
+      LOGGER.log(Level.WARNING, "Could not retrieve SQL keywords metadata", e);
+    }
+
+    return Stream.of(sqlKeywords.split(",")).filter(filter).map(map)
+      .collect(Collectors.toSet());
+  }
+
+  private final Collection<String> reservedWords;
+
+  public ReservedWords()
+  {
+    reservedWords = new HashSet<>();
+    reservedWords.addAll(loadSql2003ReservedWords());
+  }
+
+  public ReservedWords(final Connection connection)
+    throws SQLException
+  {
+    this();
+    requireNonNull(connection, "No connection provided");
+    reservedWords.addAll(lookupReservedWords(connection.getMetaData()));
+  }
+
+  public boolean contains(final String word)
+  {
+    return filter.test(word) && reservedWords.contains(map.apply(word));
+  }
+
+}
