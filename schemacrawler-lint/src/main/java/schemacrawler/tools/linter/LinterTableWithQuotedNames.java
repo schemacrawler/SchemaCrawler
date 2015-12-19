9ea83087fb5c0d971cd1b510603f21f39241b8b0
Sualeh Fatehi <sualeh@hotmail.com>
2015-12-18 22:46:01 -0500

    Turning the identifiers utility into a built with builder pattern

--- schemacrawler-lint/src/main/java/schemacrawler/tools/linter/LinterTableWithQuotedNames.java
+++ schemacrawler-lint/src/main/java/schemacrawler/tools/linter/LinterTableWithQuotedNames.java
@@ -52,7 +52,8 @@ public class LinterTableWithQuotedNames
     Identifiers identifiers;
     try
     {
-      identifiers = new Identifiers(connection);
+      identifiers = Identifiers.identifiers().withConnection(connection)
+        .build();
     }
     catch (final SQLException e)
     {
