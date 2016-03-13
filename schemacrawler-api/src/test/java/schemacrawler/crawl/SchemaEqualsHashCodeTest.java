a1bf1e37c8417cc6160860a168c0f2135ec32f8a
Sualeh Fatehi <sualeh@hotmail.com>
2016-03-13 12:03:46 -0400

    Trying to work with new version of equals verifier

--- schemacrawler-api/src/test/java/schemacrawler/crawl/SchemaEqualsHashCodeTest.java
+++ schemacrawler-api/src/test/java/schemacrawler/crawl/SchemaEqualsHashCodeTest.java
@@ -7,15 +7,20 @@ import org.junit.Test;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import schemacrawler.schema.SchemaReference;
 
-@Ignore
 public class SchemaEqualsHashCodeTest
 {
 
   @Test
-  public void equalsContract()
+  public void equalsContract1()
+  {
+    EqualsVerifier.forClass(SchemaReference.class).verify();
+  }
+
+  @Ignore
+  @Test
+  public void equalsContract2()
   {
     EqualsVerifier.forClass(MutableCatalog.class).verify();
-    EqualsVerifier.forClass(SchemaReference.class).verify();
     EqualsVerifier.forClass(MutableTable.class).verify();
     EqualsVerifier.forClass(MutablePrimaryKey.class).verify();
     EqualsVerifier.forClass(MutableColumn.class).verify();
