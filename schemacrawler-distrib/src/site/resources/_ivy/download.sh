ddbdb78367ee2e018cf52d37b948c8b3d6d95a60
Sualeh Fatehi <sualeh@hotmail.com>
2016-03-19 21:51:17 -0400

    Better examples of using the SchemaCrawler API in scripting languages Using Apache ivy 2.4.0

--- schemacrawler-distrib/src/site/resources/_ivy/download.sh
+++ schemacrawler-distrib/src/site/resources/_ivy/download.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar ivy-2.4.0-rc1.jar -ivy $1_ivy.xml -retrieve "../_schemacrawler/lib/[artifact]-[revision]-[type].[ext]"
+java -jar ivy-2.4.0.jar -ivy $1_ivy.xml -retrieve "../_schemacrawler/lib/[artifact]-[revision]-[type].[ext]"
