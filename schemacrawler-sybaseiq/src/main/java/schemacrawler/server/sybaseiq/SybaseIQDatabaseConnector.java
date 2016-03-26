1f1f7134f0ea0e8f636bff73cfddd5caa457cc80
Sualeh Fatehi <sualeh@hotmail.com>
2016-03-26 15:37:11 -0400

    Updating license

--- schemacrawler-sybaseiq/src/main/java/schemacrawler/server/sybaseiq/SybaseIQDatabaseConnector.java
+++ schemacrawler-sybaseiq/src/main/java/schemacrawler/server/sybaseiq/SybaseIQDatabaseConnector.java
@@ -1,22 +1,30 @@
 /*
- *
- * SchemaCrawler
- * http://www.schemacrawler.com
- * Copyright (c) 2000-2016, Sualeh Fatehi.
- *
- * This library is free software; you can redistribute it and/or modify it under the terms
- * of the GNU Lesser General Public License as published by the Free Software Foundation;
- * either version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along with this
- * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
- * Boston, MA 02111-1307, USA.
- *
- */
+========================================================================
+SchemaCrawler
+http://www.schemacrawler.com
+Copyright (c) 2000-2016, Sualeh Fatehi.
+All rights reserved.
+------------------------------------------------------------------------
+
+SchemaCrawler is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+SchemaCrawler and the accompanying materials are made available under
+the terms of the Eclipse Public License v1.0, GNU General Public License
+v3 or GNU Lesser General Public License v3.
+
+You may elect to redistribute this code under any of these licenses.
+
+The Eclipse Public License is available at:
+http://www.eclipse.org/legal/epl-v10.html
+
+The GNU General Public License v3 and the GNU Lesser General Public
+License v3 are available at:
+http://www.gnu.org/licenses/
+
+========================================================================
+*/
 package schemacrawler.server.sybaseiq;
 
 
