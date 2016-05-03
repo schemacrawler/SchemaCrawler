b6597b2971d402735e3680e3e5f430cd3f14bfa9
Sualeh Fatehi <sualeh@hotmail.com>
2016-05-02 21:47:42 -0400

    Changing version to 14.08.01

--- schemacrawler-api/src/test/java/schemacrawler/test/TestVersion.java
+++ schemacrawler-api/src/test/java/schemacrawler/test/TestVersion.java
@@ -62,7 +62,7 @@ public class TestVersion
   public void version()
   {
     Version.main(new String[0]);
-    assertTrue(outContent.toString().startsWith("SchemaCrawler 14.07.08"));
+    assertTrue(outContent.toString().startsWith("SchemaCrawler 14.08.01"));
   }
 
 }
