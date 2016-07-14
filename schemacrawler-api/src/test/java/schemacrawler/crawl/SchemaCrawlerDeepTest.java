04904350ccc6e16368d24cef9f79d3bae497f2b1
Sualeh Fatehi <sualeh@hotmail.com>
2016-07-13 20:32:01 -0400

    Code clean-up Fixes #79

--- schemacrawler-api/src/test/java/schemacrawler/crawl/SchemaCrawlerDeepTest.java
+++ schemacrawler-api/src/test/java/schemacrawler/crawl/SchemaCrawlerDeepTest.java
@@ -85,8 +85,8 @@ public class SchemaCrawlerDeepTest
                                                  "Test Table 2");
     final MutablePrimaryKey primaryKey = (MutablePrimaryKey) table0
       .getPrimaryKey();
-    table1.setPrimaryKey(primaryKey);
-    table2.setPrimaryKey(primaryKey);
+    table1.setPrimaryKeyAndReplaceIndex(primaryKey);
+    table2.setPrimaryKeyAndReplaceIndex(primaryKey);
     for (final Column column: table0.getColumns())
     {
       table1.addColumn((MutableColumn) column);
