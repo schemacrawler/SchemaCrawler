5865adbd56e06910383ab4d5795c58d8f94643e8
Sualeh Fatehi <sualeh@hotmail.com>
2016-08-11 23:07:57 -0400

    Adding more examples

--- schemacrawler-archetype-maven-project/src/main/resources/archetype-resources/src/main/java/ApiExample.java
+++ schemacrawler-archetype-maven-project/src/main/resources/archetype-resources/src/main/java/ApiExample.java
@@ -3,21 +3,26 @@
 #set( $symbol_escape = '\' )
 package ${package};
 
+import static us.fatehi.commandlineparser.CommandLineUtility.applyApplicationLogLevel;
+import static us.fatehi.commandlineparser.CommandLineUtility.logSystemProperties;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
 
 import javax.sql.DataSource;
 
-import schemacrawler.schema.Column;
 import schemacrawler.schema.Catalog;
+import schemacrawler.schema.Column;
 import schemacrawler.schema.Schema;
 import schemacrawler.schema.Table;
 import schemacrawler.schema.View;
 import schemacrawler.schemacrawler.DatabaseConnectionOptions;
 import schemacrawler.schemacrawler.ExcludeAll;
 import schemacrawler.schemacrawler.RegularExpressionInclusionRule;
+import schemacrawler.schemacrawler.SchemaCrawlerException;
 import schemacrawler.schemacrawler.SchemaCrawlerOptions;
-import schemacrawler.schemacrawler.SchemaInfoLevel;
+import schemacrawler.schemacrawler.SchemaInfoLevelBuilder;
 import schemacrawler.utility.SchemaCrawlerUtility;
 
 public final class ApiExample
@@ -26,23 +31,23 @@ public final class ApiExample
   public static void main(final String[] args)
     throws Exception
   {
-    // Create a database connection
-    final DataSource dataSource = new DatabaseConnectionOptions("org.hsqldb.jdbcDriver",
-                                                                "jdbc:hsqldb:hsql://localhost:9001/schemacrawler");
-    final Connection connection = dataSource.getConnection("sa", "");
+    // Set logging on
+    applyApplicationLogLevel(Level.ALL);
+    // Log system properties and classpath
+    logSystemProperties();
 
     // Create the options
     final SchemaCrawlerOptions options = new SchemaCrawlerOptions();
     // Set what details are required in the schema - this affects the
     // time taken to crawl the schema
-    options.setSchemaInfoLevel(SchemaInfoLevel.standard());
+    options.setSchemaInfoLevel(SchemaInfoLevelBuilder.standard());
     options.setRoutineInclusionRule(new ExcludeAll());
     options
       .setSchemaInclusionRule(new RegularExpressionInclusionRule("PUBLIC.BOOKS"));
 
     // Get the schema definition
-    final Catalog catalog = SchemaCrawlerUtility
-      .getCatalog(connection, options);
+    final Catalog catalog = SchemaCrawlerUtility.getCatalog(getConnection(),
+                                                            options);
 
     for (final Schema schema: catalog.getSchemas())
     {
@@ -67,6 +72,13 @@ public final class ApiExample
       }
     }
 
+  }
+
+  private static Connection getConnection()
+    throws SchemaCrawlerException, SQLException
+  {
+    final DataSource dataSource = new DatabaseConnectionOptions("jdbc:hsqldb:hsql://localhost:9001/schemacrawler");
+    return dataSource.getConnection("sa", "");
   }
 
 }
--- schemacrawler-distrib/src/site/markdown/examples/api/api-readme.md
+++ schemacrawler-distrib/src/site/markdown/examples/api/api-readme.md
@@ -8,9 +8,11 @@ alternative to using JDBC metadata.
 ## How to Run
 1. Make sure that java is on your PATH
 2. Start the test database server by running the StartDatabaseServer script from the distribution directory 
-3. Run `api.cmd` (or `api.sh` on Unix) to compile and run the program 
+3. Run `api.cmd` (or `api.sh` on Unix) to compile and run the first example 
+4. Run `executable.cmd` (or `executable.sh` on Unix) to compile and run the second example 
 
 ## How to Experiment
 1. Try uncommenting the code block in `ApiExample.java` that modifies the default options. 
 2. Read the [SchemaCrawler javadoc](http://www.schemacrawler.com/apidocs/index.html), and 
    edit `ApiExample.java` to print more details. 
+3. Make changes to `ExecutableExample.java` to produce different types of output.
