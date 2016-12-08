9ad9c6633010cea03ae4ab5facbbc9b0720e6799
Sualeh Fatehi <sualeh@hotmail.com>
2016-12-07 22:38:31 -0500

    Fixes #100

--- schemacrawler-diff/src/main/java/schemacrawler/tools/integration/objectdiffer/SchemaCrawlerDifferBuilder.java
+++ schemacrawler-diff/src/main/java/schemacrawler/tools/integration/objectdiffer/SchemaCrawlerDifferBuilder.java
@@ -30,43 +30,10 @@ package schemacrawler.tools.integration.objectdiffer;
 
 import de.danielbechler.diff.ObjectDiffer;
 import de.danielbechler.diff.ObjectDifferBuilder;
-import de.danielbechler.diff.comparison.ComparisonStrategy;
-import de.danielbechler.diff.node.DiffNode;
 import de.danielbechler.diff.node.DiffNode.State;
-import schemacrawler.schema.NamedObject;
 
 public class SchemaCrawlerDifferBuilder
 {
-
-  private final class NamedObjectComparisonStrategy
-    implements ComparisonStrategy
-  {
-    @Override
-    public void compare(final DiffNode node, final Class<?> type,
-                        final Object working, final Object base)
-    {
-      if (NamedObject.class.isAssignableFrom(type))
-      {
-        if (isEqual((NamedObject) working, (NamedObject) base))
-        {
-          node.setState(State.UNTOUCHED);
-        }
-        else
-        {
-          node.setState(State.CHANGED);
-        }
-      }
-    }
-
-    private boolean isEqual(final NamedObject working, final NamedObject base)
-    {
-      if (working != null && base != null)
-      {
-        return working.getName().equals(base.getName());
-      }
-      return false;
-    }
-  }
 
   final ObjectDifferBuilder objectDifferBuilder;
 
@@ -82,8 +49,6 @@ public class SchemaCrawlerDifferBuilder
       .propertyName("exportedForeignKeys");
     objectDifferBuilder.inclusion().exclude()
       .propertyName("importedForeignKeys");
-    objectDifferBuilder.comparison().ofType(NamedObject.class)
-      .toUse(new NamedObjectComparisonStrategy());
     objectDifferBuilder.inclusion().exclude().propertyName("deferrable");
     objectDifferBuilder.inclusion().exclude().propertyName("initiallyDeferred");
   }
