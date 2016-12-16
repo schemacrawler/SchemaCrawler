f8506b4dc4de6956091b3a041ff0c7afe22bec7d
Sualeh Fatehi <sualeh@hotmail.com>
2016-12-15 22:17:04 -0500

    Changing copyright to 2017

--- schemacrawler-lint/src/main/java/schemacrawler/tools/linter/LinterTableWithNoRemarks.java
+++ schemacrawler-lint/src/main/java/schemacrawler/tools/linter/LinterTableWithNoRemarks.java
@@ -2,7 +2,7 @@
 ========================================================================
 SchemaCrawler
 http://www.schemacrawler.com
-Copyright (c) 2000-2016, Sualeh Fatehi <sualeh@hotmail.com>.
+Copyright (c) 2000-2017, Sualeh Fatehi <sualeh@hotmail.com>.
 All rights reserved.
 ------------------------------------------------------------------------
 
