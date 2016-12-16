e5ee6d06730293d80160cd88c4e0f61160867d3f
Sualeh Fatehi <sualeh@hotmail.com>
2016-12-15 22:17:04 -0500

    Changing copyright to 2017

--- schemacrawler-examplecode/src/test/java/ExecutableExampleTest.java
+++ schemacrawler-examplecode/src/test/java/ExecutableExampleTest.java
@@ -3,7 +3,7 @@
 ========================================================================
 SchemaCrawler
 http://www.schemacrawler.com
-Copyright (c) 2000-2016, Sualeh Fatehi <sualeh@hotmail.com>.
+Copyright (c) 2000-2017, Sualeh Fatehi <sualeh@hotmail.com>.
 All rights reserved.
 ------------------------------------------------------------------------
 
