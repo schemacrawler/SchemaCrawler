6762cc030880ec1c505d4c7f328beec74341eee4
Sualeh Fatehi <sualeh@hotmail.com>
2017-02-23 23:14:34 -0500

    - Better error message

--- schemacrawler-tools/src/main/java/schemacrawler/tools/text/operation/OperationExecutable.java
+++ schemacrawler-tools/src/main/java/schemacrawler/tools/text/operation/OperationExecutable.java
@@ -130,7 +130,9 @@ public final class OperationExecutable
     }
     catch (final SQLException e)
     {
-      throw new SchemaCrawlerException("Cannot perform operation", e);
+      throw new SchemaCrawlerException(String.format("Unknown command \"%s\"",
+                                                     getCommand()),
+                                       e);
     }
   }
 
