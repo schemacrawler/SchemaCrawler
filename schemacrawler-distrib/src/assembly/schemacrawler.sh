42d80a716ffd73da47f67f58c21ddaf74e612bc5
David Guillot <david@guillot.me>
2017-03-01 14:50:00 +0100

    Shell script: made it totally path relative

--- schemacrawler-distrib/src/assembly/schemacrawler.sh
+++ schemacrawler-distrib/src/assembly/schemacrawler.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-java -cp $(echo lib/*.jar | tr ' ' ':'):config schemacrawler.Main "$@"
+
+DIR=$(dirname $(readlink -f "$0"))
+
+java -cp $(echo $DIR/lib/*.jar | tr ' ' ':'):config schemacrawler.Main "$@"
