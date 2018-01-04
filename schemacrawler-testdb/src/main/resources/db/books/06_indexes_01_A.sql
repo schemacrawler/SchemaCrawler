aa3cfee03bf423c1e024e22fab43564fe6a96a04
Sualeh Fatehi <sualeh@hotmail.com>
2018-01-04 00:34:53 -0500

    Ascending and descending index columns

--- schemacrawler-testdb/src/main/resources/db/books/06_indexes_01_A.sql
+++ schemacrawler-testdb/src/main/resources/db/books/06_indexes_01_A.sql
@@ -3,5 +3,5 @@ CREATE UNIQUE INDEX UIDX_BookAuthors ON BookAuthors(BookId, AuthorId)
 ;
 CREATE INDEX IDX_B_Authors ON Authors(LastName, FirstName)
 ;
-CREATE INDEX IDX_A_Authors ON Authors(City, State, PostalCode, Country)
+CREATE INDEX IDX_A_Authors ON Authors(City ASC, State DESC, PostalCode, Country)
 ;
