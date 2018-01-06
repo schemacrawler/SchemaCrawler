21bd2207b10c4aea1f101e002ca63d38980f98ea
Sualeh Fatehi <sualeh@hotmail.com>
2018-01-06 18:26:18 -0500

    Support for IBM DB2 test database schema

---
+++ schemacrawler-testdb/src/main/resources/db/books/05_tables_02_B.sql
@@ -0,0 +1,45 @@
+-- Table with default for a column, and a check constraint
+CREATE TABLE Authors
+(
+  Id INTEGER NOT NULL,
+  FirstName VARCHAR(20) NOT NULL,
+  LastName VARCHAR(20) NOT NULL,
+  Address1 VARCHAR(255),
+  Address2 VARCHAR(255),
+  City VARCHAR(50),
+  State VARCHAR(2),
+  PostalCode VARCHAR(10),
+  Country VARCHAR(50) DEFAULT 'USA',
+  CONSTRAINT PK_Authors PRIMARY KEY (Id),
+  CONSTRAINT CHECK_UPPERCASE_State CHECK (State=UPPER(State))
+)
+;
+
+-- Table with unique constraint, and self-referencing foreign key
+CREATE TABLE Books
+(
+  Id INTEGER NOT NULL,
+  Title VARCHAR(255) NOT NULL,
+  Description VARCHAR(255),
+  PublisherId INTEGER NOT NULL,
+  PublicationDate DATE,
+  Price FLOAT,
+  PreviousEditionId INTEGER NOT NULL DEFAULT 1,  
+  CONSTRAINT PK_Books PRIMARY KEY (Id),
+  CONSTRAINT FK_PreviousEdition FOREIGN KEY (PreviousEditionId) REFERENCES Books (Id),
+  CONSTRAINT U_PreviousEdition UNIQUE (PreviousEditionId)
+)
+;
+
+-- Table with a reserved word for a column name
+-- Contains unnamed foreign key
+-- Foreign keys have a different natural and alphabetical sort order
+CREATE TABLE BookAuthors
+(
+  BookId INTEGER NOT NULL,
+  AuthorId INTEGER NOT NULL,
+  SomeData VARCHAR(30),
+  FOREIGN KEY (BookId) REFERENCES Books (Id),
+  CONSTRAINT Z_FK_Author FOREIGN KEY (AuthorId) REFERENCES Authors (Id)
+)
+;
--- schemacrawler-testdb/src/main/resources/db/books/05_tables_04_A.sql
+++ schemacrawler-testdb/src/main/resources/db/books/05_tables_04_A.sql
@@ -1,4 +1,5 @@
 -- Table with special characters in the name
+-- Also, a foreign key where the data type does not match the primary key
 CREATE TABLE Βιβλία
 (
   Μοναδικός SMALLINT NOT NULL,
