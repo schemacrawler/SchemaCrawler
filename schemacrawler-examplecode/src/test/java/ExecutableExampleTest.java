55c49d33a9cf74a4947cd4a398399d64f1f2a7a3
Sualeh Fatehi <sualeh@hotmail.com>
2018-01-25 20:25:36 -0500

    Turn off excessive logging

--- schemacrawler-examplecode/src/test/java/ExecutableExampleTest.java
+++ schemacrawler-examplecode/src/test/java/ExecutableExampleTest.java
@@ -34,7 +34,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemErrRule;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 
 import schemacrawler.test.utility.BaseDatabaseTest;
 import schemacrawler.tools.options.TextOutputFormat;
@@ -43,6 +46,11 @@ public class ExecutableExampleTest
   extends BaseDatabaseTest
 {
 
+  @Rule
+  public final SystemErrRule sysErrLog = new SystemErrRule().enableLog().mute();
+  @Rule
+  public final SystemOutRule sysOutLog = new SystemOutRule().enableLog().mute();
+  
   @Test
   public void executableExample()
     throws Exception
