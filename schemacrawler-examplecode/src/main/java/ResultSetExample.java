6265d0532f05bc17aecb5a9af587fe36267ae492
Sualeh Fatehi <sualeh@hotmail.com>
2018-02-04 15:48:43 -0500

    Adding example code for result set metadata

---
+++ schemacrawler-examplecode/src/main/java/ResultSetExample.java
@@ -0,0 +1,61 @@
+import static us.fatehi.commandlineparser.CommandLineUtility.applyApplicationLogLevel;
+import static us.fatehi.commandlineparser.CommandLineUtility.logSystemProperties;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+
+import javax.sql.DataSource;
+
+import schemacrawler.schema.ResultsColumn;
+import schemacrawler.schema.ResultsColumns;
+import schemacrawler.schemacrawler.DatabaseConnectionOptions;
+import schemacrawler.schemacrawler.SchemaCrawlerException;
+import schemacrawler.utility.SchemaCrawlerUtility;
+
+public final class ResultSetExample
+{
+
+  public static void main(final String[] args)
+    throws Exception
+  {
+    // Turn application logging on by applying the correct log level
+    applyApplicationLogLevel(Level.OFF);
+    // Log system properties and classpath
+    logSystemProperties();
+
+    final String query = "SELECT                                                      \n"
+                         + "  BOOKS.TITLE AS BOOKTITLE,                               \n"
+                         + "  AUTHORS.FIRSTNAME + ' ' + AUTHORS.FIRSTNAME AS AUTHOR   \n"
+                         + "FROM                                                      \n"
+                         + "  PUBLIC.BOOKS.BOOKS AS BOOKS                             \n"
+                         + "  INNER JOIN PUBLIC.BOOKS.BOOKAUTHORS AS BOOKAUTHORS      \n"
+                         + "    ON BOOKS.ID = BOOKAUTHORS.BOOKID                      \n"
+                         + "  INNER JOIN PUBLIC.BOOKS.AUTHORS AS AUTHORS              \n"
+                         + "    ON BOOKAUTHORS.AUTHORID = AUTHORS.ID                  \n";
+    try (final Connection connection = getConnection();
+        final Statement statement = connection.createStatement();
+        final ResultSet results = statement.executeQuery(query);)
+    {
+      // Get result set metadata
+      final ResultsColumns resultColumns = SchemaCrawlerUtility
+        .getResultColumns(results);
+      for (final ResultsColumn column: resultColumns)
+      {
+        System.out
+          .println("o--> " + column + " (" + column.getColumnDataType() + ")");
+      }
+    }
+
+  }
+
+  private static Connection getConnection()
+    throws SchemaCrawlerException, SQLException
+  {
+    final DataSource dataSource = new DatabaseConnectionOptions("jdbc:hsqldb:hsql://localhost:9001/schemacrawler");
+    return dataSource.getConnection("sa", "");
+  }
+
+}
---
+++ schemacrawler-examplecode/src/test/java/ResultSetExampleTest.java
@@ -0,0 +1,84 @@
+
+/*
+========================================================================
+SchemaCrawler
+http://www.schemacrawler.com
+Copyright (c) 2000-2018, Sualeh Fatehi <sualeh@hotmail.com>.
+All rights reserved.
+------------------------------------------------------------------------
+
+SchemaCrawler is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+SchemaCrawler and the accompanying materials are made available under
+the terms of the Eclipse Public License v1.0, GNU General Public License
+v3 or GNU Lesser General Public License v3.
+
+You may elect to redistribute this code under any of these licenses.
+
+The Eclipse Public License is available at:
+http://www.eclipse.org/legal/epl-v10.html
+
+The GNU General Public License v3 and the GNU Lesser General Public
+License v3 are available at:
+http://www.gnu.org/licenses/
+
+========================================================================
+*/
+
+import static org.junit.Assert.fail;
+import static schemacrawler.test.utility.TestUtility.compareOutput;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemErrRule;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+
+import schemacrawler.test.utility.BaseDatabaseTest;
+import schemacrawler.tools.options.TextOutputFormat;
+
+public class ResultSetExampleTest
+  extends BaseDatabaseTest
+{
+
+  @Rule
+  public final SystemErrRule sysErrLog = new SystemErrRule().enableLog().mute();
+  @Rule
+  public final SystemOutRule sysOutLog = new SystemOutRule().enableLog().mute();
+
+  @Test
+  public void resultSetExample()
+    throws Exception
+  {
+    // Test
+    ResultSetExample.main(new String[0]);
+
+    checkSystemOutLog();
+  }
+
+  private void checkSystemOutLog()
+    throws IOException, Exception
+  {
+    final Path tempFile = Files.createTempFile("sc", ".txt");
+    Files.write(tempFile,
+                Arrays.asList(sysOutLog.getLogWithNormalizedLineSeparator()),
+                StandardOpenOption.WRITE);
+
+    final List<String> failures = compareOutput("ResultSetExample.txt",
+                                                tempFile,
+                                                TextOutputFormat.text.name());
+    if (failures.size() > 0)
+    {
+      fail(failures.toString());
+    }
+  }
+
+}
