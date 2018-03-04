3496dcf86da71ddec238ae27656f1158b9ecf2e9
Sualeh Fatehi <sualeh@hotmail.com>
2018-03-04 13:52:32 -0500

    Updates to CommandProvider

--- schemacrawler-archetype-plugin-command/src/main/resources/archetype-resources/src/main/java/AdditionalCommandProvider.java
+++ schemacrawler-archetype-plugin-command/src/main/resources/archetype-resources/src/main/java/AdditionalCommandProvider.java
@@ -4,9 +4,18 @@
 package ${package};
 
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.logging.Level;
+
 import schemacrawler.schemacrawler.SchemaCrawlerOptions;
 import schemacrawler.tools.executable.CommandProvider;
 import schemacrawler.tools.executable.Executable;
+import schemacrawler.tools.integration.freemarker.FreeMarkerRenderer;
+import schemacrawler.tools.iosource.ClasspathInputResource;
+import schemacrawler.tools.iosource.InputResource;
+import schemacrawler.tools.iosource.StringInputResource;
 import schemacrawler.tools.options.OutputOptions;
 
 public class AdditionalCommandProvider
@@ -14,9 +23,14 @@ public class AdditionalCommandProvider
 {
 
   @Override
-  public Executable configureNewExecutable(final SchemaCrawlerOptions schemaCrawlerOptions,
+  public Executable configureNewExecutable(final String command,
+                                           final SchemaCrawlerOptions schemaCrawlerOptions,
                                            final OutputOptions outputOptions)
   {
+    if (!AdditionalExecutable.COMMAND.equals(command)) {
+      throw new IllegalArgumentException("Cannot support command, " + command);
+    }
+    
     final AdditionalExecutable executable = new AdditionalExecutable();
     if (schemaCrawlerOptions != null)
     {
@@ -30,21 +44,31 @@ public class AdditionalCommandProvider
   }
 
   @Override
-  public String getCommand()
+  public InputResource getHelp()
   {
-    return AdditionalExecutable.COMMAND;
+    final String helpResource = "/help/AdditionalCommandProvider.txt";
+    try
+    {
+      return new ClasspathInputResource(helpResource);
+    }
+    catch (final IOException e)
+    {
+      // Log error...
+    }
+  }
+  
+  @Override
+  public Collection<String> getSupportedCommands()
+  {
+    return Arrays.asList(AdditionalExecutable.COMMAND);
   }
 
   @Override
-  public String getHelpAdditionalText()
+  public boolean supportsCommand(final String command,
+                                 final SchemaCrawlerOptions schemaCrawlerOptions,
+                                 final OutputOptions outputOptions)
   {
-    return "Some additional help text...";
-  }
-
-  @Override
-  public String getHelpResource()
-  {
-    return "/help/AdditionalCommandProvider.txt";
+    return AdditionalExecutable.COMMAND.equals(command);
   }
 
 }
