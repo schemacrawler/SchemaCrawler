031b0c5b550370d1abe6ec64a7688e9cafe9ed08
Sualeh Fatehi <sualeh@hotmail.com>
2018-07-08 14:15:40 -0400

    Fixing chain script

--- schemacrawler-distrib/src/examples/resources/examples/chain/chain.js
+++ schemacrawler-distrib/src/examples/resources/examples/chain/chain.js
@@ -2,8 +2,7 @@ var scCommands = function()
 {
 	chain.addNext("schema", "text", "schema.txt");
 	chain.addNext("schema", "png", "schema.png");
-	chain.executeOn(catalog, connection);
-	print('Created files "schema.txt" and "schema.png"');
+	chain.execute();
 };
 
 scCommands();
