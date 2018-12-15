173c12f9a9387f24f0618f3582cb6d0b71d2f02b
Sualeh Fatehi <sualeh@hotmail.com>
2018-12-14 19:14:51 -0500

    Add test to get PostgreSQL server info

--- schemacrawler-postgresql/src/test/java/schemacrawler/integration/test/PostgreSQLTest.java
+++ schemacrawler-postgresql/src/test/java/schemacrawler/integration/test/PostgreSQLTest.java
@@ -28,11 +28,17 @@ http://www.gnu.org/licenses/
 package schemacrawler.integration.test;
 
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assume.assumeTrue;
+import static sf.util.DatabaseUtility.checkConnection;
 import static sf.util.Utility.isBlank;
 
 import java.io.IOException;
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 
 import org.junit.After;
@@ -40,12 +46,18 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import schemacrawler.crawl.SchemaCrawler;
+import schemacrawler.schema.Catalog;
+import schemacrawler.schema.Property;
 import schemacrawler.schemacrawler.RegularExpressionInclusionRule;
 import schemacrawler.schemacrawler.SchemaCrawlerException;
 import schemacrawler.schemacrawler.SchemaCrawlerOptions;
 import schemacrawler.schemacrawler.SchemaCrawlerOptionsBuilder;
 import schemacrawler.schemacrawler.SchemaInfoLevelBuilder;
+import schemacrawler.schemacrawler.SchemaRetrievalOptions;
 import schemacrawler.server.postgresql.EmbeddedPostgreSQLWrapper;
+import schemacrawler.server.postgresql.PostgreSQLDatabaseConnector;
+import schemacrawler.tools.databaseconnector.DatabaseConnector;
 import schemacrawler.tools.executable.SchemaCrawlerExecutable;
 import schemacrawler.tools.text.schema.SchemaTextOptions;
 import schemacrawler.tools.text.schema.SchemaTextOptionsBuilder;
@@ -97,6 +109,46 @@ public class PostgreSQLTest
     {
       embeddedPostgreSQL.stopServer();
     }
+  }
+
+  @Test
+  public void testPostgreSQLCatalog()
+    throws Exception
+  {
+    if (!isDatabaseRunning)
+    {
+      LOGGER.log(Level.INFO, "Did NOT run PostgreSQL test");
+      return;
+    }
+
+    final SchemaCrawlerOptionsBuilder schemaCrawlerOptionsBuilder = SchemaCrawlerOptionsBuilder
+      .builder();
+    schemaCrawlerOptionsBuilder
+      .withSchemaInfoLevel(SchemaInfoLevelBuilder.maximum())
+      .includeSchemas(new RegularExpressionInclusionRule("books"))
+      .includeAllSequences().includeAllSynonyms().includeAllRoutines()
+      .tableTypes("TABLE,VIEW,MATERIALIZED VIEW");
+    final SchemaCrawlerOptions options = schemaCrawlerOptionsBuilder
+      .toOptions();
+
+    final Connection connection = checkConnection(getConnection());
+    final DatabaseConnector postgreSQLDatabaseConnector = new PostgreSQLDatabaseConnector();
+
+    final SchemaRetrievalOptions schemaRetrievalOptions = postgreSQLDatabaseConnector
+      .getSchemaRetrievalOptionsBuilder(connection).toOptions();
+
+    final SchemaCrawler schemaCrawler = new SchemaCrawler(getConnection(),
+                                                          schemaRetrievalOptions,
+                                                          options);
+    final Catalog catalog = schemaCrawler.crawl();
+    final List<Property> serverInfo = new ArrayList<>(catalog.getDatabaseInfo()
+      .getServerInfo());
+
+    assertThat(serverInfo.size(), equalTo(1));
+    assertThat(serverInfo.get(0).getName(), equalTo("current_database"));
+    assertThat(serverInfo.get(0).getValue(), equalTo("schemacrawler"));
+
+    LOGGER.log(Level.INFO, "Completed PostgreSQL catalog test successfully");
   }
 
   @Test
