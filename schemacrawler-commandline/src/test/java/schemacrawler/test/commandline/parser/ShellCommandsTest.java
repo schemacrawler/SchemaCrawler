9719a47445db022e0e6e55d13823ad394cbaf602
Sualeh Fatehi <sualeh@hotmail.com>
2019-04-20 00:05:23 -0400

    Add coverage

---
+++ schemacrawler-commandline/src/test/java/schemacrawler/test/commandline/parser/ShellCommandsTest.java
@@ -0,0 +1,109 @@
+/*
+========================================================================
+SchemaCrawler
+http://www.schemacrawler.com
+Copyright (c) 2000-2019, Sualeh Fatehi <sualeh@hotmail.com>.
+All rights reserved.
+------------------------------------------------------------------------
+
+SchemaCrawler is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+SchemaCrawler and the accompanying materials are made available under
+the terms of the Eclipse Public License v1.0, GNU General Public License
+v3 or GNU Lesser General Public License v3.
+
+You may elect to redistribute this code under any of these licenses.
+
+The Eclipse Public License is available at:
+http://www.eclipse.org/legal/epl-v10.html
+
+The GNU General Public License v3 and the GNU Lesser General Public
+License v3 are available at:
+http://www.gnu.org/licenses/
+
+========================================================================
+*/
+package schemacrawler.test.commandline.parser;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static schemacrawler.test.utility.FileHasContent.*;
+
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import schemacrawler.test.utility.TestContext;
+import schemacrawler.test.utility.TestContextParameterResolver;
+import schemacrawler.test.utility.TestOutputStream;
+import schemacrawler.tools.commandline.shellcommand.AvailableCommandsCommand;
+import schemacrawler.tools.commandline.shellcommand.AvailableServersCommand;
+import schemacrawler.tools.commandline.shellcommand.SystemCommand;
+
+@ExtendWith(TestContextParameterResolver.class)
+public class ShellCommandsTest
+{
+
+  private static final String SHELL_COMMANDS_OUTPUT = "shell_commands_output/";
+
+  private TestOutputStream out;
+  private TestOutputStream err;
+
+  @AfterEach
+  public void cleanUpStreams()
+  {
+    System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+    System.setErr(new PrintStream(new FileOutputStream(FileDescriptor.err)));
+  }
+
+  @Test
+  public void system(final TestContext testContext)
+  {
+    new SystemCommand().run();
+
+    assertThat(outputOf(err), hasNoContent());
+    assertThat(outputOf(out),
+               hasSameContentAs(classpathResource(
+                 SHELL_COMMANDS_OUTPUT + testContext.testMethodName()
+                 + ".stdout.txt")));
+  }
+
+  @Test
+  public void availableServers(final TestContext testContext)
+  {
+    new AvailableServersCommand().run();
+
+    assertThat(outputOf(err), hasNoContent());
+    assertThat(outputOf(out), hasNoContent());
+  }
+
+  @Test
+  public void availableCommands(final TestContext testContext)
+  {
+    new AvailableCommandsCommand().run();
+
+    assertThat(outputOf(err), hasNoContent());
+    assertThat(outputOf(out),
+               hasSameContentAs(classpathResource(
+                 SHELL_COMMANDS_OUTPUT + testContext.testMethodName()
+                 + ".stdout.txt")));
+  }
+
+  @BeforeEach
+  public void setUpStreams()
+    throws Exception
+  {
+    out = new TestOutputStream();
+    System.setOut(new PrintStream(out));
+
+    err = new TestOutputStream();
+    System.setErr(new PrintStream(err));
+  }
+
+}
