a5c7fff20c7d144635c45db7c2011a83ad975491
Sualeh Fatehi <sualeh@hotmail.com>
2019-04-21 00:26:05 -0400

    Code tweaks for shell

--- schemacrawler-commandline/src/main/java/schemacrawler/tools/commandline/SchemaCrawlerShell.java
+++ schemacrawler-commandline/src/main/java/schemacrawler/tools/commandline/SchemaCrawlerShell.java
@@ -1,3 +1,30 @@
+/*
+========================================================================
+SchemaCrawler
+http://www.schemacrawler.com
+Copyright (c) 2000-2019, Sualeh Fatehi <sualeh@hotmail.com>.
+All rights reserved.
+------------------------------------------------------------------------
+
+SchemaCrawler is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+SchemaCrawler and the accompanying materials are made available under
+the terms of the Eclipse Public License v1.0, GNU General Public License
+v3 or GNU Lesser General Public License v3.
+
+You may elect to redistribute this code under any of these licenses.
+
+The Eclipse Public License is available at:
+http://www.eclipse.org/legal/epl-v10.html
+
+The GNU General Public License v3 and the GNU Lesser General Public
+License v3 are available at:
+http://www.gnu.org/licenses/
+
+========================================================================
+*/
 package schemacrawler.tools.commandline;
 
 
@@ -35,12 +62,9 @@ public class SchemaCrawlerShell
           line = reader.readLine(prompt, null, (MaskingCallback) null, null);
           final ParsedLine pl = reader.getParser().parse(line, 0);
           final String[] arguments = pl.words().toArray(new String[0]);
-
-          final CommandLine currentCmd = new CommandLine(commands,
-                                                         stateFactory);
-          currentCmd.parseWithHandlers(new CommandLine.RunLast(),
-                                       new CommandLine.DefaultExceptionHandler(),
-                                       arguments);
+          cmd.parseWithHandlers(new CommandLine.RunLast(),
+                                new CommandLine.DefaultExceptionHandler(),
+                                arguments);
         }
         catch (final UserInterruptException e)
         {
