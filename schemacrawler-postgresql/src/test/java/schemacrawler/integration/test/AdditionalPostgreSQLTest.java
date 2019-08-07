6ab2887a08553c3548be3c6e397766385d869a8c
Sualeh Fatehi <sualeh@hotmail.com>
2019-08-07 18:09:34 -0400

    Framework to reproduce Issue #258

---
+++ schemacrawler-postgresql/src/test/java/schemacrawler/integration/test/AdditionalPostgreSQLTest.java
@@ -0,0 +1,153 @@
+/*
+========================================================================
+SchemaCrawler
+http://www.schemacrawler.com
+Copyright (c) 2000-2019, Sualeh Fatehi <sualeh@hotmail.com>.
+All rights reserved.
+------------------------------------------------------------------------
+
+SchemaCrawler is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+SchemaCrawler and the accompanying materials are made available under
+the terms of the Eclipse Public License v1.0, GNU General Public License
+v3 or GNU Lesser General Public License v3.
+
+You may elect to redistribute this code under any of these licenses.
+
+The Eclipse Public License is available at:
+http://www.eclipse.org/legal/epl-v10.html
+
+The GNU General Public License v3 and the GNU Lesser General Public
+License v3 are available at:
+http://www.gnu.org/licenses/
+
+========================================================================
+*/
+package schemacrawler.integration.test;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static sf.util.DatabaseUtility.checkConnection;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collection;
+import java.util.List;
+import java.util.logging.Level;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import schemacrawler.crawl.SchemaCrawler;
+import schemacrawler.schema.Catalog;
+import schemacrawler.schema.Column;
+import schemacrawler.schema.Table;
+import schemacrawler.schemacrawler.SchemaCrawlerException;
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+import schemacrawler.schemacrawler.SchemaCrawlerOptionsBuilder;
+import schemacrawler.schemacrawler.SchemaRetrievalOptions;
+import schemacrawler.server.postgresql.EmbeddedPostgreSQLWrapper;
+import schemacrawler.server.postgresql.PostgreSQLDatabaseConnector;
+import schemacrawler.tools.databaseconnector.DatabaseConnector;
+
+public class AdditionalPostgreSQLTest
+  extends BasePostgreSQLTest
+{
+
+  private boolean isDatabaseRunning;
+  private EmbeddedPostgreSQLWrapper embeddedPostgreSQL;
+
+  @BeforeEach
+  public void createDatabase()
+    throws SchemaCrawlerException, SQLException, IOException
+  {
+    try
+    {
+      embeddedPostgreSQL = new EmbeddedPostgreSQLWrapper(
+        getEmbeddedPostgreSQLVersion());
+      embeddedPostgreSQL.startServer();
+      createDataSource(embeddedPostgreSQL.getConnectionUrl(),
+                       embeddedPostgreSQL.getUser(),
+                       embeddedPostgreSQL.getPassword());
+
+      isDatabaseRunning = true;
+    }
+    catch (final Throwable e)
+    {
+      LOGGER.log(Level.FINE, e.getMessage(), e);
+      // Do not run if database server cannot be loaded
+      isDatabaseRunning = false;
+    }
+  }
+
+  @AfterEach
+  public void stopDatabaseServer()
+    throws SchemaCrawlerException
+  {
+    if (isDatabaseRunning)
+    {
+      embeddedPostgreSQL.stopServer();
+    }
+  }
+
+  @Test
+  public void testIssue258()
+    throws Exception
+  {
+    if (!isDatabaseRunning)
+    {
+      LOGGER.log(Level.INFO, "Did NOT run PostgreSQL test");
+      return;
+    }
+
+    try (final Connection connection = getConnection();
+      Statement stmt = connection.createStatement();)
+    {
+      stmt.execute("CREATE TABLE AIRCRAFT (NAME VARCHAR(100)) -- WITH OIDS");
+      stmt.execute("INSERT INTO AIRCRAFT VALUES ('Boeing 747')");
+      connection.commit();
+    }
+
+    final SchemaCrawlerOptionsBuilder schemaCrawlerOptionsBuilder = SchemaCrawlerOptionsBuilder
+      .builder();
+    final SchemaCrawlerOptions options = schemaCrawlerOptionsBuilder
+      .toOptions();
+
+    final Connection connection = checkConnection(getConnection());
+    final DatabaseConnector postgreSQLDatabaseConnector = new PostgreSQLDatabaseConnector();
+
+    final SchemaRetrievalOptions schemaRetrievalOptions = postgreSQLDatabaseConnector
+      .getSchemaRetrievalOptionsBuilder(connection).toOptions();
+
+    final SchemaCrawler schemaCrawler = new SchemaCrawler(getConnection(),
+                                                          schemaRetrievalOptions,
+                                                          options);
+    final Catalog catalog = schemaCrawler.crawl();
+
+    final Collection<Table> tables = catalog.getTables();
+    assertThat("Did not retrieve all tables", tables.size(), equalTo(1));
+
+    final Table table = tables.stream().findFirst().get();
+    assertThat("Got wrong table", table.getName(), equalToIgnoringCase("AIRCRAFT"));
+
+    final List<Column> columns = table.getColumns();
+    assertThat("Did not retrieve all table columns",
+               columns.size(),
+               equalTo(1));
+
+    final Column column = columns.stream().findFirst().get();
+    assertThat("Got wrong column", column.getName(), equalToIgnoringCase("NAME"));
+    assertThat("Got wrong column data type",
+               column.getColumnDataType().getDatabaseSpecificTypeName(),
+               equalToIgnoringCase("VARCHAR"));
+
+    LOGGER.log(Level.INFO, "Completed PostgreSQL catalog test successfully");
+  }
+
+}
