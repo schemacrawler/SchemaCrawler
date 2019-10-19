03689b2d345746211873f2893eb7149b8c5a5572
Sualeh Fatehi <sualeh@hotmail.com>
2019-10-19 22:35:43 +0530

    Fix Docker versions

--- schemacrawler-docker/schemacrawler-distribution-local.sh
+++ schemacrawler-docker/schemacrawler-distribution-local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-SCHEMACRAWLER_VERSION=16.2.1
+SCHEMACRAWLER_VERSION=16.2.2
 
 # Download SchemaCrawler distribution
 echo "** Obtaining SchemaCrawler v$SCHEMACRAWLER_VERSION distribution"
