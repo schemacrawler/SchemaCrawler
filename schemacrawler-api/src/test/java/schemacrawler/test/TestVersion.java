eb1e0d37cf0912cbf35f39557656b71f2ba29880
Sualeh Fatehi <sualeh@hotmail.com>
2020-01-16 19:55:54 -0500

    v16.3.1

--- schemacrawler-api/src/test/java/schemacrawler/test/TestVersion.java
+++ schemacrawler-api/src/test/java/schemacrawler/test/TestVersion.java
@@ -71,7 +71,7 @@ public class TestVersion
     throws Exception
   {
     Version.main(new String[0]);
-    assertThat(out.getFileContents(), startsWith("SchemaCrawler 16.3.0"));
+    assertThat(out.getFileContents(), startsWith("SchemaCrawler 16.3.1"));
     assertThat(outputOf(err), hasNoContent());
   }
 
