a68b6ff5a17abacb3b29c1f3dc6ae5518e98abd3
Sualeh Fatehi <sualeh@hotmail.com>
2020-01-28 21:04:15 -0500

    Add PostgreSQL test for enum values

---
+++ schemacrawler-postgresql/src/test/java/schemacrawler/integration/test/PostgreSQLEnumColumnTest.java
@@ -0,0 +1,119 @@
+/*
+========================================================================
+SchemaCrawler
+http://www.schemacrawler.com
+Copyright (c) 2000-2020, Sualeh Fatehi <sualeh@hotmail.com>.
+All rights reserved.
+------------------------------------------------------------------------
+
+SchemaCrawler is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+SchemaCrawler and the accompanying materials are made available under
+the terms of the Eclipse Public License v1.0, GNU General Public License
+v3 or GNU Lesser General Public License v3.
+
+You may elect to redistribute this code under any of these licenses.
+
+The Eclipse Public License is available at:
+http://www.eclipse.org/legal/epl-v10.html
+
+The GNU General Public License v3 and the GNU Lesser General Public
+License v3 are available at:
+http://www.gnu.org/licenses/
+
+========================================================================
+*/
+package schemacrawler.integration.test;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static schemacrawler.test.utility.ExecutableTestUtility.executableExecution;
+import static schemacrawler.test.utility.FileHasContent.classpathResource;
+import static schemacrawler.test.utility.FileHasContent.hasSameContentAs;
+import static schemacrawler.test.utility.FileHasContent.outputOf;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+import schemacrawler.schemacrawler.SchemaCrawlerOptionsBuilder;
+import schemacrawler.schemacrawler.SchemaInfoLevelBuilder;
+import schemacrawler.test.utility.BaseAdditionalDatabaseTest;
+import schemacrawler.test.utility.HeavyDatabaseBuildCondition;
+import schemacrawler.tools.executable.SchemaCrawlerExecutable;
+
+@Testcontainers(disabledWithoutDocker = true)
+@ExtendWith(HeavyDatabaseBuildCondition.class)
+@DisplayName("Test for issue #284 on GitHub")
+public class PostgreSQLEnumColumnTest
+  extends BaseAdditionalDatabaseTest
+{
+
+  @Container
+  private JdbcDatabaseContainer dbContainer =
+    new HeavyDatabaseBuildCondition().getJdbcDatabaseContainer(() -> new PostgreSQLContainer<>());
+
+  @BeforeEach
+  public void createDatabase()
+  {
+    createDataSource(dbContainer.getJdbcUrl(),
+                     dbContainer.getUsername(),
+                     dbContainer.getPassword());
+  }
+
+  @Test
+  public void columnWithEnum()
+    throws Exception
+  {
+    try (
+      final Connection connection = getConnection();
+      final Statement stmt = connection.createStatement();
+    )
+    {
+      stmt.execute(
+        "CREATE TYPE mood AS ENUM ('sad', 'ok', 'happy')");
+      stmt.execute(
+        "CREATE TABLE person (name text, current_mood mood)");
+      connection.commit();
+    }
+
+    final SchemaCrawlerOptionsBuilder schemaCrawlerOptionsBuilder =
+      SchemaCrawlerOptionsBuilder
+        .builder()
+        .withSchemaInfoLevel(SchemaInfoLevelBuilder.maximum());
+    final SchemaCrawlerOptions schemaCrawlerOptions =
+      schemaCrawlerOptionsBuilder.toOptions();
+
+    final SchemaCrawlerExecutable executable =
+      new SchemaCrawlerExecutable("details");
+    executable.setSchemaCrawlerOptions(schemaCrawlerOptions);
+
+    assertThat(outputOf(executableExecution(getConnection(), executable)),
+               hasSameContentAs(classpathResource(
+                 "testColumnWithEnum.txt")));
+
+/*
+    // Additional programmatic test
+    final Catalog catalog = executable.getCatalog();
+    final Schema schema = catalog
+      .lookupSchema("test")
+      .orElse(null);
+    assertThat(schema, notNullValue());
+    final Table table = catalog
+      .lookupTable(schema, "person")
+      .orElse(null);
+    assertThat(table, notNullValue());
+    */
+  }
+
+}
