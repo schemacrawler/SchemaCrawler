faea93a5b76c45911e06500763dff1f2dafaa289
Sualeh Fatehi <sualeh@hotmail.com>
2020-02-02 20:36:45 +0530

    Add MariaDB tests

---
+++ schemacrawler-mysql/src/test/java/schemacrawler/integration/test/MariaDBTest.java
@@ -0,0 +1,117 @@
+/*
+========================================================================
+SchemaCrawler
+http://www.schemacrawler.com
+Copyright (c) 2000-2020, Sualeh Fatehi <sualeh@hotmail.com>.
+All rights reserved.
+------------------------------------------------------------------------
+
+SchemaCrawler is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+SchemaCrawler and the accompanying materials are made available under
+the terms of the Eclipse Public License v1.0, GNU General Public License
+v3 or GNU Lesser General Public License v3.
+
+You may elect to redistribute this code under any of these licenses.
+
+The Eclipse Public License is available at:
+http://www.eclipse.org/legal/epl-v10.html
+
+The GNU General Public License v3 and the GNU Lesser General Public
+License v3 are available at:
+http://www.gnu.org/licenses/
+
+========================================================================
+*/
+package schemacrawler.integration.test;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static schemacrawler.test.utility.ExecutableTestUtility.executableExecution;
+import static schemacrawler.test.utility.FileHasContent.classpathResource;
+import static schemacrawler.test.utility.FileHasContent.hasSameContentAs;
+import static schemacrawler.test.utility.FileHasContent.outputOf;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import schemacrawler.schemacrawler.RegularExpressionInclusionRule;
+import schemacrawler.schemacrawler.SchemaCrawlerException;
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+import schemacrawler.schemacrawler.SchemaCrawlerOptionsBuilder;
+import schemacrawler.schemacrawler.SchemaInfoLevelBuilder;
+import schemacrawler.test.utility.BaseAdditionalDatabaseTest;
+import schemacrawler.test.utility.HeavyDatabaseBuildCondition;
+import schemacrawler.tools.executable.SchemaCrawlerExecutable;
+import schemacrawler.tools.text.schema.SchemaTextOptions;
+import schemacrawler.tools.text.schema.SchemaTextOptionsBuilder;
+
+@Testcontainers(disabledWithoutDocker = true)
+@ExtendWith(HeavyDatabaseBuildCondition.class)
+public class MariaDBTest
+  extends BaseAdditionalDatabaseTest
+{
+
+  @Container
+  private JdbcDatabaseContainer dbContainer =
+    new HeavyDatabaseBuildCondition().getJdbcDatabaseContainer(() -> new MariaDBContainer<>()
+      .withCommand("mysqld",
+                   "--lower_case_table_names=1",
+                   "--log_bin_trust_function_creators=1")
+      .withUsername("schemacrawler")
+      .withDatabaseName("books"));
+
+  @BeforeEach
+  public void createDatabase()
+    throws SQLException, SchemaCrawlerException
+  {
+    createDataSource(dbContainer.getJdbcUrl(),
+                     dbContainer.getUsername(),
+                     dbContainer.getPassword());
+
+    createDatabase("/mysql.scripts.txt");
+  }
+
+  @Test
+  public void testMariaDBWithConnection()
+    throws Exception
+  {
+    final SchemaCrawlerOptionsBuilder schemaCrawlerOptionsBuilder =
+      SchemaCrawlerOptionsBuilder.builder();
+    schemaCrawlerOptionsBuilder
+      .withSchemaInfoLevel(SchemaInfoLevelBuilder.maximum())
+      .includeSchemas(new RegularExpressionInclusionRule("books"))
+      .includeAllSequences()
+      .includeAllSynonyms()
+      .includeAllRoutines();
+    final SchemaCrawlerOptions options =
+      schemaCrawlerOptionsBuilder.toOptions();
+
+    final SchemaTextOptionsBuilder textOptionsBuilder =
+      SchemaTextOptionsBuilder.builder();
+    textOptionsBuilder
+      .showDatabaseInfo()
+      .showJdbcDriverInfo();
+    final SchemaTextOptions textOptions = textOptionsBuilder.toOptions();
+
+    final SchemaCrawlerExecutable executable =
+      new SchemaCrawlerExecutable("details");
+    executable.setSchemaCrawlerOptions(options);
+    executable.setAdditionalConfiguration(SchemaTextOptionsBuilder
+                                            .builder(textOptions)
+                                            .toConfig());
+
+    assertThat(outputOf(executableExecution(getConnection(), executable)),
+               hasSameContentAs(classpathResource("testMariaDBWithConnection.txt")));
+  }
+
+}
