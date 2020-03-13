22cefad470249d5df4032a8e78e1be3e35a812eb
Sualeh Fatehi <sualeh@hotmail.com>
2020-03-12 21:02:40 -0400

    v16.5.1

--- schemacrawler-docker/docker-build.sh
+++ schemacrawler-docker/docker-build.sh
@@ -3,7 +3,7 @@ set -e
 
 # ## DOCKER
 
-SCHEMACRAWLER_VERSION=16.5.2
+SCHEMACRAWLER_VERSION=16.5.1
 
 echo "** Creating SchemaCrawler v$SCHEMACRAWLER_VERSION Docker container"
 
--- schemacrawler-docker/schemacrawler-distribution-remote.sh
+++ schemacrawler-docker/schemacrawler-distribution-remote.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-SCHEMACRAWLER_VERSION=16.5.2
+SCHEMACRAWLER_VERSION=16.5.1
 
 # Download SchemaCrawler distribution
 echo "** Downloading SchemaCrawler v$SCHEMACRAWLER_VERSION distribution"
