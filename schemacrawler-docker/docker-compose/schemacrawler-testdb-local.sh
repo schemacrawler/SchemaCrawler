77792247065d3b3492ea4b9c18753ff0c2c4d0eb
Sualeh Fatehi <sualeh@hotmail.com>
2020-03-24 22:55:40 -0400

    Do not download drivers separately

--- schemacrawler-docker/docker-compose/schemacrawler-testdb-local.sh
+++ schemacrawler-docker/docker-compose/schemacrawler-testdb-local.sh
@@ -6,8 +6,6 @@ echo "** Obtaining SchemaCrawler v$SCHEMACRAWLER_VERSION distribution"
 
 echo "Starting to obtain"
 
-cp -r ../../schemacrawler-distrib/target/_distribution/_testdb .
-wget -O _testdb/lib/ojdbc8_g-19.3.0.0.jar https://search.maven.org/remotecontent?filepath=com/oracle/database/jdbc/debug/ojdbc8_g/19.3.0.0/ojdbc8_g-19.3.0.0.jar
-wget -O _testdb/lib/jcc-11.5.0.0.jar https://search.maven.org/remotecontent?filepath=com/ibm/db2/jcc/11.5.0.0/jcc-11.5.0.0.jar
+cp -r ../../schemacrawler-distrib/target/_distribution/_testdb .s
 
 echo "Done"
