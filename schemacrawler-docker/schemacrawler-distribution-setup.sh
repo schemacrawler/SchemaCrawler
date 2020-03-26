17e8d17ebd4d6f04bdc37656ca8e1040644a85df
Sualeh Fatehi <sualeh@hotmail.com>
2020-03-25 21:59:43 -0400

    v16.5.3

--- schemacrawler-docker/schemacrawler-distribution-setup.sh
+++ schemacrawler-docker/schemacrawler-distribution-setup.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "** Setting up SchemaCrawler v$SCHEMACRAWLER_VERSION distribution"
 
-SCHEMACRAWLER_VERSION=16.5.2
+SCHEMACRAWLER_VERSION=16.5.3
 SC_DIR=`pwd`/schemacrawler-"$SCHEMACRAWLER_VERSION"-distribution
 
 # Download additional libraries to allow SchemaCrawler commands to work
