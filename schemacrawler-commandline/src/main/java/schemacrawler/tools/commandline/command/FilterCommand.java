96fee4ba197c386d4c5565dcbef48f7357b8d89e
Sualeh Fatehi <sualeh@hotmail.com>
2020-04-05 18:24:07 -0400

    Tweak documentation on row counts

--- schemacrawler-commandline/src/main/java/schemacrawler/tools/commandline/command/FilterCommand.java
+++ schemacrawler-commandline/src/main/java/schemacrawler/tools/commandline/command/FilterCommand.java
@@ -61,6 +61,7 @@ public final class FilterCommand
   private Integer children;
   @Option(names = "--no-empty-tables", description = {
     "Includes only tables that have rows of data",
+    "Requires table row counts to be loaded",
     "Optional, default is false"
   }, negatable = true)
   private Boolean noemptytables;
