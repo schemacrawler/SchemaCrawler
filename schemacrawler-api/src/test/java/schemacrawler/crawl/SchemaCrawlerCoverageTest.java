5daab51a438f7bbd5cb79e9e43eb7f7f92243818
Sualeh Fatehi <sualeh@hotmail.com>
2020-04-10 15:50:37 -0400

    Enhance tests

---
+++ schemacrawler-api/src/test/java/schemacrawler/crawl/SchemaCrawlerCoverageTest.java
@@ -0,0 +1,110 @@
+/*
+========================================================================
+SchemaCrawler
+http://www.schemacrawler.com
+Copyright (c) 2000-2020, Sualeh Fatehi <sualeh@hotmail.com>.
+All rights reserved.
+------------------------------------------------------------------------
+
+SchemaCrawler is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+SchemaCrawler and the accompanying materials are made available under
+the terms of the Eclipse Public License v1.0, GNU General Public License
+v3 or GNU Lesser General Public License v3.
+
+You may elect to redistribute this code under any of these licenses.
+
+The Eclipse Public License is available at:
+http://www.eclipse.org/legal/epl-v10.html
+
+The GNU General Public License v3 and the GNU Lesser General Public
+License v3 are available at:
+http://www.gnu.org/licenses/
+
+========================================================================
+*/
+
+package schemacrawler.crawl;
+
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static schemacrawler.test.utility.DatabaseTestUtility.getCatalog;
+import static schemacrawler.test.utility.DatabaseTestUtility.loadHsqldbConfig;
+
+import java.sql.Connection;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import schemacrawler.schema.Catalog;
+import schemacrawler.schema.Index;
+import schemacrawler.schema.PrimaryKey;
+import schemacrawler.schema.SchemaReference;
+import schemacrawler.schema.Table;
+import schemacrawler.schemacrawler.Config;
+import schemacrawler.schemacrawler.RegularExpressionExclusionRule;
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+import schemacrawler.schemacrawler.SchemaCrawlerOptionsBuilder;
+import schemacrawler.schemacrawler.SchemaInfoLevelBuilder;
+import schemacrawler.schemacrawler.SchemaRetrievalOptions;
+import schemacrawler.schemacrawler.SchemaRetrievalOptionsBuilder;
+import schemacrawler.test.utility.TestContextParameterResolver;
+import schemacrawler.test.utility.TestDatabaseConnectionParameterResolver;
+
+@ExtendWith(TestDatabaseConnectionParameterResolver.class)
+@ExtendWith(TestContextParameterResolver.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SchemaCrawlerCoverageTest
+{
+
+  private Catalog catalog;
+
+  @BeforeAll
+  public void loadCatalog(final Connection connection)
+    throws Exception
+  {
+    final Config config = loadHsqldbConfig();
+
+    final SchemaRetrievalOptions schemaRetrievalOptions =
+      SchemaRetrievalOptionsBuilder.newSchemaRetrievalOptions(config);
+
+    final SchemaCrawlerOptionsBuilder schemaCrawlerOptionsBuilder = SchemaCrawlerOptionsBuilder
+      .builder()
+      .withSchemaInfoLevel(SchemaInfoLevelBuilder.maximum())
+      .includeSchemas(new RegularExpressionExclusionRule(".*\\.FOR_LINT"))
+      .includeAllSynonyms()
+      .includeAllSequences()
+      .includeAllRoutines()
+      .loadRowCounts();
+    final SchemaCrawlerOptions schemaCrawlerOptions = schemaCrawlerOptionsBuilder.toOptions();
+
+    catalog = getCatalog(connection, schemaRetrievalOptions, schemaCrawlerOptions);
+  }
+
+  @Test
+  public void coverPrimaryKey()
+  {
+    final SchemaReference schema = new SchemaReference("PUBLIC", "BOOKS");
+    final Table table = catalog
+      .lookupTable(schema, "AUTHORS")
+      .get();
+    final Index index = table
+      .lookupIndex("IDX_B_AUTHORS")
+      .get();
+    final PrimaryKey primaryKey = new MutablePrimaryKey((MutableIndex) index);
+
+    assertThat(index.getFullName(), is(primaryKey.getFullName()));
+    assertThat(index.getColumns(), is(primaryKey.getColumns()));
+    assertThat(index.getPages(), is(primaryKey.getPages()));
+    assertThat(index.getCardinality(), is(primaryKey.getCardinality()));
+    assertThat(index.getType(), is(primaryKey.getType()));
+
+    assertThat(index.isUnique(), is(false));
+    assertThat(primaryKey.isUnique(), is(true));
+  }
+
+}
