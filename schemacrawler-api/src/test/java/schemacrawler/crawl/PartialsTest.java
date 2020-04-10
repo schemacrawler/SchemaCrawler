90a353f9bb35473df429eff139cdf5af866beae5
Sualeh Fatehi <sualeh@hotmail.com>
2020-04-10 13:34:01 -0400

    Enhance partials test

--- schemacrawler-api/src/test/java/schemacrawler/crawl/PartialsTest.java
+++ schemacrawler-api/src/test/java/schemacrawler/crawl/PartialsTest.java
@@ -1,7 +1,11 @@
 package schemacrawler.crawl;
 
 
+import static com.github.npathai.hamcrestopt.OptionalMatchers.isEmpty;
+import static com.github.npathai.hamcrestopt.OptionalMatchers.isPresentAndIs;
 import static org.apache.commons.lang3.reflect.MethodUtils.invokeMethod;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.lang.reflect.InvocationTargetException;
@@ -18,6 +22,12 @@ public class PartialsTest
   {
     final SchemaReference schema = new SchemaReference("catalog", "schema");
     final TablePartial table = new TablePartial(schema, "table");
+
+    final ColumnPartial column = new ColumnPartial(table, "column");
+    table.addColumn(column);
+
+    assertThat(table.lookupColumn("column"), isPresentAndIs(column));
+    assertThat(table.lookupColumn("unknown_column"), isEmpty());
 
     for (final String methodName : new String[] {
       "getColumns",
@@ -61,8 +71,13 @@ public class PartialsTest
   {
     final SchemaReference schema = new SchemaReference("catalog", "schema");
     final TablePartial table = new TablePartial(schema, "table");
-    final ColumnPartial column = new ColumnPartial(table, "table");
+
+    final ColumnPartial column = new ColumnPartial(table, "column");
     table.addColumn(column);
+
+    final ColumnPartial columnReferenced = new ColumnPartial(table, "other_column");
+    column.setReferencedColumn(columnReferenced);
+    table.addColumn(columnReferenced);
 
     for (final String methodName : new String[] {
       "getColumnDataType",
@@ -90,12 +105,15 @@ public class PartialsTest
 
     for (final String methodName : new String[] {
       "lookupPrivilege",
-    })
+      })
     {
       assertThrows(InvocationTargetException.class,
                    () -> invokeMethod(column, methodName, ""),
                    "Testing partial method, " + methodName);
     }
+
+    assertThat(column.getParent(), is(table));
+    assertThat(column.getReferencedColumn(), is(columnReferenced));
 
   }
 
