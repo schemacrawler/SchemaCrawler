384125099d5ea031ccc91f833e02832ed640aef9
Sualeh Fatehi <sualeh@hotmail.com>
2020-04-17 20:27:16 -0400

    Download artifacts with naming convention

--- schemacrawler-distrib/src/examples/resources/_downloader/download.sh
+++ schemacrawler-distrib/src/examples/resources/_downloader/download.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "Downloading $1"
-java -jar ivy-2.5.0.jar -ivy $1_ivy.xml -settings ivysettings.xml -retrieve "../_schemacrawler/lib/[artifact]-[revision].[ext]"
+java -jar ivy-2.5.0.jar -ivy $1_ivy.xml -settings ivysettings.xml -retrieve "../_schemacrawler/lib/[artifact]-[revision]-[type].[ext]"
