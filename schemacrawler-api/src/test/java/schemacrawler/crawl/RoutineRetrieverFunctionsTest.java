08265b93bcd273c3cbe741d6a895de36d5c3c260
Sualeh Fatehi <sualeh@hotmail.com>
2020-05-19 19:30:32 -0400

    Add test for functions from data dictionary

---
+++ schemacrawler-api/src/test/java/schemacrawler/crawl/RoutineRetrieverFunctionsTest.java
@@ -0,0 +1,135 @@
+/*
+========================================================================
+SchemaCrawler
+http://www.schemacrawler.com
+Copyright (c) 2000-2020, Sualeh Fatehi <sualeh@hotmail.com>.
+All rights reserved.
+------------------------------------------------------------------------
+
+SchemaCrawler is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+SchemaCrawler and the accompanying materials are made available under
+the terms of the Eclipse Public License v1.0, GNU General Public License
+v3 or GNU Lesser General Public License v3.
+
+You may elect to redistribute this code under any of these licenses.
+
+The Eclipse Public License is available at:
+http://www.eclipse.org/legal/epl-v10.html
+
+The GNU General Public License v3 and the GNU Lesser General Public
+License v3 are available at:
+http://www.gnu.org/licenses/
+
+========================================================================
+*/
+package schemacrawler.crawl;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
+import static schemacrawler.test.utility.DatabaseTestUtility.getCatalog;
+import static schemacrawler.test.utility.FileHasContent.classpathResource;
+import static schemacrawler.test.utility.FileHasContent.hasSameContentAs;
+import static schemacrawler.test.utility.FileHasContent.outputOf;
+
+import java.sql.Connection;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import schemacrawler.inclusionrule.IncludeAll;
+import schemacrawler.inclusionrule.RegularExpressionInclusionRule;
+import schemacrawler.schema.Catalog;
+import schemacrawler.schema.Routine;
+import schemacrawler.schemacrawler.InfoLevel;
+import schemacrawler.schemacrawler.InformationSchemaKey;
+import schemacrawler.schemacrawler.MetadataRetrievalStrategy;
+import schemacrawler.schemacrawler.SchemaCrawlerException;
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+import schemacrawler.schemacrawler.SchemaCrawlerOptionsBuilder;
+import schemacrawler.schemacrawler.SchemaInfoLevelBuilder;
+import schemacrawler.schemacrawler.SchemaRetrievalOptions;
+import schemacrawler.schemacrawler.SchemaRetrievalOptionsBuilder;
+import schemacrawler.test.utility.TestContext;
+import schemacrawler.test.utility.TestContextParameterResolver;
+import schemacrawler.test.utility.TestDatabaseConnectionParameterResolver;
+import schemacrawler.test.utility.TestWriter;
+import schemacrawler.utility.NamedObjectSort;
+
+@ExtendWith(TestDatabaseConnectionParameterResolver.class)
+@ExtendWith(TestContextParameterResolver.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class RoutineRetrieverFunctionsTest
+{
+
+  private MutableCatalog catalog;
+
+  @Test
+  @DisplayName("Retrieve functions from data dictionary")
+  public void functionsFromDataDictionary(final TestContext testContext, final Connection connection)
+    throws Exception
+  {
+    final SchemaRetrievalOptionsBuilder schemaRetrievalOptionsBuilder = SchemaRetrievalOptionsBuilder.builder();
+    schemaRetrievalOptionsBuilder
+      .withFunctionRetrievalStrategy(MetadataRetrievalStrategy.data_dictionary_all)
+      .withInformationSchemaViewsBuilder()
+      .withSql(InformationSchemaKey.FUNCTIONS, "SELECT "
+                                               + "PROCEDURE_CAT AS FUNCTION_CAT, PROCEDURE_SCHEM AS FUNCTION_SCHEM, "
+                                               + "PROCEDURE_NAME AS FUNCTION_NAME, PROCEDURE_TYPE AS FUNCTION_TYPE, "
+                                               + "REMARKS, SPECIFIC_NAME "
+                                               + "FROM INFORMATION_SCHEMA.SYSTEM_PROCEDURES");
+    final SchemaRetrievalOptions schemaRetrievalOptions = schemaRetrievalOptionsBuilder.toOptions();
+    final RetrieverConnection retrieverConnection = new RetrieverConnection(connection, schemaRetrievalOptions);
+
+    final SchemaCrawlerOptions options = SchemaCrawlerOptionsBuilder.newSchemaCrawlerOptions();
+
+    final RoutineRetriever functionRetriever = new RoutineRetriever(retrieverConnection, catalog, options);
+    functionRetriever.retrieveFunctions(catalog.getAllSchemas(), new IncludeAll());
+
+    final TestWriter testout = new TestWriter();
+    try (final TestWriter out = testout)
+    {
+      final Routine[] functions = ((Catalog) catalog)
+        .getRoutines()
+        .toArray(new Routine[0]);
+      Arrays.sort(functions, NamedObjectSort.alphabetical);
+      for (final Routine function : functions)
+      {
+        out.println(String.format("%s (%s) [%s]",
+                                  function.getFullName(),
+                                  function.getSpecificName(),
+                                  function.getRoutineType()));
+      }
+    }
+    assertThat(outputOf(testout), hasSameContentAs(classpathResource(testContext.testMethodFullName())));
+
+  }
+
+  @BeforeAll
+  public void loadBaseCatalog(final Connection connection)
+    throws SchemaCrawlerException
+  {
+    final SchemaCrawlerOptions schemaCrawlerOptions = SchemaCrawlerOptionsBuilder
+      .builder()
+      .withSchemaInfoLevel(SchemaInfoLevelBuilder
+                             .builder()
+                             .withInfoLevel(InfoLevel.minimum)
+                             .setRetrieveRoutines(false)
+                             .toOptions())
+      .includeSchemas(new RegularExpressionInclusionRule(".*\\.BOOKS"))
+      .toOptions();
+    catalog = (MutableCatalog) getCatalog(connection,
+                                          SchemaRetrievalOptionsBuilder.newSchemaRetrievalOptions(),
+                                          schemaCrawlerOptions);
+
+    assertThat(catalog.getRoutines(), is(empty()));
+  }
+
+}
