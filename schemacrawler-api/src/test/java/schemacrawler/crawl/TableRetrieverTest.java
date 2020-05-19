745c9eeae4d9634babd559fe9f243d340057209e
Sualeh Fatehi <sualeh@hotmail.com>
2020-05-18 20:23:24 -0400

    Add data dictionary tests for tables

---
+++ schemacrawler-api/src/test/java/schemacrawler/crawl/TableRetrieverTest.java
@@ -0,0 +1,139 @@
+/*
+========================================================================
+SchemaCrawler
+http://www.schemacrawler.com
+Copyright (c) 2000-2020, Sualeh Fatehi <sualeh@hotmail.com>.
+All rights reserved.
+------------------------------------------------------------------------
+
+SchemaCrawler is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+SchemaCrawler and the accompanying materials are made available under
+the terms of the Eclipse Public License v1.0, GNU General Public License
+v3 or GNU Lesser General Public License v3.
+
+You may elect to redistribute this code under any of these licenses.
+
+The Eclipse Public License is available at:
+http://www.eclipse.org/legal/epl-v10.html
+
+The GNU General Public License v3 and the GNU Lesser General Public
+License v3 are available at:
+http://www.gnu.org/licenses/
+
+========================================================================
+*/
+package schemacrawler.crawl;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.arrayWithSize;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
+import static schemacrawler.test.utility.DatabaseTestUtility.getCatalog;
+import static schemacrawler.test.utility.FileHasContent.classpathResource;
+import static schemacrawler.test.utility.FileHasContent.hasSameContentAs;
+import static schemacrawler.test.utility.FileHasContent.outputOf;
+
+import java.sql.Connection;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import schemacrawler.inclusionrule.IncludeAll;
+import schemacrawler.inclusionrule.RegularExpressionExclusionRule;
+import schemacrawler.schema.Catalog;
+import schemacrawler.schema.Schema;
+import schemacrawler.schema.Table;
+import schemacrawler.schemacrawler.InfoLevel;
+import schemacrawler.schemacrawler.InformationSchemaKey;
+import schemacrawler.schemacrawler.MetadataRetrievalStrategy;
+import schemacrawler.schemacrawler.SchemaCrawlerException;
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+import schemacrawler.schemacrawler.SchemaCrawlerOptionsBuilder;
+import schemacrawler.schemacrawler.SchemaInfoLevelBuilder;
+import schemacrawler.schemacrawler.SchemaRetrievalOptions;
+import schemacrawler.schemacrawler.SchemaRetrievalOptionsBuilder;
+import schemacrawler.test.utility.TestContext;
+import schemacrawler.test.utility.TestContextParameterResolver;
+import schemacrawler.test.utility.TestDatabaseConnectionParameterResolver;
+import schemacrawler.test.utility.TestWriter;
+import schemacrawler.utility.NamedObjectSort;
+
+@ExtendWith(TestDatabaseConnectionParameterResolver.class)
+@ExtendWith(TestContextParameterResolver.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TableRetrieverTest
+{
+
+  private MutableCatalog catalog;
+
+  @Test
+  @DisplayName("Retrieve tables from data dictionary")
+  public void tablesFromDataDictionary(final TestContext testContext, final Connection connection)
+    throws Exception
+  {
+    final SchemaRetrievalOptionsBuilder schemaRetrievalOptionsBuilder = SchemaRetrievalOptionsBuilder.builder();
+    schemaRetrievalOptionsBuilder
+      .withTableRetrievalStrategy(MetadataRetrievalStrategy.data_dictionary_all)
+      .withInformationSchemaViewsBuilder()
+      .withSql(InformationSchemaKey.TABLES, "SELECT * FROM INFORMATION_SCHEMA.SYSTEM_TABLES");
+    final SchemaRetrievalOptions schemaRetrievalOptions = schemaRetrievalOptionsBuilder.toOptions();
+    final RetrieverConnection retrieverConnection = new RetrieverConnection(connection, schemaRetrievalOptions);
+
+    final SchemaCrawlerOptions options = SchemaCrawlerOptionsBuilder.newSchemaCrawlerOptions();
+
+    final TableRetriever tableRetriever = new TableRetriever(retrieverConnection, catalog, options);
+    tableRetriever.retrieveTables(catalog.getAllSchemas(), "", Arrays.asList("TABLE", "VIEW"), new IncludeAll());
+
+    final TestWriter testout = new TestWriter();
+    try (final TestWriter out = testout)
+    {
+      final Schema[] schemas = ((Catalog) catalog)
+        .getSchemas()
+        .toArray(new Schema[0]);
+      assertThat("Schema count does not match", schemas, arrayWithSize(5));
+      for (final Schema schema : schemas)
+      {
+        final Table[] tables = ((Catalog) catalog)
+          .getTables(schema)
+          .toArray(new Table[0]);
+        Arrays.sort(tables, NamedObjectSort.alphabetical);
+        for (final Table table : tables)
+        {
+          out.println(String.format("%s [%s]", table.getFullName(), table.getTableType()));
+        }
+      }
+    }
+    assertThat(outputOf(testout), hasSameContentAs(classpathResource(testContext.testMethodFullName())));
+
+  }
+
+  @BeforeAll
+  public void loadBaseCatalog(final Connection connection)
+    throws SchemaCrawlerException
+  {
+    final SchemaCrawlerOptions schemaCrawlerOptions = SchemaCrawlerOptionsBuilder
+      .builder()
+      .withSchemaInfoLevel(SchemaInfoLevelBuilder
+                             .builder()
+                             .withInfoLevel(InfoLevel.minimum)
+                             .setRetrieveTables(false)
+                             .toOptions())
+      .includeSchemas(new RegularExpressionExclusionRule(".*\\.FOR_LINT"))
+      .toOptions();
+    catalog = (MutableCatalog) getCatalog(connection,
+                                          SchemaRetrievalOptionsBuilder.newSchemaRetrievalOptions(),
+                                          schemaCrawlerOptions);
+
+    final Collection<Table> tables = catalog.getTables();
+    assertThat(tables, is(empty()));
+  }
+
+}
