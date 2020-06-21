76d9f2aad2332749580bcbad945a5393dc194973
Sualeh Fatehi <sualeh@hotmail.com>
2020-06-20 23:49:24 -0400

    Fix shell script lint issues

--- schemacrawler-distrib/src/examples/resources/examples/api/executable.sh
+++ schemacrawler-distrib/src/examples/resources/examples/api/executable.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 rm -f *.class
 javac -classpath ../../_schemacrawler/config:$(echo ../../_schemacrawler/lib/*.jar | tr ' ' ':'):. com/example/ExecutableExample.java
 java -classpath ../../_schemacrawler/config:$(echo ../../_schemacrawler/lib/*.jar | tr ' ' ':'):. com.example.ExecutableExample
