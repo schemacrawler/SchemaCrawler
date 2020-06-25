73d7b94e6a9fc60f85152f69028a32b28b25d819
Sualeh Fatehi <sualeh@hotmail.com>
2020-06-24 22:42:51 -0400

    Use wildcards to use all jars in lib/

--- schemacrawler-distrib/src/examples/resources/examples/api/api.sh
+++ schemacrawler-distrib/src/examples/resources/examples/api/api.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-javac -classpath ../../_schemacrawler/config:$(echo ../../_schemacrawler/lib/*.jar | tr ' ' ':'):.  com/example/ApiExample.java
-java -classpath ../../_schemacrawler/config:$(echo ../../_schemacrawler/lib/*.jar | tr ' ' ':'):. com.example.ApiExample
+javac -classpath ../../_schemacrawler/lib/*:../../_schemacrawler/config:. com/example/ApiExample.java
+java -classpath ../../_schemacrawler/lib/*:../../_schemacrawler/config:. com.example.ApiExample
--- schemacrawler-distrib/src/examples/resources/examples/api/executable.sh
+++ schemacrawler-distrib/src/examples/resources/examples/api/executable.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-javac -classpath ../../_schemacrawler/config:$(echo ../../_schemacrawler/lib/*.jar | tr ' ' ':'):. com/example/ExecutableExample.java
-java -classpath ../../_schemacrawler/config:$(echo ../../_schemacrawler/lib/*.jar | tr ' ' ':'):. com.example.ExecutableExample
+javac -classpath ../../_schemacrawler/lib/*:../../_schemacrawler/config:. com/example/ExecutableExample.java
+java -classpath ../../_schemacrawler/lib/*:../../_schemacrawler/config:. com.example.ExecutableExample
