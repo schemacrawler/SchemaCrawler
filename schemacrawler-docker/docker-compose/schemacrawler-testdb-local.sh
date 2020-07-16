dccca1c0a4fcd3fda3c03b1a51eb5f68fb9aa78e
Sualeh Fatehi <sualeh@hotmail.com>
2020-07-15 21:45:04 -0400

    Copy files with original timestamps

--- schemacrawler-docker/docker-compose/schemacrawler-testdb-local.sh
+++ schemacrawler-docker/docker-compose/schemacrawler-testdb-local.sh
@@ -6,6 +6,6 @@ echo "** Obtaining SchemaCrawler v$SCHEMACRAWLER_VERSION distribution"
 
 echo "Starting to obtain"
 
-cp -r ../../schemacrawler-distrib/target/_distribution/_testdb .
+cp -rp ../../schemacrawler-distrib/target/_distribution/_testdb .
 
 echo "Done"
