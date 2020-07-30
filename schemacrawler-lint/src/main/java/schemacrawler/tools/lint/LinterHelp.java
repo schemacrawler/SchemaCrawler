dc5f5190259c2c852316e68df7a37af3282822ff
Sualeh Fatehi <sualeh@hotmail.com>
2020-07-30 12:13:25 -0400

    Reorganize how lint documentation is generated for the website and for help, and remove redundencies

--- schemacrawler-lint/src/main/java/schemacrawler/tools/lint/LinterHelp.java
+++ schemacrawler-lint/src/main/java/schemacrawler/tools/lint/LinterHelp.java
@@ -28,12 +28,48 @@ http://www.gnu.org/licenses/
 package schemacrawler.tools.lint;
 
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.file.StandardOpenOption.APPEND;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.function.Supplier;
 
 import schemacrawler.schemacrawler.SchemaCrawlerException;
 
-public final class LinterHelp implements Supplier<String>
+public final class LinterHelp
+  implements Supplier<String>
 {
+
+  public static void main(final String[] args)
+    throws IOException
+  {
+    if (args.length == 1)
+    {
+      Files.write(Paths.get(args[0]),
+                  new LinterHelp(true)
+                    .get()
+                    .getBytes(UTF_8),
+                  APPEND);
+    }
+    else
+    {
+      System.out.println(new LinterHelp(true).get());
+    }
+  }
+
+  private final boolean generateMarkdown;
+
+  public LinterHelp(final boolean generateMarkdown)
+  {
+    this.generateMarkdown = generateMarkdown;
+  }
+
+  public LinterHelp()
+  {
+    this(false);
+  }
 
   public String get()
   {
@@ -41,21 +77,27 @@ public final class LinterHelp implements Supplier<String>
     {
       final StringBuilder buffer = new StringBuilder(1024);
 
-      buffer
-        .append(System.lineSeparator())
-        .append("--- Available Linters ---")
-        .append(System.lineSeparator())
-        .append(System.lineSeparator());
+      if (generateMarkdown)
+      {
+        printMarkdownHeader(buffer);
+      }
+      else
+      {
+        printHelpHeader(buffer);
+      }
 
       final LinterRegistry registry = new LinterRegistry();
       for (final String linterId : registry)
       {
         final Linter linter = registry.newLinter(linterId);
-
-        buffer
-          .append("Linter: ")
-          .append(linter.getLinterId())
-          .append(System.lineSeparator());
+        if (generateMarkdown)
+        {
+          printMarkdownLinterHeader(buffer, linter);
+        }
+        else
+        {
+          printLinterHeader(buffer, linter);
+        }
         buffer
           .append(linter.getDescription())
           .append(System.lineSeparator())
@@ -70,9 +112,41 @@ public final class LinterHelp implements Supplier<String>
     }
   }
 
-  public static void main(final String[] args)
+  private void printMarkdownLinterHeader(final StringBuilder buffer,
+                                         final Linter linter)
   {
-    System.out.println(new LinterHelp().get());
+    buffer
+      .append("### Linter: *")
+      .append(linter.getLinterId())
+      .append("*  ")
+      .append(System.lineSeparator());
+  }
+
+  private void printLinterHeader(final StringBuilder buffer,
+                                 final Linter linter)
+  {
+    buffer
+      .append("Linter: ")
+      .append(linter.getLinterId())
+      .append(System.lineSeparator());
+  }
+
+  private void printHelpHeader(final StringBuilder buffer)
+  {
+    buffer
+      .append(System.lineSeparator())
+      .append("Available SchemaCrawler linters:")
+      .append(System.lineSeparator())
+      .append(System.lineSeparator());
+  }
+
+  private void printMarkdownHeader(final StringBuilder buffer)
+  {
+    buffer
+      .append(System.lineSeparator())
+      .append("## Lint Checks")
+      .append(System.lineSeparator())
+      .append(System.lineSeparator());
   }
 
 }
