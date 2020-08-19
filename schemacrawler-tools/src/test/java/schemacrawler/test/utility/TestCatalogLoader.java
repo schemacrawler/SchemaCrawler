077c3dd026108d339636059ec0c93a367047d73b
Sualeh Fatehi <sualeh@hotmail.com>
2020-08-19 17:09:19 -0400

    Add test catalog loader, and registry test

---
+++ schemacrawler-tools/src/test/java/schemacrawler/test/utility/TestCatalogLoader.java
@@ -0,0 +1,84 @@
+package schemacrawler.test.utility;
+
+
+import static org.mockito.Mockito.mock;
+
+import java.sql.Connection;
+
+import schemacrawler.schema.Catalog;
+import schemacrawler.schemacrawler.Config;
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+import schemacrawler.schemacrawler.SchemaRetrievalOptions;
+import schemacrawler.tools.catalogloader.CatalogLoader;
+
+public class TestCatalogLoader
+  implements CatalogLoader
+{
+
+  private Config additionalConfiguration;
+  private Connection connection;
+  private SchemaCrawlerOptions schemaCrawlerOptions;
+  private SchemaRetrievalOptions schemaRetrievalOptions;
+
+  @Override
+  public Config getAdditionalConfiguration()
+  {
+    return additionalConfiguration;
+  }
+
+  @Override
+  public void setAdditionalConfiguration(final Config additionalConfiguration)
+  {
+    this.additionalConfiguration = additionalConfiguration;
+  }
+
+  @Override
+  public Connection getConnection()
+  {
+    return connection;
+  }
+
+  @Override
+  public void setConnection(final Connection connection)
+  {
+    this.connection = connection;
+  }
+
+  @Override
+  public String getDatabaseSystemIdentifier()
+  {
+    return "test-db";
+  }
+
+  @Override
+  public SchemaCrawlerOptions getSchemaCrawlerOptions()
+  {
+    return schemaCrawlerOptions;
+  }
+
+  @Override
+  public void setSchemaCrawlerOptions(final SchemaCrawlerOptions schemaCrawlerOptions)
+  {
+    this.schemaCrawlerOptions = schemaCrawlerOptions;
+  }
+
+  @Override
+  public SchemaRetrievalOptions getSchemaRetrievalOptions()
+  {
+    return schemaRetrievalOptions;
+  }
+
+  @Override
+  public void setSchemaRetrievalOptions(final SchemaRetrievalOptions schemaRetrievalOptions)
+  {
+    this.schemaRetrievalOptions = schemaRetrievalOptions;
+  }
+
+  @Override
+  public Catalog loadCatalog()
+    throws Exception
+  {
+    return mock(Catalog.class);
+  }
+
+}
---
+++ schemacrawler-tools/src/test/java/schemacrawler/tools/catalogloader/CatalogLoaderRegistryTest.java
@@ -0,0 +1,70 @@
+/*
+========================================================================
+SchemaCrawler
+http://www.schemacrawler.com
+Copyright (c) 2000-2020, Sualeh Fatehi <sualeh@hotmail.com>.
+All rights reserved.
+------------------------------------------------------------------------
+
+SchemaCrawler is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+SchemaCrawler and the accompanying materials are made available under
+the terms of the Eclipse Public License v1.0, GNU General Public License
+v3 or GNU Lesser General Public License v3.
+
+You may elect to redistribute this code under any of these licenses.
+
+The Eclipse Public License is available at:
+http://www.eclipse.org/legal/epl-v10.html
+
+The GNU General Public License v3 and the GNU Lesser General Public
+License v3 are available at:
+http://www.gnu.org/licenses/
+
+========================================================================
+*/
+
+package schemacrawler.tools.catalogloader;
+
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.jupiter.api.Test;
+import schemacrawler.schemacrawler.SchemaCrawlerException;
+
+public class CatalogLoaderRegistryTest
+{
+
+  @Test
+  public void catalogLoaderRegistry()
+    throws SchemaCrawlerException
+  {
+    final CatalogLoaderRegistry registry = new CatalogLoaderRegistry();
+
+    // Look up test catalog loader
+    assertThat(registry.hasDatabaseSystemIdentifier("test-db"), is(true));
+    assertThat(registry.lookupCatalogLoader("test-db"), is(not(nullValue())));
+    assertThat(registry
+                 .lookupCatalogLoader("test-db")
+                 .getClass().getSimpleName(), is("TestCatalogLoader"));
+    assertThat(registry
+                 .lookupCatalogLoader("test-db")
+                 .getDatabaseSystemIdentifier(), is("test-db"));
+
+    // Look up unknown catalog loader
+    assertThat(registry.hasDatabaseSystemIdentifier("some-db"), is(false));
+    assertThat(registry.lookupCatalogLoader("some-db"), is(not(nullValue())));
+    assertThat(registry
+                 .lookupCatalogLoader("some-db")
+                 .getClass().getSimpleName(), is("SchemaCrawlerCatalogLoader"));
+    assertThat(registry
+                 .lookupCatalogLoader("some-db")
+                 .getDatabaseSystemIdentifier(), is(nullValue()));
+  }
+
+}
