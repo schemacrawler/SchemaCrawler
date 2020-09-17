57d6b1c19148d30fbd188cca08b64ba13b9f9aaf
Sualeh Fatehi <sualeh@hotmail.com>
2020-09-17 13:47:56 -0400

    Fix build

--- schemacrawler-tools/src/main/java/schemacrawler/utility/PropertiesUtility.java
+++ schemacrawler-tools/src/main/java/schemacrawler/utility/PropertiesUtility.java
@@ -25,7 +25,7 @@ http://www.gnu.org/licenses/
 
 ========================================================================
 */
-package schemacrawler.tools.commandline.utility;
+package schemacrawler.utility;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Objects.requireNonNull;
