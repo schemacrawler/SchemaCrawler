6889c7d8ba511cc3952a6cd782ccc7227b855214
Sualeh Fatehi <sualeh@hotmail.com>
2020-10-05 17:24:45 -0400

    Test plugin command classes

---
+++ schemacrawler-tools/src/test/java/schemacrawler/tools/executable/commandline/PluginCommandPojoTest.java
@@ -0,0 +1,66 @@
+/*
+========================================================================
+SchemaCrawler
+http://www.schemacrawler.com
+Copyright (c) 2000-2020, Sualeh Fatehi <sualeh@hotmail.com>.
+All rights reserved.
+------------------------------------------------------------------------
+
+SchemaCrawler is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+SchemaCrawler and the accompanying materials are made available under
+the terms of the Eclipse Public License v1.0, GNU General Public License
+v3 or GNU Lesser General Public License v3.
+
+You may elect to redistribute this code under any of these licenses.
+
+The Eclipse Public License is available at:
+http://www.eclipse.org/legal/epl-v10.html
+
+The GNU General Public License v3 and the GNU Lesser General Public
+License v3 are available at:
+http://www.gnu.org/licenses/
+
+========================================================================
+*/
+package schemacrawler.tools.executable.commandline;
+
+import org.junit.jupiter.api.Test;
+
+import com.openpojo.reflection.impl.PojoClassFactory;
+import com.openpojo.validation.Validator;
+import com.openpojo.validation.ValidatorBuilder;
+import com.openpojo.validation.rule.impl.GetterMustExistRule;
+import com.openpojo.validation.test.impl.GetterTester;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+
+public class PluginCommandPojoTest {
+
+  @Test
+  public void pluginCommandOption() {
+    EqualsVerifier.forClass(PluginCommandOption.class)
+        .suppress(Warning.STRICT_INHERITANCE)
+        .withOnlyTheseFields("name")
+        .verify();
+
+    final Validator validator =
+        ValidatorBuilder.create().with(new GetterMustExistRule()).with(new GetterTester()).build();
+    validator.validate(PojoClassFactory.getPojoClass(PluginCommandOption.class));
+  }
+
+  @Test
+  public void pluginCommand() {
+    EqualsVerifier.forClass(PluginCommand.class)
+        .suppress(Warning.STRICT_INHERITANCE)
+        .withOnlyTheseFields("name")
+        .verify();
+
+    final Validator validator =
+        ValidatorBuilder.create().with(new GetterMustExistRule()).with(new GetterTester()).build();
+    validator.validate(PojoClassFactory.getPojoClass(PluginCommand.class));
+  }
+}
