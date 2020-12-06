be6652f829e7f697251710710c2ad7af7397427b
Sualeh Fatehi <sualeh@hotmail.com>
2020-12-06 11:51:04 -0500

    Raw cut at System property extension - work in progress

---
+++ schemacrawler-api/src/test/java/schemacrawler/test/utility/TestWithSystemProperty.java
@@ -0,0 +1,59 @@
+/*
+========================================================================
+SchemaCrawler
+http://www.schemacrawler.com
+Copyright (c) 2000-2020, Sualeh Fatehi <sualeh@hotmail.com>.
+All rights reserved.
+------------------------------------------------------------------------
+
+SchemaCrawler is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+SchemaCrawler and the accompanying materials are made available under
+the terms of the Eclipse Public License v1.0, GNU General Public License
+v3 or GNU Lesser General Public License v3.
+
+You may elect to redistribute this code under any of these licenses.
+
+The Eclipse Public License is available at:
+http://www.eclipse.org/legal/epl-v10.html
+
+The GNU General Public License v3 and the GNU Lesser General Public
+License v3 are available at:
+http://www.gnu.org/licenses/
+
+========================================================================
+*/
+package schemacrawler.test.utility;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyString;
+import static org.hamcrest.Matchers.is;
+
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.AbstractMap.SimpleImmutableEntry;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+public class TestWithSystemProperty implements BeforeEachCallback, AfterEachCallback {
+
+  private SimpleImmutableEntry<String, String> err;
+
+  @Override
+  public void afterEach(final ExtensionContext context) throws Exception {
+    System.setErr(new PrintStream(new FileOutputStream(FileDescriptor.err)));
+
+    assertThat("Expected no System.err output", err.getFileContents(), is(emptyString()));
+  }
+
+  @Override
+  public void beforeEach(final ExtensionContext context) throws Exception {
+    err = new TestOutputStream();
+    System.setErr(new PrintStream(err));
+  }
+}
