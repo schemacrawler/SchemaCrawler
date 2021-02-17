d398bf960eea60975f58c9745459decfac76a47f
Sualeh Fatehi <sualeh@hotmail.com>
2021-02-17 18:49:32 -0500

    Add project section

--- schemacrawler-scripting/src/test/resources/dbml.py
+++ schemacrawler-scripting/src/test/resources/dbml.py
@@ -1,6 +1,14 @@
+from __future__ import print_function
 from schemacrawler.schema import TableRelationshipType # pylint: disable=import-error
 import re
 
+print('Project "' + catalog.crawlInfo.runId + '" {')
+print('  database_type: "' + re.sub(r'\"', '', catalog.crawlInfo.databaseVersion.toString()) + '"')
+print("  Note: '''")
+print(catalog.crawlInfo)
+print("  '''")
+print("}")
+    
 for table in catalog.getTables():
   print('Table "' + re.sub(r'\"', '', table.fullName) + '" {')
   for column in table.columns:
