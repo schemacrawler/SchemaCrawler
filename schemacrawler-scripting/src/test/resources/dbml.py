286eafc29cb1cefd81e5958e2be9c8ebef314be1
Sualeh Fatehi <sualeh@hotmail.com>
2021-02-17 17:47:51 -0500

    Add table groups

--- schemacrawler-scripting/src/test/resources/dbml.py
+++ schemacrawler-scripting/src/test/resources/dbml.py
@@ -1,7 +1,7 @@
 from schemacrawler.schema import TableRelationshipType # pylint: disable=import-error
 import re
 
-for table in catalog.tables:
+for table in catalog.getTables():
   print('Table "' + re.sub(r'\"', '', table.fullName) + '" {')
   for column in table.columns:
     print('  "' + column.name + '" "' + column.columnDataType.name + '" ' )
@@ -11,10 +11,18 @@ for table in catalog.tables:
     print("  '''")
   print('}')
   print('')
-  
+      
 """
 for table in catalog.tables:  
   for childTable in table.getRelatedTables(TableRelationshipType.child):
     print('  ' + table.name + ' ||--o{ ' + childTable.name + ' : "foreign key"')
   print('')
 """
+
+# Table groups
+for schema in catalog.schemas:
+  print('TableGroup "' + re.sub(r'\"', '', schema.fullName) + '" {')
+  for table in catalog.getTables(schema):
+    print('  "' + re.sub(r'\"', '', table.fullName) + '\"')
+  print('}')
+  print('')
