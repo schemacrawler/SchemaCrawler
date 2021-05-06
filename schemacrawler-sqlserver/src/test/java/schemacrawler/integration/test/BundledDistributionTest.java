6e3c5f5525323501bd1f76567083956d92074371
Sualeh Fatehi <sualeh@hotmail.com>
2021-05-06 18:51:14 -0400

    Remove ability to retrieve stored procedure definitions in SQL Server - see full discussion in issue #470 (fixes #470)

--- schemacrawler-sqlserver/src/test/java/schemacrawler/integration/test/BundledDistributionTest.java
+++ schemacrawler-sqlserver/src/test/java/schemacrawler/integration/test/BundledDistributionTest.java
@@ -27,43 +27,39 @@ http://www.gnu.org/licenses/
 */
 package schemacrawler.integration.test;
 
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import java.sql.Connection;
 
 import org.junit.jupiter.api.Test;
+
 import schemacrawler.tools.databaseconnector.DatabaseConnector;
 import schemacrawler.tools.databaseconnector.DatabaseConnectorRegistry;
 
-public class BundledDistributionTest
-{
+public class BundledDistributionTest {
 
   @Test
-  public void testInformationSchema_sqlserver()
-    throws Exception
-  {
+  public void testInformationSchema_sqlserver() throws Exception {
 
     final Connection connection = null;
     final DatabaseConnectorRegistry registry =
-      DatabaseConnectorRegistry.getDatabaseConnectorRegistry();
+        DatabaseConnectorRegistry.getDatabaseConnectorRegistry();
     final DatabaseConnector databaseSystemIdentifier =
-      registry.findDatabaseConnectorFromDatabaseSystemIdentifier("sqlserver");
-    assertThat(databaseSystemIdentifier
-                 .getSchemaRetrievalOptionsBuilder(connection)
-                 .toOptions()
-                 .getInformationSchemaViews()
-                 .size(), is(11));
+        registry.findDatabaseConnectorFromDatabaseSystemIdentifier("sqlserver");
+    assertThat(
+        databaseSystemIdentifier
+            .getSchemaRetrievalOptionsBuilder(connection)
+            .toOptions()
+            .getInformationSchemaViews()
+            .size(),
+        is(10));
   }
 
   @Test
-  public void testPlugin_sqlserver()
-    throws Exception
-  {
+  public void testPlugin_sqlserver() throws Exception {
     final DatabaseConnectorRegistry registry =
-      DatabaseConnectorRegistry.getDatabaseConnectorRegistry();
+        DatabaseConnectorRegistry.getDatabaseConnectorRegistry();
     assertThat(registry.hasDatabaseSystemIdentifier("sqlserver"), is(true));
   }
-
 }
