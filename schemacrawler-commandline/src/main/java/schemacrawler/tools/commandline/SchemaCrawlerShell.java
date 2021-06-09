4e99b3fbbe6812d333b9e3afb96fc24891df9850
Sualeh Fatehi <sualeh@hotmail.com>
2021-06-08 20:26:46 -0400

    Refactor fatal error handling

--- schemacrawler-commandline/src/main/java/schemacrawler/tools/commandline/SchemaCrawlerShell.java
+++ schemacrawler-commandline/src/main/java/schemacrawler/tools/commandline/SchemaCrawlerShell.java
@@ -143,26 +143,28 @@ public final class SchemaCrawlerShell {
         }
       }
     } catch (final Throwable throwable) {
-      logSafeArguments(args);
-      logFullStackTrace(Level.SEVERE, throwable);
-
-      final String errorMessage;
-      if (throwable instanceof picocli.CommandLine.PicocliException) {
-        final Throwable cause = throwable.getCause();
-        if (cause != null && !isBlank(cause.getMessage())) {
-          errorMessage = cause.getMessage();
-        } else {
-          errorMessage = throwable.getMessage();
-        }
-      } else {
-        errorMessage = throwable.getMessage();
-      }
-
-      printCommandLineErrorMessage(errorMessage);
-
-      System.exit(1);
+      handleFatalError(args,throwable);
     }
   }
+
+private static void handleFatalError(final String[] args,final Throwable throwable){logSafeArguments(args);
+  logFullStackTrace(Level.SEVERE, throwable);
+
+  final String errorMessage;
+  if (throwable instanceof picocli.CommandLine.PicocliException) {
+    final Throwable cause = throwable.getCause();
+    if (cause != null && !isBlank(cause.getMessage())) {
+      errorMessage = cause.getMessage();
+    } else {
+      errorMessage = throwable.getMessage();
+    }
+  } else {
+    errorMessage = throwable.getMessage();
+  }
+
+  printCommandLineErrorMessage(errorMessage);
+
+  System.exit(1);}
 
   private SchemaCrawlerShell() {
     // Prevent instantiation
