068d769699570872229a6377f9f6e30c01e920aa
Sualeh Fatehi <sualeh@hotmail.com>
2021-07-16 15:54:49 -0400

    Add system classpath log test

---
+++ schemacrawler-commandline/src/test/java/schemacrawler/test/CommandLineLoggingUtilityLogSystemClasspathTest.java
@@ -0,0 +1,57 @@
+package schemacrawler.test;
+
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.matchesPattern;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import nl.altindag.log.LogCaptor;
+import schemacrawler.tools.commandline.utility.CommandLineLoggingUtility;
+
+public class CommandLineLoggingUtilityLogSystemClasspathTest {
+
+  private static LogCaptor logCaptor;
+
+  @BeforeAll
+  public static void setupLogCaptor() {
+    logCaptor = LogCaptor.forClass(CommandLineLoggingUtility.class);
+  }
+
+  @AfterAll
+  public static void tearDown() {
+    logCaptor.close();
+  }
+
+  @AfterEach
+  public void clearLogs() {
+    logCaptor.clearLogs();
+
+    logCaptor.setLogLevelToInfo();
+  }
+
+  @Test
+  public void logSystemClasspath_simple() {
+
+    CommandLineLoggingUtility.logSystemClasspath();
+
+    assertThat(logCaptor.getLogs(), hasSize(2));
+    assertThat(logCaptor.getInfoLogs().get(0), startsWith("Classpath:"));
+    assertThat(
+        logCaptor.getInfoLogs().get(1).replaceAll("\r", ""), matchesPattern("LD_LIBRARY_PATH: \n"));
+  }
+
+  @Test
+  public void logSystemClasspath_simple_noConfig() {
+
+    logCaptor.disableLogs();
+
+    CommandLineLoggingUtility.logSystemClasspath();
+
+    assertThat(logCaptor.getLogs(), hasSize(0));
+  }
+}
