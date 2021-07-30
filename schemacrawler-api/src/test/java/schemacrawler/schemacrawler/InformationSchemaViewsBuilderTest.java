bf29b1e5d20044c8bdd4d7c219b305bc9e1593ec
Sualeh Fatehi <sualeh@hotmail.com>
2021-07-30 17:30:44 -0400

    Add information schema views coverage test

--- schemacrawler-api/src/test/java/schemacrawler/schemacrawler/InformationSchemaViewsBuilderTest.java
+++ schemacrawler-api/src/test/java/schemacrawler/schemacrawler/InformationSchemaViewsBuilderTest.java
@@ -27,8 +27,15 @@ http://www.gnu.org/licenses/
 */
 package schemacrawler.schemacrawler;
 
+import static java.lang.System.lineSeparator;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+
+import java.sql.Connection;
+import java.util.function.BiConsumer;
 
 import org.junit.jupiter.api.Test;
 
@@ -50,5 +57,91 @@ public class InformationSchemaViewsBuilderTest {
             .getQuery(InformationSchemaKey.ADDITIONAL_COLUMN_ATTRIBUTES)
             .getQuery(),
         is("SOME SQL SELECT"));
+  }
+
+  @Test
+  public void fromResourceFolder() {
+
+    InformationSchemaViews informationSchemaViews;
+
+    informationSchemaViews =
+        InformationSchemaViewsBuilder.builder().fromResourceFolder(null).toOptions();
+
+    assertThat(informationSchemaViews.size(), is(0));
+
+    informationSchemaViews =
+        InformationSchemaViewsBuilder.builder().fromResourceFolder("/").toOptions();
+
+    assertThat(informationSchemaViews.size(), is(0));
+  }
+
+  @Test
+  public void newOptions() {
+
+    InformationSchemaViews informationSchemaViews;
+
+    informationSchemaViews = InformationSchemaViewsBuilder.newInformationSchemaViews();
+
+    assertThat(informationSchemaViews.size(), is(0));
+
+    informationSchemaViews = InformationSchemaViewsBuilder.builder().fromOptions(null).toOptions();
+
+    assertThat(informationSchemaViews.size(), is(0));
+
+    informationSchemaViews =
+        InformationSchemaViewsBuilder.builder().fromResourceFolder(null).toOptions();
+
+    assertThat(informationSchemaViews.size(), is(0));
+  }
+
+  @Test
+  public void substituteAll() {
+    final InformationSchemaViewsBuilder builder =
+        InformationSchemaViewsBuilder.builder()
+            .withSql(InformationSchemaKey.ADDITIONAL_COLUMN_ATTRIBUTES, "SOME SQL SELECT")
+            .withSql(
+                InformationSchemaKey.ADDITIONAL_TABLE_ATTRIBUTES,
+                "SOME ${key} SUBSTITUTE SQL SELECT");
+
+    builder.substituteAll("key", "value");
+
+    assertThat(
+        builder.toString(),
+        is(
+            lineSeparator()
+                + "ADDITIONAL_COLUMN_ATTRIBUTES: SOME SQL SELECT"
+                + lineSeparator()
+                + "ADDITIONAL_TABLE_ATTRIBUTES: SOME value SUBSTITUTE SQL SELECT"));
+  }
+
+  @Test
+  public void withFunction() {
+
+    final Connection connection = mock(Connection.class);
+    final InformationSchemaViewsBuilder builder = InformationSchemaViewsBuilder.builder();
+    final BiConsumer<InformationSchemaViewsBuilder, Connection> function =
+        (bldr, conn) -> {
+          throw new RuntimeException("Forced exception");
+        };
+
+    final RuntimeException exception =
+        assertThrows(RuntimeException.class, () -> builder.withFunction(function, connection));
+    assertThat(exception.getMessage(), is("Forced exception"));
+
+    assertDoesNotThrow(() -> builder.withFunction(null, connection));
+  }
+
+  @Test
+  public void withSql() {
+    final InformationSchemaViewsBuilder builder = InformationSchemaViewsBuilder.builder();
+
+    builder.withSql(InformationSchemaKey.ADDITIONAL_COLUMN_ATTRIBUTES, "SOME SQL SELECT");
+
+    assertThat(
+        builder.toString(), is(lineSeparator() + "ADDITIONAL_COLUMN_ATTRIBUTES: SOME SQL SELECT"));
+
+    builder.withSql(InformationSchemaKey.ADDITIONAL_COLUMN_ATTRIBUTES, null);
+
+    assertThat(builder.toString(), is(""));
   }
 }
