97f35b0f66ca9326ebf09738112145408e58b448
Sualeh Fatehi <sualeh@hotmail.com>
2021-10-30 16:06:23 -0400

    Show remarks for table constraints in output

--- schemacrawler-text/src/main/java/schemacrawler/tools/text/formatter/schema/SchemaTextFormatter.java
+++ schemacrawler-text/src/main/java/schemacrawler/tools/text/formatter/schema/SchemaTextFormatter.java
@@ -839,6 +839,7 @@ public final class SchemaTextFormatter extends BaseTabularFormatter<SchemaTextOp
         formattingHelper.writeEmptyRow();
         formattingHelper.writeNameRow(constraintName, constraintDetails);
 
+        printRemarks(constraint);
         if (!isBrief) {
           printTableColumns(constraint.getConstrainedColumns(), false);
         }
