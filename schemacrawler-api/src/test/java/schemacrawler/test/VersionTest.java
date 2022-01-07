d9824343bf3693c619deb6f80145765b07e02bc9
Sualeh Fatehi <sualeh@hotmail.com>
2022-01-07 13:44:24 -0500

    v16.16.7

--- schemacrawler-api/src/test/java/schemacrawler/test/VersionTest.java
+++ schemacrawler-api/src/test/java/schemacrawler/test/VersionTest.java
@@ -66,7 +66,7 @@ public class VersionTest {
   public void version() throws Exception {
     Version.main(new String[0]);
 
-    assertThat(out.getFileContents(), startsWith("SchemaCrawler 16.16.6"));
+    assertThat(out.getFileContents(), startsWith("SchemaCrawler 16.16.7"));
     assertThat(outputOf(err), hasNoContent());
   }
 }
