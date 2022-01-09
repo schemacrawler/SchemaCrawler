9704b05c9756cd54f2033cf857df33c664468991
Sualeh Fatehi <sualeh@hotmail.com>
2022-01-09 01:13:01 -0500

    Add MySQL tests for cross-database foreign keys and views

---
+++ schemacrawler-mysql/src/test/java/schemacrawler/integration/test/AcrossDatabaseTest.java
@@ -0,0 +1,120 @@
+/*
+========================================================================
+SchemaCrawler
+http://www.schemacrawler.com
+Copyright (c) 2000-2022, Sualeh Fatehi <sualeh@hotmail.com>.
+All rights reserved.
+------------------------------------------------------------------------
+
+SchemaCrawler is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+SchemaCrawler and the accompanying materials are made available under
+the terms of the Eclipse Public License v1.0, GNU General Public License
+v3 or GNU Lesser General Public License v3.
+
+You may elect to redistribute this code under any of these licenses.
+
+The Eclipse Public License is available at:
+http://www.eclipse.org/legal/epl-v10.html
+
+The GNU General Public License v3 and the GNU Lesser General Public
+License v3 are available at:
+http://www.gnu.org/licenses/
+
+========================================================================
+*/
+package schemacrawler.integration.test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static schemacrawler.integration.test.utility.MySQLTestUtility.newMySQLContainer8;
+import static schemacrawler.test.utility.ExecutableTestUtility.executableExecution;
+import static schemacrawler.test.utility.FileHasContent.classpathResource;
+import static schemacrawler.test.utility.FileHasContent.hasSameContentAs;
+import static schemacrawler.test.utility.FileHasContent.outputOf;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import schemacrawler.schemacrawler.InfoLevel;
+import schemacrawler.schemacrawler.LimitOptionsBuilder;
+import schemacrawler.schemacrawler.LoadOptionsBuilder;
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+import schemacrawler.schemacrawler.SchemaCrawlerOptionsBuilder;
+import schemacrawler.schemacrawler.SchemaInfoLevelBuilder;
+import schemacrawler.test.utility.BaseAdditionalDatabaseTest;
+import schemacrawler.test.utility.TestContext;
+import schemacrawler.test.utility.TestContextParameterResolver;
+import schemacrawler.test.utility.TestLoggingExtension;
+import schemacrawler.testdb.SqlScript;
+import schemacrawler.tools.command.text.schema.options.SchemaTextOptions;
+import schemacrawler.tools.command.text.schema.options.SchemaTextOptionsBuilder;
+import schemacrawler.tools.executable.SchemaCrawlerExecutable;
+
+@ExtendWith(TestLoggingExtension.class)
+@ExtendWith(TestContextParameterResolver.class)
+@Testcontainers(disabledWithoutDocker = true)
+@EnabledIfSystemProperty(named = "heavydb", matches = "^((?!(false|no)).)*$")
+public class AcrossDatabaseTest extends BaseAdditionalDatabaseTest {
+
+  @Container private final JdbcDatabaseContainer<?> dbContainer = newMySQLContainer8();
+
+  @Test
+  public void acrossDatabase(final TestContext testContext) throws Exception {
+
+    final LimitOptionsBuilder limitOptionsBuilder =
+        LimitOptionsBuilder.builder()
+            .includeSchemas(schema -> Arrays.asList("schema_a_a", "schema_a_b").contains(schema));
+    final SchemaInfoLevelBuilder schemaInfoLevelBuilder =
+        SchemaInfoLevelBuilder.builder()
+            .withTag("maximum-without-grants")
+            .withInfoLevel(InfoLevel.maximum)
+            .setRetrieveTablePrivileges(false)
+            .setRetrieveTableColumnPrivileges(false);
+    final LoadOptionsBuilder loadOptionsBuilder =
+        LoadOptionsBuilder.builder().withSchemaInfoLevel(schemaInfoLevelBuilder.toOptions());
+    final SchemaCrawlerOptions schemaCrawlerOptions =
+        SchemaCrawlerOptionsBuilder.newSchemaCrawlerOptions()
+            .withLimitOptions(limitOptionsBuilder.toOptions())
+            .withLoadOptions(loadOptionsBuilder.toOptions());
+
+    final SchemaTextOptionsBuilder textOptionsBuilder = SchemaTextOptionsBuilder.builder();
+    textOptionsBuilder.noInfo();
+    final SchemaTextOptions textOptions = textOptionsBuilder.toOptions();
+
+    final SchemaCrawlerExecutable executable = new SchemaCrawlerExecutable("details");
+    executable.setSchemaCrawlerOptions(schemaCrawlerOptions);
+    executable.setAdditionalConfiguration(SchemaTextOptionsBuilder.builder(textOptions).toConfig());
+
+    final String expectedResource = testContext.testMethodFullName();
+    assertThat(
+        outputOf(executableExecution(getConnection(), executable)),
+        hasSameContentAs(classpathResource(expectedResource)));
+  }
+
+  @BeforeEach
+  public void createDatabase() throws SQLException {
+    // IMPORTANT: Use root user, since permissions are not granted to the new databases created by
+    // the script. Also do not verify the server, and allow public key retrieval
+    final String connectionUrl =
+        dbContainer.getJdbcUrl()
+            + "?verifyServerCertificate=false"
+            + "&allowPublicKeyRetrieval=true";
+    createDataSource(connectionUrl, "root", dbContainer.getPassword());
+
+    // Note: The database connection needs to be closed for the new schemas to be recognized
+    try (Connection connection = getConnection()) {
+      SqlScript.executeScriptFromResource("/across-database.sql", connection);
+    }
+  }
+}
