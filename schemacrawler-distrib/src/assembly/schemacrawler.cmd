9583dce6de5b984cacac2cbd11bc49848f38d4b1
Sualeh Fatehi <sualeh@hotmail.com>
2022-01-22 19:53:28 -0500

    Move shell scripts to bin/ folder

--- schemacrawler-distrib/src/assembly/schemacrawler.cmd
+++ schemacrawler-distrib/src/assembly/schemacrawler.cmd
@@ -1,1 +1,1 @@
-@java "-Djava.library.path=%~dp0/lib/" -classpath "%~dp0/lib/*";"%~dp0/config";. schemacrawler.Main %*
+@java "-Djava.library.path=%~dp0/../lib/" -classpath "%~dp0/../lib/*";"%~dp0/../config";. schemacrawler.Main %*
--- schemacrawler-distrib/src/assembly/schemacrawler.sh
+++ schemacrawler-distrib/src/assembly/schemacrawler.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-SC_DIR=$(dirname "$0")
+SC_DIR=$(dirname "$0")/..
 java -cp "$SC_DIR"/lib/*:"$SC_DIR"/config schemacrawler.Main "$@"
