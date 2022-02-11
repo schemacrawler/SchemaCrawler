6633cdd187ab57ed3288ea337744e09148e6dc7c
Sualeh Fatehi <sualeh@hotmail.com>
2022-02-11 13:46:39 -0500

    Add support for hidden or invisible columns in MySQL

--- schemacrawler-mysql/src/test/java/schemacrawler/integration/test/BundledDistributionTest.java
+++ schemacrawler-mysql/src/test/java/schemacrawler/integration/test/BundledDistributionTest.java
@@ -27,43 +27,39 @@ http://www.gnu.org/licenses/
 */
 package schemacrawler.integration.test;
 
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import java.sql.Connection;
 
 import org.junit.jupiter.api.Test;
+
 import schemacrawler.tools.databaseconnector.DatabaseConnector;
 import schemacrawler.tools.databaseconnector.DatabaseConnectorRegistry;
 
-public class BundledDistributionTest
-{
+public class BundledDistributionTest {
 
   @Test
-  public void testInformationSchema_mysql()
-    throws Exception
-  {
+  public void testInformationSchema_mysql() throws Exception {
 
     final Connection connection = null;
     final DatabaseConnectorRegistry registry =
-      DatabaseConnectorRegistry.getDatabaseConnectorRegistry();
+        DatabaseConnectorRegistry.getDatabaseConnectorRegistry();
     final DatabaseConnector databaseSystemIdentifier =
-      registry.findDatabaseConnectorFromDatabaseSystemIdentifier("mysql");
-    assertThat(databaseSystemIdentifier
-                 .getSchemaRetrievalOptionsBuilder(connection)
-                 .toOptions()
-                 .getInformationSchemaViews()
-                 .size(), is(12));
+        registry.findDatabaseConnectorFromDatabaseSystemIdentifier("mysql");
+    assertThat(
+        databaseSystemIdentifier
+            .getSchemaRetrievalOptionsBuilder(connection)
+            .toOptions()
+            .getInformationSchemaViews()
+            .size(),
+        is(13));
   }
 
   @Test
-  public void testPlugin_mysql()
-    throws Exception
-  {
+  public void testPlugin_mysql() throws Exception {
     final DatabaseConnectorRegistry registry =
-      DatabaseConnectorRegistry.getDatabaseConnectorRegistry();
+        DatabaseConnectorRegistry.getDatabaseConnectorRegistry();
     assertThat(registry.hasDatabaseSystemIdentifier("mysql"), is(true));
   }
-
 }
