da134e71102ca879744e6764cdd11d787a835820
Sualeh Fatehi <sualeh@hotmail.com>
2022-03-01 18:58:08 -0500

    Add tests for issue #628 - fix #628

--- schemacrawler-oracle/src/test/java/schemacrawler/integration/test/Issue628Test.java
+++ schemacrawler-oracle/src/test/java/schemacrawler/integration/test/Issue628Test.java
@@ -27,18 +27,14 @@ http://www.gnu.org/licenses/
 */
 package schemacrawler.integration.test;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static schemacrawler.integration.test.utility.OracleTestUtility.newOracle21Container;
 import static schemacrawler.test.utility.ExecutableTestUtility.executableExecution;
 import static schemacrawler.test.utility.FileHasContent.classpathResource;
 import static schemacrawler.test.utility.FileHasContent.hasSameContentAs;
 import static schemacrawler.test.utility.FileHasContent.outputOf;
-import static schemacrawler.test.utility.TestUtility.javaVersion;
-import static schemacrawler.tools.utility.SchemaCrawlerUtility.getCatalog;
 
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.sql.Statement;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -46,40 +42,28 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.testcontainers.containers.JdbcDatabaseContainer;
-import org.testcontainers.containers.OracleContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
 import schemacrawler.inclusionrule.RegularExpressionInclusionRule;
 import schemacrawler.schemacrawler.LimitOptionsBuilder;
-import schemacrawler.schemacrawler.LoadOptionsBuilder;
 import schemacrawler.schemacrawler.SchemaCrawlerOptions;
 import schemacrawler.schemacrawler.SchemaCrawlerOptionsBuilder;
-import schemacrawler.schemacrawler.SchemaInfoLevelBuilder;
-import schemacrawler.schemacrawler.exceptions.ExecutionRuntimeException;
-import schemacrawler.test.utility.BaseAdditionalDatabaseTest;
 import schemacrawler.tools.command.text.schema.options.SchemaTextOptions;
 import schemacrawler.tools.command.text.schema.options.SchemaTextOptionsBuilder;
 import schemacrawler.tools.executable.SchemaCrawlerExecutable;
+import us.fatehi.utility.database.SqlScript;
 
 @Testcontainers(disabledWithoutDocker = true)
 @EnabledIfSystemProperty(named = "heavydb", matches = "^((?!(false|no)).)*$")
-public class WithoutPluginOracleTest extends BaseAdditionalDatabaseTest {
+public class Issue628Test extends BaseOracleWithConnectionTest {
 
-  final DockerImageName imageName = DockerImageName.parse("gvenzl/oracle-xe");
-
-  @Container
-  private final JdbcDatabaseContainer<?> dbContainer =
-      new OracleContainer(imageName.withTag("21-slim"));
+  @Container private final JdbcDatabaseContainer<?> dbContainer = newOracle21Container();
 
   @BeforeEach
   public void createDatabase() {
-
     final String urlx = "restrictGetTables=true;useFetchSizeWithLongColumn=true";
     createDataSource(dbContainer.getJdbcUrl(), "SYS AS SYSDBA", dbContainer.getPassword(), urlx);
-
-    createDatabase("/oracle.scripts.txt");
   }
 
   @Test
@@ -87,13 +71,16 @@ public class WithoutPluginOracleTest extends BaseAdditionalDatabaseTest {
   public void slashedName() throws Exception {
 
     final Connection connection = getConnection();
+    SqlScript.executeScriptFromResource(";,/db/books/01_schemas_C.sql", connection);
     try (final Statement stmt = connection.createStatement()) {
       stmt.execute("CREATE TABLE \"A/B\" (I INT)");
       stmt.execute("CREATE TABLE CD (\"E/F\" INT)");
       stmt.execute("CREATE TABLE \"G.H\" (J INT)");
-      stmt.execute("CREATE TABLE \"KL\" (\"M/N\" INT)");
+      stmt.execute("CREATE TABLE \"KL\" (\"M.N\" INT)");
       // Auto-commited
     }
+
+    final String expectedResource = "slashedName.txt";
 
     final LimitOptionsBuilder limitOptionsBuilder =
         LimitOptionsBuilder.builder()
@@ -103,44 +90,17 @@ public class WithoutPluginOracleTest extends BaseAdditionalDatabaseTest {
         SchemaCrawlerOptionsBuilder.newSchemaCrawlerOptions()
             .withLimitOptions(limitOptionsBuilder.toOptions());
 
-    final ExecutionRuntimeException executionRuntimeException =
-        assertThrows(
-            ExecutionRuntimeException.class, () -> getCatalog(connection, schemaCrawlerOptions));
-    final SQLException cause = (SQLException) executionRuntimeException.getCause().getCause();
-    assertThat(
-        cause.getSQLState(),
-        is("22025")); // ORA-01424: missing or illegal character following the escape character
-  }
-
-  @Test
-  public void testOracleWithConnection() throws Exception {
-    final LimitOptionsBuilder limitOptionsBuilder =
-        LimitOptionsBuilder.builder()
-            .includeSchemas(new RegularExpressionInclusionRule("BOOKS"))
-            .includeAllSequences()
-            .includeAllSynonyms()
-            .includeTables(table -> !table.contains("Global")) // NOTE: Index retrieval fails
-            .includeRoutines(new RegularExpressionInclusionRule("[0-9a-zA-Z_\\.]*"))
-            .tableTypes("TABLE,VIEW,MATERIALIZED VIEW");
-    final LoadOptionsBuilder loadOptionsBuilder =
-        LoadOptionsBuilder.builder().withSchemaInfoLevel(SchemaInfoLevelBuilder.maximum());
-    final SchemaCrawlerOptions schemaCrawlerOptions =
-        SchemaCrawlerOptionsBuilder.newSchemaCrawlerOptions()
-            .withLimitOptions(limitOptionsBuilder.toOptions())
-            .withLoadOptions(loadOptionsBuilder.toOptions());
-
     final SchemaTextOptionsBuilder textOptionsBuilder = SchemaTextOptionsBuilder.builder();
-    textOptionsBuilder.showDatabaseInfo().showJdbcDriverInfo();
+    textOptionsBuilder.noInfo();
     final SchemaTextOptions textOptions = textOptionsBuilder.toOptions();
 
-    final SchemaCrawlerExecutable executable = new SchemaCrawlerExecutable("details");
+    final SchemaCrawlerExecutable executable = new SchemaCrawlerExecutable("schema");
     executable.setSchemaCrawlerOptions(schemaCrawlerOptions);
     executable.setAdditionalConfiguration(SchemaTextOptionsBuilder.builder(textOptions).toConfig());
 
     // -- Schema output tests
-    final String expectedResource = String.format("testOracleWithConnection.%s.txt", javaVersion());
     assertThat(
-        outputOf(executableExecution(getConnection(), executable)),
+        outputOf(executableExecution(connection, executable)),
         hasSameContentAs(classpathResource(expectedResource)));
   }
 }
