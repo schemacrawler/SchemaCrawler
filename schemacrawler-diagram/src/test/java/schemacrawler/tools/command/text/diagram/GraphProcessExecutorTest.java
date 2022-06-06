12ba770775c3cf660f570d013929cc8bb52c54cc
Sualeh Fatehi <sualeh@hotmail.com>
2022-06-06 17:04:37 -0400

    Add test coverage

---
+++ schemacrawler-diagram/src/test/java/schemacrawler/tools/command/text/diagram/GraphProcessExecutorTest.java
@@ -0,0 +1,114 @@
+package schemacrawler.tools.command.text.diagram;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import schemacrawler.schemacrawler.exceptions.IORuntimeException;
+import schemacrawler.test.utility.TestOutputStream;
+import schemacrawler.tools.command.text.diagram.options.DiagramOutputFormat;
+import us.fatehi.utility.IOUtility;
+
+public class GraphProcessExecutorTest {
+
+  private final class TestGraphProcessExecutor extends AbstractGraphProcessExecutor {
+    private TestGraphProcessExecutor(
+        final Path dotFile, final Path outputFile, final DiagramOutputFormat diagramOutputFormat) {
+      super(dotFile, outputFile, diagramOutputFormat);
+    }
+
+    @Override
+    public boolean canGenerate() {
+      return false;
+    }
+
+    @Override
+    public void run() {}
+  }
+
+  private TestOutputStream err;
+  private TestOutputStream out;
+
+  @AfterEach
+  public void cleanUpStreams() {
+    System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+    System.setErr(new PrintStream(new FileOutputStream(FileDescriptor.err)));
+  }
+
+  @Test
+  public void constructorGraphProcessExecutor() throws IOException {
+
+    final Path dotFile = Files.createTempFile("", "");
+    Files.write(dotFile, "hello, world".getBytes());
+
+    final Path outputFile = IOUtility.createTempFilePath("", "");
+
+    final DiagramOutputFormat diagramOutputFormat = DiagramOutputFormat.png;
+
+    // Assert no errors
+    assertThat(
+        new TestGraphProcessExecutor(dotFile, outputFile, diagramOutputFormat),
+        is(not(nullValue())));
+
+    // DOT file not readable
+    final IORuntimeException exception1 =
+        assertThrows(
+            IORuntimeException.class,
+            () ->
+                new TestGraphProcessExecutor(
+                    IOUtility.createTempFilePath("sc", "data"), outputFile, diagramOutputFormat));
+    assertThat(exception1.getMessage(), containsString("Cannot read DOT file"));
+
+    // Output file not writable
+    final IORuntimeException exception2 =
+        assertThrows(
+            IORuntimeException.class,
+            () ->
+                new TestGraphProcessExecutor(
+                    dotFile, Paths.get("/unwritable_path"), diagramOutputFormat));
+    assertThat(exception2.getMessage(), containsString("Cannot write output file"));
+  }
+
+  @Test
+  public void graphvizProcessExecutorError() throws IOException {
+
+    final Path dotFile = Files.createTempFile("", "");
+    Files.write(dotFile, "hello, world".getBytes());
+
+    final Path outputFile = IOUtility.createTempFilePath("", "");
+
+    final DiagramOutputFormat diagramOutputFormat = DiagramOutputFormat.png;
+
+    // Graphviz error due to bad input
+    final GraphvizProcessExecutor processExecutor =
+        new GraphvizProcessExecutor(
+            dotFile, outputFile, diagramOutputFormat, Collections.emptyList());
+    processExecutor.run();
+    assertThat(err.getContents(), containsString("syntax error in line 1 near 'hello'"));
+  }
+
+  @BeforeEach
+  public void setUpStreams() throws Exception {
+    out = new TestOutputStream();
+    System.setOut(new PrintStream(out));
+
+    err = new TestOutputStream();
+    System.setErr(new PrintStream(err));
+  }
+}
---
+++ schemacrawler-diagram/src/test/java/schemacrawler/tools/command/text/diagram/GraphvizJavaExecutorUtilityTest.java
@@ -0,0 +1,67 @@
+package schemacrawler.tools.command.text.diagram;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static schemacrawler.tools.command.text.diagram.options.DiagramOutputFormat.plain;
+import static schemacrawler.tools.command.text.diagram.options.DiagramOutputFormat.png;
+import static schemacrawler.tools.command.text.diagram.options.DiagramOutputFormat.ps;
+import static schemacrawler.tools.command.text.diagram.options.DiagramOutputFormat.svg;
+import static schemacrawler.tools.command.text.diagram.options.DiagramOutputFormat.xdot;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.Test;
+
+import schemacrawler.schemacrawler.exceptions.ExecutionRuntimeException;
+import schemacrawler.tools.command.text.diagram.options.DiagramOutputFormat;
+import us.fatehi.utility.IOUtility;
+
+public class GraphvizJavaExecutorUtilityTest {
+
+  @Test
+  public void graphvizJavaSupportedFormats() throws IOException {
+
+    final Path dotFile = Files.createTempFile("", "");
+    Files.write(dotFile, "hello, world".getBytes());
+
+    final Path outputFile = IOUtility.createTempFilePath("", "");
+
+    for (final DiagramOutputFormat diagramOutputFormat :
+        new DiagramOutputFormat[] {svg, png, ps, xdot, plain}) {
+      final ExecutionRuntimeException runtimeException =
+          assertThrows(
+              ExecutionRuntimeException.class,
+              () ->
+                  GraphvizJavaExecutorUtility.generateGraph(
+                      dotFile, outputFile, diagramOutputFormat));
+      assertThat(
+          runtimeException.getMessage(),
+          containsString("Error: syntax error in line 1 near 'hello'"));
+    }
+  }
+
+  @Test
+  public void graphvizJavaUnsupportedFormat() throws IOException {
+
+    final Path dotFile = Files.createTempFile("", "");
+    Files.write(dotFile, "hello, world".getBytes());
+
+    final Path outputFile = IOUtility.createTempFilePath("", "");
+
+    final DiagramOutputFormat diagramOutputFormat = DiagramOutputFormat.cgimage;
+    final Throwable runtimeException =
+        assertThrows(
+                ExecutionRuntimeException.class,
+                () ->
+                    GraphvizJavaExecutorUtility.generateGraph(
+                        dotFile, outputFile, diagramOutputFormat))
+            .getCause();
+    assertThat(
+        runtimeException.getMessage(),
+        is("Unsupported output format <[cgimage] CGImage bitmap format>"));
+  }
+}
