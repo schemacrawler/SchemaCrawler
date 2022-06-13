201331a3b550279d5df96d086eb74446bde2a0df
Sualeh Fatehi <sualeh@hotmail.com>
2022-06-13 17:12:37 -0400

    Add LintUtility coverage

---
+++ schemacrawler-lint/src/test/java/schemacrawler/test/LintUtilityTest.java
@@ -0,0 +1,29 @@
+package schemacrawler.test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import schemacrawler.tools.lint.LintUtility;
+
+public class LintUtilityTest {
+
+  @Test
+  public void lintUtility() {
+
+    assertThat(LintUtility.listStartsWith(null, new ArrayList<>()), is(false));
+    assertThat(LintUtility.listStartsWith(new ArrayList<>(), null), is(false));
+    assertThat(LintUtility.listStartsWith(null, null), is(false));
+
+    assertThat(LintUtility.listStartsWith(new ArrayList<>(), new ArrayList<>()), is(true));
+    assertThat(LintUtility.listStartsWith(new ArrayList<>(), Arrays.asList("1", "2")), is(false));
+    assertThat(LintUtility.listStartsWith(Arrays.asList("1"), Arrays.asList("1", "2")), is(false));
+    assertThat(LintUtility.listStartsWith(Arrays.asList("1", "2"), Arrays.asList("1")), is(true));
+
+    assertThat(LintUtility.listStartsWith(Arrays.asList("3", "4"), Arrays.asList("1")), is(false));
+  }
+}
