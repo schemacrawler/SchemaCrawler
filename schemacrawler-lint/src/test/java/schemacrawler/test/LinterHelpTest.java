cea2e5630d5949f62d072dd14c6b45d4dc32863c
Sualeh Fatehi <sualeh@hotmail.com>
2022-06-13 17:21:34 -0400

    Add coverage for linter help

---
+++ schemacrawler-lint/src/test/java/schemacrawler/test/LinterHelpTest.java
@@ -0,0 +1,45 @@
+package schemacrawler.test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static schemacrawler.test.utility.FileHasContent.classpathResource;
+import static schemacrawler.test.utility.FileHasContent.hasSameContentAs;
+import static schemacrawler.test.utility.FileHasContent.outputOf;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import schemacrawler.test.utility.ResolveTestContext;
+import schemacrawler.test.utility.TestContext;
+import schemacrawler.test.utility.TestWriter;
+import schemacrawler.tools.lint.LinterHelp;
+
+@ResolveTestContext
+public class LinterHelpTest {
+
+  @Test
+  public void markdown(final TestContext testContext) throws IOException {
+
+    final String[] strings = new LinterHelp(true).get();
+
+    final TestWriter testout = new TestWriter();
+    try (final TestWriter out = testout) {
+      out.write(String.join("\n", strings));
+    }
+    assertThat(
+        outputOf(testout), hasSameContentAs(classpathResource(testContext.testMethodFullName())));
+  }
+
+  @Test
+  public void text(final TestContext testContext) throws IOException {
+
+    final String[] strings = new LinterHelp().get();
+
+    final TestWriter testout = new TestWriter();
+    try (final TestWriter out = testout) {
+      out.write(String.join("\n", strings));
+    }
+    assertThat(
+        outputOf(testout), hasSameContentAs(classpathResource(testContext.testMethodFullName())));
+  }
+}
