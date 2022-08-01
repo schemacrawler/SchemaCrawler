bff574c263db7aa317445ad24dd2297ab43b7986
Sualeh Fatehi <sualeh@hotmail.com>
2022-08-01 18:43:57 -0400

    Add output file argument to script

--- schemacrawler-distrib/src/examples/resources/examples/dump/dump.cmd
+++ schemacrawler-distrib/src/examples/resources/examples/dump/dump.cmd
@@ -1,4 +1,2 @@
 @echo off
-del /q /f database-dump.html
-call ..\..\_schemacrawler\bin\schemacrawler.cmd --server=hsqldb --database=schemacrawler --user=sa --password= --info-level=standard -c=dump --output-format=html -o=database-dump.html %*
-echo Database dump is in database-dump.html
+..\..\_schemacrawler\bin\schemacrawler.cmd --server=hsqldb --database=schemacrawler --user=sa --password= --info-level=standard -c=dump --output-format=html -o %1
