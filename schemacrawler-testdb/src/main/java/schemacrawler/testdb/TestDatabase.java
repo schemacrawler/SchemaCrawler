b322d613ccab6626d0fad9dca3fe196c1f491244
Sualeh Fatehi <sualeh@hotmail.com>
2022-08-21 14:32:25 -0400

    Throw exception for test database exceptions

--- schemacrawler-testdb/src/main/java/schemacrawler/testdb/TestDatabase.java
+++ schemacrawler-testdb/src/main/java/schemacrawler/testdb/TestDatabase.java
@@ -64,9 +64,10 @@ public class TestDatabase {
       testDatabase.start();
       return testDatabase;
     } catch (final Exception e) {
-      e.printStackTrace();
-      System.exit(1);
-      return null;
+      throw new RuntimeException("Could not initialize test database", e);
+      //      e.printStackTrace();
+      //      System.exit(1);
+      //      return null;
     }
   }
 
@@ -80,9 +81,10 @@ public class TestDatabase {
       testDatabase.start();
       return testDatabase;
     } catch (final Exception e) {
-      e.printStackTrace();
-      System.exit(1);
-      return null;
+      throw new RuntimeException("Could not initialize test database", e);
+      //      e.printStackTrace();
+      //      System.exit(1);
+      //      return null;
     }
   }
 
