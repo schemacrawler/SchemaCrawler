1025baf2b487d8428c62ceb89bc43ec7cd08add0
Sualeh Fatehi <sualeh@hotmail.com>
2022-10-14 19:37:35 -0400

    Disable logging, and logging tests

--- schemacrawler-commandline/src/test/java/schemacrawler/test/CommandLineLoggingUtilityLogSystemInformationTest.java
+++ schemacrawler-commandline/src/test/java/schemacrawler/test/CommandLineLoggingUtilityLogSystemInformationTest.java
@@ -7,11 +7,13 @@ import static org.hamcrest.Matchers.matchesPattern;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import nl.altindag.log.LogCaptor;
 import schemacrawler.tools.commandline.utility.CommandLineLoggingUtility;
 
+@Disabled
 public class CommandLineLoggingUtilityLogSystemInformationTest {
 
   private LogCaptor logCaptor;
