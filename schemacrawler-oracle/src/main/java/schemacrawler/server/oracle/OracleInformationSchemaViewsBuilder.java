d9ca5d930f425d733c3e7fedf8afafa2598bca90
Sualeh Fatehi <sualeh@hotmail.com>
2022-10-29 22:22:33 +0530

    Fix #892 - Ignored exception is logged as warning

--- schemacrawler-oracle/src/main/java/schemacrawler/server/oracle/OracleInformationSchemaViewsBuilder.java
+++ schemacrawler-oracle/src/main/java/schemacrawler/server/oracle/OracleInformationSchemaViewsBuilder.java
@@ -1,15 +1,15 @@
 package schemacrawler.server.oracle;
 
-import static schemacrawler.schemacrawler.QueryUtility.executeForScalar;
+import static us.fatehi.utility.database.DatabaseUtility.createStatement;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.function.BiConsumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import schemacrawler.schemacrawler.InformationSchemaViewsBuilder;
-import schemacrawler.schemacrawler.Query;
 import us.fatehi.utility.string.StringFormat;
 
 class OracleInformationSchemaViewsBuilder
@@ -18,17 +18,13 @@ class OracleInformationSchemaViewsBuilder
   private static final Logger LOGGER = Logger.getLogger(OracleDatabaseConnector.class.getName());
 
   private static String getCatalogScope(final Connection connection) {
-    String catalogScope = "ALL";
-    try {
-      final Query query =
-          new Query(
-              "Check access to DBA tables", "SELECT TABLE_NAME FROM DBA_TABLES WHERE ROWNUM = 1");
-      final Object scalar = executeForScalar(query, connection);
-      if (scalar != null) {
-        catalogScope = "DBA";
-      }
+    final String sql = "SELECT TABLE_NAME FROM DBA_TABLES WHERE ROWNUM = 1";
+    String catalogScope;
+    try (final Statement statement = createStatement(connection); ) {
+      statement.execute(sql);
+      catalogScope = "DBA";
     } catch (final SQLException e) {
-      LOGGER.log(Level.FINE, "Could not check access to DBA tables", e);
+      LOGGER.log(Level.FINE, "Could not access DBA tables", e);
       catalogScope = "ALL";
     }
 
