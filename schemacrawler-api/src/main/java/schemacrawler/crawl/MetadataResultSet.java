5216523c6bd42d5f3c2143db1011bc92c7032f0e
Sualeh Fatehi <sualeh@hotmail.com>
2023-01-12 15:09:12 -0500

    Revert workaround for older Cassandra JDBC driver

--- schemacrawler-api/src/main/java/schemacrawler/crawl/MetadataResultSet.java
+++ schemacrawler-api/src/main/java/schemacrawler/crawl/MetadataResultSet.java
@@ -272,7 +272,7 @@ public final class MetadataResultSet implements AutoCloseable {
                   "NULL int value for column <%s>, so using default %d", columnName, defaultValue));
           value = defaultValue;
         }
-      } catch (final NumberFormatException | SQLException e) {
+      } catch (final SQLException e) {
         LOGGER.log(
             Level.WARNING,
             e,
@@ -303,7 +303,7 @@ public final class MetadataResultSet implements AutoCloseable {
                   columnName, defaultValue));
           value = defaultValue;
         }
-      } catch (final NumberFormatException | SQLException e) {
+      } catch (final SQLException e) {
         LOGGER.log(
             Level.WARNING,
             e,
@@ -334,7 +334,7 @@ public final class MetadataResultSet implements AutoCloseable {
                   columnName, defaultValue));
           value = defaultValue;
         }
-      } catch (final NumberFormatException | SQLException e) {
+      } catch (final SQLException e) {
         LOGGER.log(
             Level.WARNING,
             e,
