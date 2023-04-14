457578df5126b7dc37f927f628e0ffbf16f5e1c3
Sualeh Fatehi <sualeh@hotmail.com>
2023-04-14 06:47:59 -0400

    Refactor into smaller methods

--- schemacrawler-commandline/src/main/java/schemacrawler/tools/commandline/SchemaCrawlerCommandLine.java
+++ schemacrawler-commandline/src/main/java/schemacrawler/tools/commandline/SchemaCrawlerCommandLine.java
@@ -71,18 +71,7 @@ public final class SchemaCrawlerCommandLine {
       final CommandLineLogger commandLineLogger = new CommandLineLogger(LOGGER);
       commandLineLogger.logState(state);
 
-      final String errorMessage;
-      if (throwable instanceof picocli.CommandLine.PicocliException) {
-        final Throwable cause = throwable.getCause();
-        if (cause != null && !isBlank(cause.getMessage())) {
-          errorMessage = cause.getMessage();
-        } else {
-          errorMessage = throwable.getMessage();
-        }
-      } else {
-        errorMessage = throwable.getMessage();
-      }
-
+      final String errorMessage = extractErrorMessage(throwable);
       printCommandLineErrorMessage(errorMessage, state);
 
       return 1;
@@ -94,14 +83,27 @@ public final class SchemaCrawlerCommandLine {
   private static void executeCommandLine(final CommandLine commandLine) {
     final Map<String, Object> subcommands = commandLine.getMixins();
 
-    for (final String commandName :
-        new String[] {
-          "log", "configfile", "connect", "limit", "grep", "filter", "showstate", "load", "execute"
-        }) {
+    for (final String commandName : new String[] {"log", "configfile", "connect", "limit", "grep",
+        "filter", "showstate", "load", "execute"}) {
       final Runnable command = (Runnable) subcommands.get(commandName);
       LOGGER.log(Level.INFO, "Running command " + command.getClass().getSimpleName());
       command.run();
     }
+  }
+
+  private static String extractErrorMessage(final Throwable throwable) {
+    final String errorMessage;
+    if (throwable instanceof picocli.CommandLine.PicocliException) {
+      final Throwable cause = throwable.getCause();
+      if (cause != null && !isBlank(cause.getMessage())) {
+        errorMessage = cause.getMessage();
+      } else {
+        errorMessage = throwable.getMessage();
+      }
+    } else {
+      errorMessage = throwable.getMessage();
+    }
+    return errorMessage;
   }
 
   private SchemaCrawlerCommandLine() {
