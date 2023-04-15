6c532c66c50185a037e628cbf10247f62744d8b1
Sualeh Fatehi <sualeh@hotmail.com>
2023-04-15 13:13:57 -0400

    Create an empty file if there are no test failures

--- .github/scripts/unit-tests-results-output.sh
+++ .github/scripts/unit-tests-results-output.sh
@@ -3,11 +3,18 @@ dir_name="unit_tests_results_output"
 zip_file="unit_tests_results_output.zip"
 temp_dir=$(mktemp -d)
 
-find . -type d -name "$dir_name" -exec cp -r {} "$temp_dir" \;
+# Use the find command to search for directories with the given name,
+# and copy each directory to the temporary directory.
+if ! find . -type d -name "$dir_name" -exec cp -r {} "$temp_dir" \; ; then
+  # If there are no matching directories, create an empty zip file.
+  touch "$zip_file"
+else
+  # If there are matching directories, create a zip file containing the directories.
+  cd "$temp_dir"
+  zip -r "$zip_file" .
+  mv "$zip_file" ..
+fi
 
-cd "$temp_dir"
-zip -r "$zip_file" .
-
-mv "$zip_file" ..
+# Clean up the temporary directory.
 cd ..
 rm -rf "$temp_dir"
