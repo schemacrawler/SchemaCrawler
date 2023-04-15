bd6ecc096747df020c1c641c435f12ab23de6216
Sualeh Fatehi <sualeh@hotmail.com>
2023-04-15 13:04:31 -0400

    Use find -exec

--- .github/scripts/unit-tests-results-output.sh
+++ .github/scripts/unit-tests-results-output.sh
@@ -3,9 +3,7 @@ dir_name="unit_tests_results_output"
 zip_file="unit_tests_results_output.zip"
 temp_dir=$(mktemp -d)
 
-for dir in $(find . -type d -name "$dir_name"); do
-  cp -r "$dir" "$temp_dir"
-done
+find . -type d -name "$dir_name" -exec cp -r {} "$temp_dir" \;
 
 cd "$temp_dir"
 zip -r "$zip_file" .
