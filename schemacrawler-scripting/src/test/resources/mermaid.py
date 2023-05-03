cd04e23325b98dc35cd6a8b42375d00e775f1cd7
Sualeh Fatehi <sualeh@hotmail.com>
2023-05-03 17:07:46 -0400

    Enhance Mermaid diagram generation with remarks and key designations (#1107)
    * Add columns remarks to Mermaid erDiagram
    
    * Add primary and foreign key designations

--- schemacrawler-scripting/src/test/resources/mermaid.py
+++ schemacrawler-scripting/src/test/resources/mermaid.py
@@ -1,16 +1,37 @@
+from __future__ import print_function
 import re
+
+
+# Mermaid only allows alphanumeric identifiers
+def cleanname(name):
+    namepattern = r'[^0-9a-zA-Z]'
+    cleanedname = re.sub(namepattern, '', name)
+    if not cleanedname:
+        cleanedname = "UNKNOWN"
+    return cleanedname
+
 
 print('erDiagram')
 print('')
 for table in catalog.tables:
-    print('  ' + re.sub(r'\.', '-', table.fullName) + ' {')
+    print('  ' + cleanname(table.fullName) + ' {')
     for column in table.columns:
-        print('    ' + re.sub(r'\([\d ,]+\)|\[[\d ,]+\]|\s+', '',
-                              column.columnDataType.name) + ' ' + column.name)
+        print('    ' + cleanname(column.columnDataType.name) + ' ' + cleanname(column.name),
+              end='')
+        if column.isPartOfPrimaryKey():
+            print(' PK', end='')
+        elif column.isPartOfForeignKey():
+            print(' FK', end='')
+        elif column.isPartOfUniqueIndex():
+            print(' UK', end='')
+        if column.hasRemarks():
+            print(' "' + ' '.join(column.remarks.splitlines()) + '"',
+                  end='')
+        print()
     print('  }')
     print('')
 
 for table in catalog.tables:
     for childTable in table.referencingTables:
-        print('  ' + re.sub(r'\.', '-', table.fullName) + ' ||--o{ ' +
-              re.sub(r'\.', '-', childTable.fullName) + ' : "foreign key"')
+        print('  ' + cleanname(table.fullName) + ' ||--o{ ' +
+              cleanname(childTable.fullName) + ' : "foreign key"')
