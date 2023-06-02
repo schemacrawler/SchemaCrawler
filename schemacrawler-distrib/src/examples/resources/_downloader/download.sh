2ec119894ea6d6c01e354297fc0f044d89cee375
Sualeh Fatehi <sualeh@hotmail.com>
2023-06-02 19:42:35 -0400

    Fix issue #1146

--- schemacrawler-distrib/src/examples/resources/_downloader/download.sh
+++ schemacrawler-distrib/src/examples/resources/_downloader/download.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo Downloading "$1"
-java -jar ivy-2.5.0.jar -ivy "$1"_ivy.xml -settings ivysettings.xml -retrieve "../_schemacrawler/lib/[artifact]-[revision]-[type].[ext]"
+java -jar ivy-2.5.1.jar -ivy "$1"_ivy.xml -settings ivysettings.xml -retrieve "../_schemacrawler/lib/[artifact]-[revision]-[type].[ext]"
