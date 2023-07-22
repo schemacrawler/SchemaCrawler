5e0270a1f26aa4a4c04685f5149c3d6fd20dd546
Sualeh Fatehi <sualeh@hotmail.com>
2023-07-22 10:34:59 -0400

    Add function type to prepare for system functions

--- schemacrawler-chatgpt/src/main/java/schemacrawler/tools/command/chatgpt/FunctionDefinition.java
+++ schemacrawler-chatgpt/src/main/java/schemacrawler/tools/command/chatgpt/FunctionDefinition.java
@@ -28,17 +28,27 @@ http://www.gnu.org/licenses/
 
 package schemacrawler.tools.command.chatgpt;
 
+import static schemacrawler.tools.command.chatgpt.FunctionDefinition.FunctionType.USER;
 import java.sql.Connection;
 import java.util.function.Function;
 import schemacrawler.schema.Catalog;
 
 public interface FunctionDefinition<P extends FunctionParameters> {
 
+  public enum FunctionType {
+    USER,
+    SYSTEM;
+  }
+
   Catalog getCatalog();
 
   String getDescription();
 
   Function<P, FunctionReturn> getExecutor();
+
+  default FunctionType getFunctionType() {
+    return USER;
+  }
 
   default String getName() {
     return this.getClass().getSimpleName();
