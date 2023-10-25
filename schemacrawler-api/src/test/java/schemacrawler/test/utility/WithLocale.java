7c6c6081f875eb12076389e220cff6af9f1cedcf
Sualeh Fatehi <sualeh@hotmail.com>
2023-10-25 22:00:38 +0200

    Run tests in a locale specific way

---
+++ schemacrawler-api/src/test/java/schemacrawler/test/utility/WithLocale.java
@@ -0,0 +1,43 @@
+/*
+========================================================================
+SchemaCrawler
+http://www.schemacrawler.com
+Copyright (c) 2000-2023, Sualeh Fatehi <sualeh@hotmail.com>.
+All rights reserved.
+------------------------------------------------------------------------
+
+SchemaCrawler is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+SchemaCrawler and the accompanying materials are made available under
+the terms of the Eclipse Public License v1.0, GNU General Public License
+v3 or GNU Lesser General Public License v3.
+
+You may elect to redistribute this code under any of these licenses.
+
+The Eclipse Public License is available at:
+http://www.eclipse.org/legal/epl-v10.html
+
+The GNU General Public License v3 and the GNU Lesser General Public
+License v3 are available at:
+http://www.gnu.org/licenses/
+
+========================================================================
+*/
+
+package schemacrawler.test.utility;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@ExtendWith(WithLocaleExtension.class)
+public @interface WithLocale {
+
+  String value();
+}
---
+++ schemacrawler-api/src/test/java/schemacrawler/test/utility/WithLocaleExtension.java
@@ -0,0 +1,69 @@
+/*
+========================================================================
+SchemaCrawler
+http://www.schemacrawler.com
+Copyright (c) 2000-2023, Sualeh Fatehi <sualeh@hotmail.com>.
+All rights reserved.
+------------------------------------------------------------------------
+
+SchemaCrawler is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+SchemaCrawler and the accompanying materials are made available under
+the terms of the Eclipse Public License v1.0, GNU General Public License
+v3 or GNU Lesser General Public License v3.
+
+You may elect to redistribute this code under any of these licenses.
+
+The Eclipse Public License is available at:
+http://www.eclipse.org/legal/epl-v10.html
+
+The GNU General Public License v3 and the GNU Lesser General Public
+License v3 are available at:
+http://www.gnu.org/licenses/
+
+========================================================================
+*/
+
+package schemacrawler.test.utility;
+
+import static org.junit.platform.commons.support.AnnotationSupport.findAnnotation;
+import static us.fatehi.utility.Utility.isBlank;
+
+import java.util.Locale;
+import java.util.Optional;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+final class WithLocaleExtension implements BeforeEachCallback, AfterEachCallback {
+
+  private Locale systemLocale;
+
+  @Override
+  public void afterEach(final ExtensionContext context) throws Exception {
+    if (systemLocale != null) {
+      Locale.setDefault(systemLocale);
+    } else {
+      Locale.setDefault(Locale.US);
+    }
+    systemLocale = null;
+  }
+
+  @Override
+  public void beforeEach(final ExtensionContext context) throws Exception {
+    final Optional<WithLocale> optionalAnnotation =
+        findAnnotation(context.getTestMethod(), WithLocale.class);
+    optionalAnnotation.ifPresent(
+        withLocale -> {
+          final String localeString = withLocale.value();
+          if (isBlank(localeString)) {
+            throw new IllegalArgumentException("No locale provided");
+          }
+          final Locale locale = Locale.forLanguageTag(localeString);
+          Locale.setDefault(locale);
+        });
+  }
+}
