37e9db00aa64320ee4718b6dfa6b3494feae9612
Sualeh Fatehi <sualeh@hotmail.com>
2024-03-09 17:32:55 -0500

    Update Trino driver

--- schemacrawler-dbtest/src/test/java/schemacrawler/integration/test/TrinoTest.java
+++ schemacrawler-dbtest/src/test/java/schemacrawler/integration/test/TrinoTest.java
@@ -62,7 +62,7 @@ public class TrinoTest extends BaseAdditionalDatabaseTest {
   private final DockerImageName imageName = DockerImageName.parse("trinodb/trino");
 
   @Container
-  private final JdbcDatabaseContainer<?> dbContainer = new TrinoContainer(imageName.withTag("439"));
+  private final JdbcDatabaseContainer<?> dbContainer = new TrinoContainer(imageName.withTag("440"));
 
   @BeforeEach
   public void createDatabase() {
