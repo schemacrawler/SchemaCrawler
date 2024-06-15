5b6bfa4658ab79f6358ad242df29360249241f17
Sualeh Fatehi <sualeh@hotmail.com>
2024-06-15 11:00:32 -0400

    Use Apache Commons instead of Spring to reduce dependencies

--- schemacrawler-api/src/test/java/schemacrawler/test/utility/TestDatabaseConnectionParameterResolver.java
+++ schemacrawler-api/src/test/java/schemacrawler/test/utility/TestDatabaseConnectionParameterResolver.java
@@ -28,16 +28,20 @@ http://www.gnu.org/licenses/
 
 package schemacrawler.test.utility;
 
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.platform.commons.support.AnnotationSupport.findAnnotation;
-import static us.fatehi.utility.Utility.isBlank;
-
+import java.io.IOException;
 import java.lang.reflect.Parameter;
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Optional;
-
 import javax.sql.DataSource;
-
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.hsqldb.jdbc.JDBCDataSource;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
@@ -45,9 +49,7 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.ParameterResolver;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-
+import static us.fatehi.utility.Utility.isBlank;
 import schemacrawler.testdb.TestDatabase;
 import us.fatehi.utility.LoggingConfig;
 import us.fatehi.utility.datasource.DatabaseConnectionSource;
@@ -130,35 +132,35 @@ final class TestDatabaseConnectionParameterResolver
 
     final String script = locateAnnotation(extensionContext).getScript();
     final Parameter parameter = parameterContext.getParameter();
-    if (isBlank(script)) {
-      final DataSource ds = newDataSource();
-      this.dataSource = DatabaseConnectionSources.fromDataSource(ds);
-
-      if (isParameterConnection(parameter)) {
-        return dataSource.get();
-      } else if (isParameterDatabaseConnectionInfo(parameter)) {
-        return new DatabaseConnectionInfo(
-            testDatabase.getHost(),
-            testDatabase.getPort(),
-            testDatabase.getDatabase(),
-            testDatabase.getConnectionUrl());
-      } else if (isParameterDatabaseConnectionSource(parameter)) {
-        return dataSource;
-      } else {
-        throw new ParameterResolutionException("Could not resolve " + parameter);
-      }
-    } else {
+    if (!isBlank(script)) {
       final DataSource ds = newEmbeddedDatabase(script);
-      this.dataSource = DatabaseConnectionSources.fromDataSource(ds);
+      dataSource = DatabaseConnectionSources.fromDataSource(ds);
 
       if (isParameterConnection(parameter)) {
         return dataSource.get();
-      } else if (isParameterDatabaseConnectionSource(parameter)) {
-        return dataSource;
-      } else {
-        throw new ParameterResolutionException("Could not resolve " + parameter);
       }
+      if (isParameterDatabaseConnectionSource(parameter)) {
+        return dataSource;
+      }
+      throw new ParameterResolutionException("Could not resolve " + parameter);
     }
+    final DataSource ds = newDataSource();
+    dataSource = DatabaseConnectionSources.fromDataSource(ds);
+
+    if (isParameterConnection(parameter)) {
+      return dataSource.get();
+    }
+    if (isParameterDatabaseConnectionInfo(parameter)) {
+      return new DatabaseConnectionInfo(
+          testDatabase.getHost(),
+          testDatabase.getPort(),
+          testDatabase.getDatabase(),
+          testDatabase.getConnectionUrl());
+    }
+    if (isParameterDatabaseConnectionSource(parameter)) {
+      return dataSource;
+    }
+    throw new ParameterResolutionException("Could not resolve " + parameter);
   }
 
   @Override
@@ -209,14 +211,27 @@ final class TestDatabaseConnectionParameterResolver
     return ds;
   }
 
-  private EmbeddedDatabase newEmbeddedDatabase(final String script) {
-    final EmbeddedDatabase db =
-        new EmbeddedDatabaseBuilder()
-            .generateUniqueName(true)
-            .setScriptEncoding("UTF-8")
-            .ignoreFailedDrops(true)
-            .addScript(script)
-            .build();
-    return db;
+  private DataSource newEmbeddedDatabase(final String script) {
+    try {
+      // Create data source
+      final String randomDatabaseName = RandomStringUtils.randomAlphabetic(7);
+      final JDBCDataSource hsqlDataSource = new JDBCDataSource();
+      hsqlDataSource.setDatabase("jdbc:hsqldb:mem:" + randomDatabaseName);
+      // Read script
+      final String sql = IOUtils.resourceToString(script, StandardCharsets.UTF_8);
+      String[] statements = sql.split(";");
+      // Create a QueryRunner to execute the SQL statements
+      final QueryRunner runner = new QueryRunner(hsqlDataSource);
+      for (String statement : statements) {
+          statement = statement.trim();
+          System.out.println(statement);
+          if (!statement.isEmpty()) {
+              runner.update(statement);
+          }
+      }
+      return hsqlDataSource;
+    } catch (IOException | SQLException e) {
+      return fail("Could not create a data source", e);
+    }
   }
 }
