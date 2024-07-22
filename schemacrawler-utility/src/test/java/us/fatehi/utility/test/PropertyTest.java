22bac1784c5d602b4e7a64b69dc1a4232780107a
Sualeh Fatehi <sualeh@hotmail.com>
2024-07-22 12:29:14 -0400

    Add full coverage for properties

---
+++ schemacrawler-utility/src/test/java/us/fatehi/utility/test/PropertyTest.java
@@ -0,0 +1,80 @@
+package us.fatehi.utility.test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import us.fatehi.utility.property.AbstractProperty;
+import us.fatehi.utility.property.Property;
+import us.fatehi.utility.property.PropertyName;
+
+public class PropertyTest {
+
+  private final Property property =
+      new AbstractProperty(new PropertyName("name", "description"), "value") {};
+  private final Property property1 =
+      new Property() {
+        @Override
+        public String getName() {
+          return "name";
+        }
+
+        @Override
+        public String getDescription() {
+          return "description";
+        }
+
+        @Override
+        public Object getValue() {
+          return "value";
+        }
+      };
+
+  @Test
+  public void fields() {
+    assertThat(property.getName(), is("name"));
+    assertThat(property.getDescription(), is("description"));
+    assertThat(property.getValue(), is("value"));
+    assertThat(property.toString(), is("name = value"));
+  }
+
+  @Test
+  public void arrayValue() {
+    final Property arrayValueProperty =
+        new AbstractProperty(new PropertyName("name", "description"), new String[] {"v1", "v2"}) {};
+    assertThat(arrayValueProperty.getName(), is("name"));
+    assertThat(arrayValueProperty.getDescription(), is("description"));
+    assertThat(arrayValueProperty.getValue(), is(Arrays.asList("v1", "v2")));
+    assertThat(arrayValueProperty.toString(), is("name = [v1, v2]"));
+  }
+
+  @Test
+  public void nullPropertyValue() {
+    final Property nullValueProperty =
+        new AbstractProperty(new PropertyName("name", "description"), null) {};
+    assertThat(nullValueProperty.getName(), is("name"));
+    assertThat(nullValueProperty.getDescription(), is("description"));
+    assertThat(nullValueProperty.getValue(), is(nullValue()));
+    assertThat(nullValueProperty.toString(), is("name = null"));
+  }
+
+  @Test
+  public void testHashCode() {
+    assertThat(property.hashCode(), is(property.hashCode()));
+  }
+
+  @Test
+  public void equalsTest() {
+    EqualsVerifier.forClass(AbstractProperty.class).verify();
+
+    // Additional equals test for properties not subclasses of AbstractProperty
+    assertThat(property, is(property1));
+  }
+
+  @Test
+  public void compareTo() {
+    assertThat(property.compareTo(property1), is(0));
+  }
+}
