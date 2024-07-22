2b822fed39cc76039ff7095cb06bc20e5367ed98
Sualeh Fatehi <sualeh@hotmail.com>
2024-07-21 21:22:17 -0400

    Remove tests that do not assume singleton behavior

--- schemacrawler-tools/src/test/java/schemacrawler/test/CatalogLoaderRegistryTest.java
+++ schemacrawler-tools/src/test/java/schemacrawler/test/CatalogLoaderRegistryTest.java
@@ -1,17 +1,13 @@
 package schemacrawler.test;
 
-import static com.github.stefanbirkner.systemlambda.SystemLambda.restoreSystemProperties;
 import static java.util.stream.Collectors.toList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import schemacrawler.schemacrawler.exceptions.InternalRuntimeException;
-import schemacrawler.test.utility.TestCatalogLoader;
 import schemacrawler.tools.catalogloader.CatalogLoader;
 import schemacrawler.tools.catalogloader.CatalogLoaderRegistry;
 import schemacrawler.tools.catalogloader.ChainedCatalogLoader;
@@ -28,15 +24,6 @@ public class CatalogLoaderRegistryTest {
     final List<String> names =
         commandLineCommands.stream().map(PluginCommand::getName).collect(toList());
     assertThat(names, containsInAnyOrder("loader:testloader", null));
-
-    restoreSystemProperties(
-        () -> {
-          System.setProperty(
-              TestCatalogLoader.class.getName() + ".force-instantiation-failure", "throw");
-          assertThrows(
-              InternalRuntimeException.class,
-              () -> CatalogLoaderRegistry.getCatalogLoaderRegistry().getCommandLineCommands());
-        });
   }
 
   @Test
@@ -46,15 +33,6 @@ public class CatalogLoaderRegistryTest {
     assertThat(String.valueOf(helpCommands), helpCommands, hasSize(2));
     final List<String> names = helpCommands.stream().map(PluginCommand::getName).collect(toList());
     assertThat(names, containsInAnyOrder("loader:testloader", null));
-
-    restoreSystemProperties(
-        () -> {
-          System.setProperty(
-              TestCatalogLoader.class.getName() + ".force-instantiation-failure", "throw");
-          assertThrows(
-              InternalRuntimeException.class,
-              () -> CatalogLoaderRegistry.getCatalogLoaderRegistry().getHelpCommands());
-        });
   }
 
   @Test
@@ -76,14 +54,5 @@ public class CatalogLoaderRegistryTest {
     final List<String> names =
         supportedCatalogLoaders.stream().map(CommandDescription::getName).collect(toList());
     assertThat(names, containsInAnyOrder("testloader", "schemacrawlerloader"));
-
-    restoreSystemProperties(
-        () -> {
-          System.setProperty(
-              TestCatalogLoader.class.getName() + ".force-instantiation-failure", "throw");
-          assertThrows(
-              InternalRuntimeException.class,
-              () -> CatalogLoaderRegistry.getCatalogLoaderRegistry().getCommandDescriptions());
-        });
   }
 }
