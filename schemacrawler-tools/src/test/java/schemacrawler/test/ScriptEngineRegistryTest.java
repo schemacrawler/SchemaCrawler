a008b694cc447f557524f2ae9a231d73c6ec1b28
Sualeh Fatehi <sualeh@hotmail.com>
2024-07-21 22:55:13 -0400

    Add test

---
+++ schemacrawler-tools/src/test/java/schemacrawler/test/ScriptEngineRegistryTest.java
@@ -0,0 +1,48 @@
+package schemacrawler.test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.condition.JRE.JAVA_8;
+import java.util.Collection;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.JRE;
+import schemacrawler.tools.executable.CommandDescription;
+import schemacrawler.tools.executable.commandline.PluginCommand;
+import schemacrawler.tools.registry.ScriptEngineRegistry;
+
+public class ScriptEngineRegistryTest {
+
+  @Test
+  public void commandLineCommands() {
+    final ScriptEngineRegistry driverRegistry = ScriptEngineRegistry.getScriptEngineRegistry();
+    final Collection<PluginCommand> commandLineCommands = driverRegistry.getCommandLineCommands();
+    assertThat(commandLineCommands, is(empty()));
+  }
+
+  @Test
+  public void helpCommands() {
+    final ScriptEngineRegistry driverRegistry = ScriptEngineRegistry.getScriptEngineRegistry();
+    final Collection<PluginCommand> commandLineCommands = driverRegistry.getHelpCommands();
+    assertThat(commandLineCommands, is(empty()));
+  }
+
+  @Test
+  public void commandDescriptions() {
+    if (JRE.currentVersion() != JAVA_8) {
+      // No script engines ship with Java versions later than 8
+      return;
+    }
+    final ScriptEngineRegistry driverRegistry = ScriptEngineRegistry.getScriptEngineRegistry();
+    final Collection<CommandDescription> commandLineCommands =
+        driverRegistry.getCommandDescriptions();
+    assertThat(commandLineCommands, hasSize(1));
+  }
+
+  @Test
+  public void name() {
+    final ScriptEngineRegistry driverRegistry = ScriptEngineRegistry.getScriptEngineRegistry();
+    assertThat(driverRegistry.getName(), is("script engines"));
+  }
+}
