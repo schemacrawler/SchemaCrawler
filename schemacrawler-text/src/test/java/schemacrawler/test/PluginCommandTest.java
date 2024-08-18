ea5376d9525112ae804cf40ca7b604064b2d5bbf
Sualeh Fatehi <sualeh@hotmail.com>
2024-08-18 13:08:58 -0400

    Refactor test coverage for plugin commands

--- schemacrawler-text/src/test/java/schemacrawler/test/PluginCommandTest.java
+++ schemacrawler-text/src/test/java/schemacrawler/test/PluginCommandTest.java
@@ -1,18 +1,11 @@
 package schemacrawler.test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static schemacrawler.test.utility.FileHasContent.classpathResource;
-import static schemacrawler.test.utility.FileHasContent.hasSameContentAs;
-import static schemacrawler.test.utility.FileHasContent.outputOf;
-import java.util.Collection;
 import org.junit.jupiter.api.Test;
+import schemacrawler.test.utility.PluginCommandTestUtility;
 import schemacrawler.test.utility.ResolveTestContext;
 import schemacrawler.test.utility.TestContext;
-import schemacrawler.test.utility.TestWriter;
 import schemacrawler.tools.command.text.schema.SchemaTextCommandProvider;
 import schemacrawler.tools.executable.commandline.PluginCommand;
-import schemacrawler.tools.executable.commandline.PluginCommandOption;
 
 @ResolveTestContext
 public class PluginCommandTest {
@@ -20,32 +13,6 @@ public class PluginCommandTest {
   @Test
   public void testSchemaTextCommandProviderPluginCommand(final TestContext testContext) {
     final PluginCommand pluginCommand = new SchemaTextCommandProvider().getCommandLineCommand();
-
-    assertThat(pluginCommand.getName(), is("command:schema"));
-
-    final TestWriter testout = new TestWriter();
-    try (final TestWriter out = testout) {
-      out.println(pluginCommand.getHelpHeader());
-      final String[] helpDescription = pluginCommand.getHelpDescription().get();
-      writeStringArray(out, helpDescription);
-      out.println();
-      final Collection<PluginCommandOption> options = pluginCommand.getOptions();
-      for (final PluginCommandOption pluginCommandOption : options) {
-        out.println(pluginCommandOption.getName());
-        final String[] optionHelpText = pluginCommandOption.getHelpText();
-        writeStringArray(out, optionHelpText);
-        out.println();
-      }
-      final String[] helpFooter = pluginCommand.getHelpFooter().get();
-      writeStringArray(out, helpFooter);
-    }
-    assertThat(
-        outputOf(testout), hasSameContentAs(classpathResource(testContext.testMethodFullName())));
-  }
-
-  private void writeStringArray(final TestWriter out, final String[] helpFooter) {
-    for (final String helpLine : helpFooter) {
-      out.println(helpLine);
-    }
+    PluginCommandTestUtility.testPluginCommand(testContext, pluginCommand);
   }
 }
---
+++ schemacrawler-tools/src/test/java/schemacrawler/test/utility/PluginCommandTestUtility.java
@@ -0,0 +1,52 @@
+package schemacrawler.test.utility;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static schemacrawler.test.utility.FileHasContent.classpathResource;
+import static schemacrawler.test.utility.FileHasContent.hasSameContentAs;
+import static schemacrawler.test.utility.FileHasContent.outputOf;
+import java.util.Collection;
+import static java.util.Objects.requireNonNull;
+import schemacrawler.tools.executable.commandline.PluginCommand;
+import schemacrawler.tools.executable.commandline.PluginCommandOption;
+
+public class PluginCommandTestUtility {
+
+  public static void testPluginCommand(final TestContext testContext,
+      final PluginCommand pluginCommand) {
+    requireNonNull(testContext, "No test context provided");
+    requireNonNull(pluginCommand, "No plugin command provided");
+
+    final TestWriter testout = new TestWriter();
+    try (final TestWriter out = testout) {
+      out.println(pluginCommand.getName());
+      out.println();
+      out.println(pluginCommand.getHelpHeader());
+      final String[] helpDescription = pluginCommand.getHelpDescription().get();
+      writeStringArray(out, helpDescription);
+      out.println();
+      final Collection<PluginCommandOption> options = pluginCommand.getOptions();
+      for (final PluginCommandOption pluginCommandOption : options) {
+        out.println(pluginCommandOption.getName());
+        final String[] optionHelpText = pluginCommandOption.getHelpText();
+        writeStringArray(out, optionHelpText);
+        out.println();
+      }
+      final String[] helpFooter = pluginCommand.getHelpFooter().get();
+      writeStringArray(out, helpFooter);
+    }
+    assertThat(outputOf(testout),
+        hasSameContentAs(classpathResource(testContext.testMethodFullName())));
+  }
+
+  private static void writeStringArray(final TestWriter out, final String[] helpText) {
+    for (final String helpLine : helpText) {
+      out.println(helpLine);
+    }
+  }
+
+  private PluginCommandTestUtility() {
+    // Prevent instantiation
+  }
+
+}
+
