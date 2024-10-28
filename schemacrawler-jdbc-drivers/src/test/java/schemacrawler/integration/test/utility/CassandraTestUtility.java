882663b899e4d66c56ea22aff56b8921d351d102
Sualeh Fatehi <sualeh@hotmail.com>
2024-10-28 18:42:58 -0400

    Allow reading of resources

--- schemacrawler-jdbc-drivers/src/test/java/schemacrawler/integration/test/utility/CassandraTestUtility.java
+++ schemacrawler-jdbc-drivers/src/test/java/schemacrawler/integration/test/utility/CassandraTestUtility.java
@@ -29,8 +29,11 @@ http://www.gnu.org/licenses/
 package schemacrawler.integration.test.utility;
 
 import org.testcontainers.cassandra.CassandraContainer;
+import org.testcontainers.cassandra.delegate.CassandraDatabaseDelegate;
+import org.testcontainers.ext.ScriptUtils;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
+import com.github.dockerjava.api.command.InspectContainerResponse;
 
 public final class CassandraTestUtility {
 
@@ -41,10 +44,44 @@ public final class CassandraTestUtility {
   @SuppressWarnings("resource")
   private static CassandraContainer newCassandraContainer(final String version) {
 
+    class CassandraInitContainer extends CassandraContainer {
+
+      private String containerInitScriptPath;
+
+      public CassandraInitContainer(final DockerImageName dockerImageName) {
+        super(dockerImageName);
+      }
+
+      public CassandraInitContainer withContainerInitScript(final String containerInitScriptPath) {
+        this.containerInitScriptPath = containerInitScriptPath;
+        return this;
+      }
+
+      @Override
+      protected void containerIsStarted(final InspectContainerResponse containerInfo) {
+        if (containerInitScriptPath == null) {
+          return;
+        }
+        // Apply init script to the database
+        try {
+          new CassandraDatabaseDelegate(this)
+              .execute(null, containerInitScriptPath, -1, false, false);
+        } catch (ScriptUtils.ScriptStatementFailedException e) {
+          logger().error("Error while executing init script: {}", containerInitScriptPath, e);
+          throw new ScriptUtils.UncategorizedScriptException(
+              "Error while executing init script: " + containerInitScriptPath, e);
+        }
+      }
+    }
+
     final DockerImageName imageName = DockerImageName.parse("cassandra");
 
     final CassandraContainer dbContainer =
-        new CassandraContainer(imageName.withTag(version))
+        new CassandraInitContainer(imageName.withTag(version))
+            .withContainerInitScript("cassandra.create-database.cql")
+            .withCopyFileToContainer(
+                MountableFile.forClasspathResource("cassandra.create-database.cql"),
+                "cassandra.create-database.cql")
             .withCopyFileToContainer(
                 MountableFile.forClasspathResource("cassandra.yaml"),
                 "/etc/cassandra/cassandra.yaml")
@@ -53,8 +90,7 @@ public final class CassandraTestUtility {
                 "/security/cassandra.keystore")
             .withCopyFileToContainer(
                 MountableFile.forClasspathResource("cassandra.truststore"),
-                "/security/cassandra.truststore")
-            .withInitScript("cassandra.create-database.cql");
+                "/security/cassandra.truststore");
 
     return dbContainer;
   }
