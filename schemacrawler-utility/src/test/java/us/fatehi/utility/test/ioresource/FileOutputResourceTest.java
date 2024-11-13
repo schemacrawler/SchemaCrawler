613d3f2abd89d3b3b55986dc8c80850cc71dad06
Sualeh Fatehi <sualeh@hotmail.com>
2024-11-12 21:07:15 -0500

    Tests for NonCloseableWriter

--- schemacrawler-utility/src/test/java/us/fatehi/utility/test/ioresource/FileOutputResourceTest.java
+++ schemacrawler-utility/src/test/java/us/fatehi/utility/test/ioresource/FileOutputResourceTest.java
@@ -35,13 +35,11 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static us.fatehi.utility.IOUtility.readFully;
-
 import java.io.IOException;
 import java.io.Writer;
+import java.nio.file.Files;
 import java.nio.file.Path;
-
 import org.junit.jupiter.api.Test;
-
 import us.fatehi.utility.ioresource.FileOutputResource;
 
 public class FileOutputResourceTest {
@@ -55,8 +53,25 @@ public class FileOutputResourceTest {
     assertThat(outputResource.toString(), is(tempFile.toString()));
     assertThat(outputResource.getDescription(), is(tempFile.toString()));
 
-    final Writer writer = outputResource.openNewOutputWriter(UTF_8, true);
+    final Writer writer = outputResource.openNewOutputWriter(UTF_8, false);
     writer.write("hello, world");
+    writer.close();
+
+    assertThat(readFully(newBufferedReader(tempFile)), is("hello, world"));
+  }
+
+  @Test
+  public void append() throws IOException {
+    final Path tempFile = createTempFile("sc", ".txt");
+    Files.write(tempFile, "hello,".getBytes(UTF_8));
+
+    final FileOutputResource outputResource = new FileOutputResource(tempFile);
+    assertThat(outputResource.getOutputFile(), is(tempFile));
+    assertThat(outputResource.toString(), is(tempFile.toString()));
+    assertThat(outputResource.getDescription(), is(tempFile.toString()));
+
+    final Writer writer = outputResource.openNewOutputWriter(UTF_8, true);
+    writer.write(" world");
     writer.close();
 
     assertThat(readFully(newBufferedReader(tempFile)), is("hello, world"));
