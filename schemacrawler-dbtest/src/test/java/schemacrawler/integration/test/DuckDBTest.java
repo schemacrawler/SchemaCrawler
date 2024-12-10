3d73958affd9a5774f67988ef7c3801d17a27744
Sualeh Fatehi <sualeh@hotmail.com>
2024-12-09 21:53:24 -0500

    Do not run DuckDB tests on Windows ARM

--- schemacrawler-dbtest/src/test/java/schemacrawler/integration/test/DuckDBTest.java
+++ schemacrawler-dbtest/src/test/java/schemacrawler/integration/test/DuckDBTest.java
@@ -39,6 +39,7 @@ import java.nio.file.Path;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 import schemacrawler.schemacrawler.LoadOptionsBuilder;
 import schemacrawler.schemacrawler.SchemaCrawlerOptions;
 import schemacrawler.schemacrawler.SchemaCrawlerOptionsBuilder;
@@ -50,6 +51,7 @@ import schemacrawler.tools.executable.SchemaCrawlerExecutable;
 import us.fatehi.utility.IOUtility;
 
 @EnabledOnOs(
+    value = {OS.WINDOWS},
     architectures = {"x64", "x86_64", "amd64"},
     disabledReason = "DuckDB does not run on Windows ARM")
 public class DuckDBTest extends BaseAdditionalDatabaseTest {
