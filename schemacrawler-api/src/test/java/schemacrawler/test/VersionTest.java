d3e0b0b7456cd817b7fe8585da44fb61be898c11
Sualeh Fatehi <sualeh@hotmail.com>
2024-12-14 09:55:24 -0500

    Match SchemaCrawler version with a regex

--- schemacrawler-api/src/test/java/schemacrawler/test/VersionTest.java
+++ schemacrawler-api/src/test/java/schemacrawler/test/VersionTest.java
@@ -28,11 +28,13 @@ http://www.gnu.org/licenses/
 
 package schemacrawler.test;
 
+import static java.util.regex.Pattern.DOTALL;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.startsWith;
+import static org.hamcrest.Matchers.matchesRegex;
 import static schemacrawler.test.utility.FileHasContent.contentsOf;
 import static schemacrawler.test.utility.FileHasContent.hasNoContent;
 import static schemacrawler.test.utility.FileHasContent.outputOf;
+import java.util.regex.Pattern;
 import org.junit.jupiter.api.Test;
 import schemacrawler.Version;
 import schemacrawler.test.utility.CaptureSystemStreams;
@@ -43,9 +45,11 @@ public class VersionTest {
 
   @Test
   public void version(final CapturedSystemStreams streams) throws Exception {
+    final Pattern VERSION = Pattern.compile("SchemaCrawler 16\\.24\\.\\d{1,2}\\R.*", DOTALL);
+
     Version.main(new String[0]);
 
-    assertThat(contentsOf(streams.out()), startsWith("SchemaCrawler 16.23."));
+    assertThat(contentsOf(streams.out()), matchesRegex(VERSION));
     assertThat(outputOf(streams.err()), hasNoContent());
   }
 }
