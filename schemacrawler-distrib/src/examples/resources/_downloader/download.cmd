11ab551142abc532249b633db9ab18ddc91026bc
Sualeh Fatehi <sualeh@hotmail.com>
2025-03-23 17:41:53 -0400

    Fix #1882

--- schemacrawler-distrib/src/examples/resources/_downloader/download.cmd
+++ schemacrawler-distrib/src/examples/resources/_downloader/download.cmd
@@ -1,3 +1,3 @@
 @echo off
 echo Downloading %1
-java -jar ivy-2.5.2.jar -ivy %1_ivy.xml -settings ivysettings.xml -retrieve "../_schemacrawler/lib/[artifact]-[revision]-[type].[ext]"
+java -jar ivy-2.5.3.jar -ivy %1_ivy.xml -settings ivysettings.xml -retrieve "../_schemacrawler/lib/[artifact]-[revision](-[classifier]).[ext]"
