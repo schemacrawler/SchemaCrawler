e88c75b48d8622566f2f79b1626a97e7f60d684c
Sualeh Fatehi <sualeh@hotmail.com>
2025-05-04 09:14:57 -0400

    Clean up tests

--- schemacrawler-api/src/test/java/schemacrawler/crawl/DatabaseInfoRetrieverTest.java
+++ schemacrawler-api/src/test/java/schemacrawler/crawl/DatabaseInfoRetrieverTest.java
@@ -29,29 +29,25 @@ http://www.gnu.org/licenses/
 package schemacrawler.crawl;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import java.lang.reflect.Method;
+import static org.mockito.Mockito.when;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import schemacrawler.schema.DatabaseInfo;
 import schemacrawler.schema.DatabaseUser;
 import schemacrawler.schema.JdbcDriverInfo;
 import schemacrawler.schema.JdbcDriverProperty;
@@ -65,15 +61,94 @@ import schemacrawler.schemacrawler.SchemaRetrievalOptionsBuilder;
 import schemacrawler.test.utility.ResolveTestContext;
 import schemacrawler.test.utility.TestContext;
 import schemacrawler.test.utility.WithTestDatabase;
+import us.fatehi.test.utility.TestObjectUtility;
 import us.fatehi.utility.datasource.DatabaseConnectionSource;
+import us.fatehi.utility.datasource.DatabaseConnectionSourceUtility;
 import us.fatehi.utility.property.Property;
 
 @WithTestDatabase
 @ResolveTestContext
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class DatabaseInfoRetrieverTest {
 
   private MutableCatalog catalog;
+
+  @Test
+  @DisplayName("Retrieve additional database info")
+  public void additionalDatabaseInfo(
+      final TestContext testContext, final DatabaseConnectionSource dataSource) throws Exception {
+
+    assertThat(
+        "Should not have database properties",
+        catalog.getDatabaseInfo().getProperties(),
+        is(empty()));
+
+    final RetrieverConnection retrieverConnection =
+        new RetrieverConnection(dataSource, SchemaRetrievalOptionsBuilder.builder().toOptions());
+
+    final SchemaCrawlerOptions options = SchemaCrawlerOptionsBuilder.newSchemaCrawlerOptions();
+
+    final DatabaseInfoRetriever databaseInfoRetriever =
+        new DatabaseInfoRetriever(retrieverConnection, catalog, options);
+    databaseInfoRetriever.retrieveAdditionalDatabaseInfo();
+
+    // Verify that database properties were retrieved
+    final Collection<Property> databaseProperties = catalog.getDatabaseInfo().getProperties();
+    assertThat("Should have database properties", databaseProperties, is(not(empty())));
+
+    // Verify that some common database properties are present
+    boolean foundSupportedProperty = false;
+    for (final Property property : databaseProperties) {
+      if (property.getName().startsWith("supports")) {
+        foundSupportedProperty = true;
+        break;
+      }
+    }
+    assertThat("Should find at least one 'supports' property", foundSupportedProperty, is(true));
+
+    // Verify that result set type properties were retrieved
+    boolean foundResultSetTypeProperty = false;
+    for (final Property property : databaseProperties) {
+      if (property.getName().contains("ResultSets")) {
+        foundResultSetTypeProperty = true;
+        break;
+      }
+    }
+    assertThat(
+        "Should find at least one result set type property", foundResultSetTypeProperty, is(true));
+  }
+
+  @Test
+  @DisplayName("Retrieve additional JDBC driver info")
+  public void additionalJdbcDriverInfo(
+      final TestContext testContext, final DatabaseConnectionSource dataSource) throws Exception {
+
+    assertThat(catalog.getJdbcDriverInfo(), is(notNullValue()));
+    assertThat(
+        "Should not have JDBC driver properties",
+        catalog.getJdbcDriverInfo().getDriverProperties(),
+        is(empty()));
+
+    final RetrieverConnection retrieverConnection =
+        new RetrieverConnection(dataSource, SchemaRetrievalOptionsBuilder.builder().toOptions());
+
+    final SchemaCrawlerOptions options = SchemaCrawlerOptionsBuilder.newSchemaCrawlerOptions();
+
+    final DatabaseInfoRetriever databaseInfoRetriever =
+        new DatabaseInfoRetriever(retrieverConnection, catalog, options);
+    databaseInfoRetriever.retrieveAdditionalJdbcDriverInfo();
+
+    // Verify that JDBC driver properties were retrieved
+    final JdbcDriverInfo jdbcDriverInfo = catalog.getJdbcDriverInfo();
+    assertThat(jdbcDriverInfo, is(notNullValue()));
+
+    // HSQLDB driver should have some properties
+    final Collection<JdbcDriverProperty> driverProperties = jdbcDriverInfo.getDriverProperties();
+    assertThat("Should have JDBC driver properties", driverProperties, is(not(empty())));
+
+    // Verify driver name and version
+    assertThat(jdbcDriverInfo.getDriverName(), containsString("HSQL"));
+    assertThat(jdbcDriverInfo.getDriverVersion(), is(notNullValue()));
+  }
 
   @Test
   @DisplayName("Database info")
@@ -89,6 +164,7 @@ public class DatabaseInfoRetrieverTest {
   @DisplayName("Retrieve database users")
   public void databaseUsers(
       final TestContext testContext, final DatabaseConnectionSource dataSource) throws Exception {
+
     assertThat(catalog.getDatabaseUsers(), is(empty()));
 
     final InformationSchemaViews informationSchemaViews =
@@ -129,7 +205,31 @@ public class DatabaseInfoRetrieverTest {
         hasItems("AUTHENTICATION", "PASSWORD_DIGEST", "ADMIN"));
   }
 
-  @BeforeAll
+  @Test
+  @DisplayName("Test error handling in retrieveAdditionalDatabaseInfo")
+  public void errorHandlingInRetrieveAdditionalDatabaseInfo(final TestContext testContext)
+      throws Exception {
+
+    // Create a retriever connection with a mocked connection source to simulate errors
+    final Connection mockConnection = TestObjectUtility.mockConnection();
+    final DatabaseMetaData mockMetaData = mockConnection.getMetaData();
+    when(mockMetaData.isReadOnly()).thenThrow(SQLException.class);
+    final DatabaseConnectionSource dataSource =
+        DatabaseConnectionSourceUtility.newTestDatabaseConnectionSource(mockConnection);
+
+    final RetrieverConnection retrieverConnection =
+        new RetrieverConnection(dataSource, SchemaRetrievalOptionsBuilder.builder().toOptions());
+
+    final SchemaCrawlerOptions options = SchemaCrawlerOptionsBuilder.newSchemaCrawlerOptions();
+
+    final DatabaseInfoRetriever databaseInfoRetriever =
+        new DatabaseInfoRetriever(retrieverConnection, catalog, options);
+
+    // The method should handle exceptions gracefully and not throw
+    assertDoesNotThrow(() -> databaseInfoRetriever.retrieveAdditionalDatabaseInfo());
+  }
+
+  @BeforeEach
   public void loadBaseCatalog(final Connection connection) throws SQLException {
     final ConnectionInfoBuilder connectionInfoBuilder = ConnectionInfoBuilder.builder(connection);
     final MutableDatabaseInfo databaseInfo =
@@ -144,111 +244,12 @@ public class DatabaseInfoRetrieverTest {
   }
 
   @Test
-  @DisplayName("Retrieve server info")
-  public void serverInfo(final TestContext testContext, final DatabaseConnectionSource dataSource)
-      throws Exception {
-    assertThat(catalog.getDatabaseInfo().getServerInfo(), is(empty()));
-
-    final String name = "TEST Server Info Property - Name";
-    final String description = "TEST Server Info Property - Description";
-    final String value = "TEST Server Info Property - Value";
-
-    final InformationSchemaViews informationSchemaViews =
-        InformationSchemaViewsBuilder.builder()
-            .withSql(
-                InformationSchemaKey.SERVER_INFORMATION,
-                String.format(
-                    "SELECT '%s' AS NAME, '%s' AS DESCRIPTION, '%s' AS VALUE "
-                        + "FROM INFORMATION_SCHEMA.SYSTEM_TYPEINFO",
-                    name, description, value))
-            .toOptions();
-    final SchemaRetrievalOptionsBuilder schemaRetrievalOptionsBuilder =
-        SchemaRetrievalOptionsBuilder.builder().withInformationSchemaViews(informationSchemaViews);
-    final SchemaRetrievalOptions schemaRetrievalOptions = schemaRetrievalOptionsBuilder.toOptions();
-    final RetrieverConnection retrieverConnection =
-        new RetrieverConnection(dataSource, schemaRetrievalOptions);
-
-    final SchemaCrawlerOptions options = SchemaCrawlerOptionsBuilder.newSchemaCrawlerOptions();
-
-    final DatabaseInfoRetriever databaseInfoRetriever =
-        new DatabaseInfoRetriever(retrieverConnection, catalog, options);
-    databaseInfoRetriever.retrieveServerInfo();
-
-    final List<Property> serverInfo = new ArrayList<>(catalog.getDatabaseInfo().getServerInfo());
-    assertThat(serverInfo, hasSize(1));
-    final Property serverInfoProperty = serverInfo.get(0);
-    assertThat(serverInfoProperty, is(new ImmutableServerInfoProperty(name, value, description)));
-    assertThat(serverInfoProperty.getDescription(), is(description));
-  }
-
-  @Test
-  @DisplayName("Retrieve additional database info")
-  public void additionalDatabaseInfo(final TestContext testContext, final DatabaseConnectionSource dataSource)
-      throws Exception {
-    final RetrieverConnection retrieverConnection =
-        new RetrieverConnection(dataSource, SchemaRetrievalOptionsBuilder.builder().toOptions());
-
-    final SchemaCrawlerOptions options = SchemaCrawlerOptionsBuilder.newSchemaCrawlerOptions();
-
-    final DatabaseInfoRetriever databaseInfoRetriever =
-        new DatabaseInfoRetriever(retrieverConnection, catalog, options);
-    databaseInfoRetriever.retrieveAdditionalDatabaseInfo();
-
-    // Verify that database properties were retrieved
-    final Collection<Property> databaseProperties = catalog.getDatabaseInfo().getProperties();
-    assertThat("Should have database properties", databaseProperties, is(not(empty())));
-
-    // Verify that some common database properties are present
-    boolean foundSupportedProperty = false;
-    for (final Property property : databaseProperties) {
-      if (property.getName().startsWith("supports")) {
-        foundSupportedProperty = true;
-        break;
-      }
-    }
-    assertThat("Should find at least one 'supports' property", foundSupportedProperty, is(true));
-
-    // Verify that result set type properties were retrieved
-    boolean foundResultSetTypeProperty = false;
-    for (final Property property : databaseProperties) {
-      if (property.getName().contains("ResultSets")) {
-        foundResultSetTypeProperty = true;
-        break;
-      }
-    }
-    assertThat("Should find at least one result set type property", foundResultSetTypeProperty, is(true));
-  }
-
-  @Test
-  @DisplayName("Retrieve additional JDBC driver info")
-  public void additionalJdbcDriverInfo(final TestContext testContext, final DatabaseConnectionSource dataSource)
-      throws Exception {
-    final RetrieverConnection retrieverConnection =
-        new RetrieverConnection(dataSource, SchemaRetrievalOptionsBuilder.builder().toOptions());
-
-    final SchemaCrawlerOptions options = SchemaCrawlerOptionsBuilder.newSchemaCrawlerOptions();
-
-    final DatabaseInfoRetriever databaseInfoRetriever =
-        new DatabaseInfoRetriever(retrieverConnection, catalog, options);
-    databaseInfoRetriever.retrieveAdditionalJdbcDriverInfo();
-
-    // Verify that JDBC driver properties were retrieved
-    final JdbcDriverInfo jdbcDriverInfo = catalog.getJdbcDriverInfo();
-    assertThat(jdbcDriverInfo, is(notNullValue()));
-
-    // HSQLDB driver should have some properties
-    final Collection<JdbcDriverProperty> driverProperties = jdbcDriverInfo.getDriverProperties();
-    assertThat("Should have JDBC driver properties", driverProperties, is(not(empty())));
-
-    // Verify driver name and version
-    assertThat(jdbcDriverInfo.getDriverName(), containsString("HSQL"));
-    assertThat(jdbcDriverInfo.getDriverVersion(), is(notNullValue()));
-  }
-
-  @Test
   @DisplayName("Test result set type properties retrieval")
-  public void resultSetTypePropertiesRetrieval(final TestContext testContext, final DatabaseConnectionSource dataSource)
-      throws Exception {
+  public void resultSetTypePropertiesRetrieval(
+      final TestContext testContext, final DatabaseConnectionSource dataSource) throws Exception {
+
+    assertThat(catalog.getDatabaseInfo().getProperties(), is(empty()));
+
     final RetrieverConnection retrieverConnection =
         new RetrieverConnection(dataSource, SchemaRetrievalOptionsBuilder.builder().toOptions());
 
@@ -283,23 +284,46 @@ public class DatabaseInfoRetrieverTest {
     assertThat("Should find deletesAreDetected properties", foundDeletesAreDetected, is(true));
     assertThat("Should find insertsAreDetected properties", foundInsertsAreDetected, is(true));
     assertThat("Should find updatesAreDetected properties", foundUpdatesAreDetected, is(true));
-    assertThat("Should find supportsResultSetType properties", foundSupportsResultSetType, is(true));
+    assertThat(
+        "Should find supportsResultSetType properties", foundSupportsResultSetType, is(true));
   }
 
   @Test
-  @DisplayName("Test error handling in retrieveAdditionalDatabaseInfo")
-  public void errorHandlingInRetrieveAdditionalDatabaseInfo(final TestContext testContext, final DatabaseConnectionSource dataSource)
+  @DisplayName("Retrieve server info")
+  public void serverInfo(final TestContext testContext, final DatabaseConnectionSource dataSource)
       throws Exception {
-    // Create a retriever connection with a closed connection source to simulate errors
+
+    assertThat(catalog.getDatabaseInfo().getServerInfo(), is(empty()));
+
+    final String name = "TEST Server Info Property - Name";
+    final String description = "TEST Server Info Property - Description";
+    final String value = "TEST Server Info Property - Value";
+
+    final InformationSchemaViews informationSchemaViews =
+        InformationSchemaViewsBuilder.builder()
+            .withSql(
+                InformationSchemaKey.SERVER_INFORMATION,
+                String.format(
+                    "SELECT '%s' AS NAME, '%s' AS DESCRIPTION, '%s' AS VALUE "
+                        + "FROM INFORMATION_SCHEMA.SYSTEM_TYPEINFO",
+                    name, description, value))
+            .toOptions();
+    final SchemaRetrievalOptionsBuilder schemaRetrievalOptionsBuilder =
+        SchemaRetrievalOptionsBuilder.builder().withInformationSchemaViews(informationSchemaViews);
+    final SchemaRetrievalOptions schemaRetrievalOptions = schemaRetrievalOptionsBuilder.toOptions();
     final RetrieverConnection retrieverConnection =
-        new RetrieverConnection(dataSource, SchemaRetrievalOptionsBuilder.builder().toOptions());
+        new RetrieverConnection(dataSource, schemaRetrievalOptions);
 
     final SchemaCrawlerOptions options = SchemaCrawlerOptionsBuilder.newSchemaCrawlerOptions();
 
     final DatabaseInfoRetriever databaseInfoRetriever =
         new DatabaseInfoRetriever(retrieverConnection, catalog, options);
+    databaseInfoRetriever.retrieveServerInfo();
 
-    // The method should handle exceptions gracefully and not throw
-    assertDoesNotThrow(() -> databaseInfoRetriever.retrieveAdditionalDatabaseInfo());
+    final List<Property> serverInfo = new ArrayList<>(catalog.getDatabaseInfo().getServerInfo());
+    assertThat(serverInfo, hasSize(1));
+    final Property serverInfoProperty = serverInfo.get(0);
+    assertThat(serverInfoProperty, is(new ImmutableServerInfoProperty(name, value, description)));
+    assertThat(serverInfoProperty.getDescription(), is(description));
   }
 }
