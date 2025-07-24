36e1daf66afdf84abe74b0db0e83cc790898a9bc
Sualeh Fatehi <sualeh@hotmail.com>
2025-07-23 21:03:05 -0400

    Put tablesample file in correct location

--- schemacrawler-db2/src/test/java/schemacrawler/integration/test/DB2Test.java
+++ schemacrawler-db2/src/test/java/schemacrawler/integration/test/DB2Test.java
@@ -27,7 +27,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -56,9 +55,6 @@ import us.fatehi.utility.property.Property;
 @HeavyDatabaseTest("db2")
 @Testcontainers
 @ResolveTestContext
-@EnabledOnOs(
-    architectures = {"x64", "x86_64", "amd64"},
-    disabledReason = "IBM DB2 Docker container does not run on ARM")
 public class DB2Test extends BaseAdditionalDatabaseTest {
 
   @Container private final JdbcDatabaseContainer<?> dbContainer = newDB2Container();
