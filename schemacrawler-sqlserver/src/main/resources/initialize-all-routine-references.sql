064a994703ad681ffdab4e4e06f9d0e5b67a95d7
Sualeh Fatehi <sualeh@hotmail.com>
2025-08-10 10:20:45 -0400

    Add SQL Server support for routine references

---
+++ schemacrawler-sqlserver/src/main/resources/initialize-all-routine-references.sql
@@ -0,0 +1,56 @@
+-- SchemaCrawler
+-- http://www.schemacrawler.com
+-- Copyright (c) 2000-2025, Sualeh Fatehi <sualeh@hotmail.com>.
+-- All rights reserved.
+-- SPDX-License-Identifier: EPL-2.0
+
+IF OBJECT_ID('tempdb..#schcrwlr_CollectProcedureReferences') IS NOT NULL
+    DROP PROCEDURE #schcrwlr_CollectProcedureReferences;
+@
+
+CREATE PROCEDURE #schcrwlr_CollectProcedureReferences
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Drop the global temp table if it exists
+    IF OBJECT_ID('tempdb..##ProcedureReferences') IS NOT NULL
+        DROP TABLE ##ProcedureReferences;
+
+    -- Create the global temp table for collecting procedure references
+    CREATE TABLE ##ProcedureReferences (
+        ROUTINE_CATALOG SYSNAME,
+        ROUTINE_SCHEMA SYSNAME,
+        ROUTINE_NAME SYSNAME,
+		SPECIFIC_NAME SYSNAME NULL,
+        REFERENCED_OBJECT_CATALOG SYSNAME NULL,
+        REFERENCED_OBJECT_SCHEMA SYSNAME NULL,
+        REFERENCED_OBJECT_NAME SYSNAME NULL,
+		REFERENCED_OBJECT_SPECIFIC_NAME SYSNAME NULL,
+        REFERENCED_OBJECT_TYPE NVARCHAR(60) NULL
+    );
+
+    -- Execute against each non-system database
+    EXEC sp_msforeachdb N'
+    IF ''?'' NOT IN (''master'',''model'',''msdb'',''tempdb'')
+    BEGIN
+        INSERT INTO ##ProcedureReferences
+        SELECT 
+            ''?'' AS ROUTINE_CATALOG,
+            OBJECT_ROUTINE_SCHEMA(d.referencing_id, DB_ID(''?'')) AS ROUTINE_SCHEMA,
+            OBJECT_NAME(d.referencing_id, DB_ID(''?'')) AS ROUTINE_NAME,
+            NULL AS SPECIFIC_NAME,
+			''?'' AS REFERENCED_OBJECT_CATALOG,
+			OBJECT_ROUTINE_SCHEMA(o.object_id, DB_ID(DB_NAME())) AS REFERENCED_OBJECT_SCHEMA,
+            o.name AS REFERENCED_OBJECT_NAME,
+            NULL AS REFERENCED_OBJECT_SPECIFIC_NAME,
+            o.type_desc AS REFERENCED_OBJECT_TYPE
+        FROM [?].sys.sql_expression_dependencies AS d
+        JOIN [?].sys.objects AS o ON d.referenced_id = o.object_id
+        WHERE OBJECTPROPERTY(d.referencing_id, ''IsProcedure'') = 1;
+    END';
+
+    -- Return the combined results
+    SELECT * FROM ##ProcedureReferences;
+END;
+@
