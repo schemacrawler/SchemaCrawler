1598069b552d982bbdc12fe753426f587bab2bb1
Sualeh Fatehi <sualeh@hotmail.com>
2025-08-17 02:08:46 -0400

    Add additional HyperSQL server information

--- schemacrawler-hsqldb/src/main/resources/hsqldb.information_schema/SERVER_INFORMATION.sql
+++ schemacrawler-hsqldb/src/main/resources/hsqldb.information_schema/SERVER_INFORMATION.sql
@@ -14,9 +14,21 @@ FROM (
     ('PRODUCT_VERSION',
      DATABASE_VERSION(),
      'HSQLDB engine version'),
-     
+
+    ('DATABASE_NAME', 
+     DATABASE_NAME(), 
+     'Database name'),
+
     ('CATALOG_NAME', 
      (SELECT CATALOG_NAME FROM INFORMATION_SCHEMA.INFORMATION_SCHEMA_CATALOG_NAME), 
-     'Current catalog name')
+     'Current catalog name'),
      
+    ('DATABASE_ISOLATION_LEVEL', 
+     DATABASE_ISOLATION_LEVEL(), 
+     'Default transaction isolation level for the database - READ COMMITTED or SERIALIZABLE'),   
+
+    ('TRANSACTION_CONTROL', 
+     TRANSACTION_CONTROL(), 
+     'Returns the current transaction model for the database - LOCKS, MVLOCKS or MVCC')  
+      
 ) AS server_information(name, value, description)
