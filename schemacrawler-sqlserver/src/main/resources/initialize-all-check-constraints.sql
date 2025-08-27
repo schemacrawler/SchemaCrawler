f5596882e716575a5ffe49d763d84804f643ca1d
Sualeh Fatehi <sualeh@hotmail.com>
2025-08-27 18:04:47 -0400

    Loop over sys.databases

--- schemacrawler-sqlserver/src/main/resources/initialize-all-check-constraints.sql
+++ schemacrawler-sqlserver/src/main/resources/initialize-all-check-constraints.sql
@@ -23,18 +23,43 @@ BEGIN
         CHECK_CLAUSE NVARCHAR(MAX)
     );
 
-    EXEC sp_msforeachdb N'
-    IF ''?'' NOT IN (''master'',''model'',''msdb'',''tempdb'')
+    DECLARE @dbName SYSNAME;
+    DECLARE @sql NVARCHAR(MAX);
+
+    DECLARE db_cursor CURSOR FOR
+        SELECT name
+        FROM sys.databases
+        WHERE name NOT IN ('master', 'model', 'msdb', 'tempdb')
+          AND state_desc = 'ONLINE';
+
+    OPEN db_cursor;
+    FETCH NEXT FROM db_cursor INTO @dbName;
+
+    WHILE @@FETCH_STATUS = 0
     BEGIN
+        SET @sql = N'
         INSERT INTO ##AllCheckConstraintMetadata
         SELECT
-            CONSTRAINT_CATALOG,
+            ''' + @dbName + ''' AS CONSTRAINT_CATALOG,
             CONSTRAINT_SCHEMA,
             CONSTRAINT_NAME,
             CHECK_CLAUSE
         FROM 
-            [?].INFORMATION_SCHEMA.CHECK_CONSTRAINTS;
-    END';
+		    ' + QUOTENAME(@dbName) + '.INFORMATION_SCHEMA.CHECK_CONSTRAINTS;';
+
+        BEGIN TRY
+            EXEC sp_executesql @sql;
+        END TRY
+        BEGIN CATCH
+            DECLARE @error NVARCHAR(MAX) = ERROR_MESSAGE();
+            RAISERROR(@error, 5, 1);
+        END CATCH;
+
+        FETCH NEXT FROM db_cursor INTO @dbName;
+    END;
+
+    CLOSE db_cursor;
+    DEALLOCATE db_cursor;
 
     SELECT * FROM ##AllCheckConstraintMetadata;
 END;
