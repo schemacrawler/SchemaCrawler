1d5a562b34b452faffcbfff4b80768260f223d6f
Sualeh Fatehi <sualeh@hotmail.com>
2025-08-26 20:45:00 -0400

    Add SQL for table columns

--- schemacrawler-sqlserver/src/main/resources/initialize-all-table-columns.sql
+++ schemacrawler-sqlserver/src/main/resources/initialize-all-table-columns.sql
@@ -24,21 +24,31 @@ BEGIN
         TABLE_NAME SYSNAME,
         COLUMN_NAME SYSNAME,
         ORDINAL_POSITION INT,
-        IS_NULLABLE VARCHAR(3),
+        NULLABLE INT,
+        COLUMN_DEF VARCHAR(4000),
         TYPE_NAME SYSNAME,
         COLUMN_SIZE INT,
         DECIMAL_DIGITS INT,
         CHAR_OCTET_LENGTH INT,
         IS_AUTOINCREMENT VARCHAR(3),
         IS_GENERATEDCOLUMN VARCHAR(3),
-        REMARKS SYSNAME,
+        REMARKS SYSNAME NULL,
         DATA_TYPE INT
     );
 
-    -- Execute against each non-system database
-    EXEC sp_msforeachdb N'
-    IF ''?'' NOT IN (''master'',''model'',''msdb'',''tempdb'')
+    DECLARE @dbName SYSNAME;
+    DECLARE db_cursor CURSOR FOR
+        SELECT name
+        FROM sys.databases
+        WHERE name NOT IN ('master', 'model', 'msdb', 'tempdb', 'rdsadmin')
+          AND state_desc = 'ONLINE';
+
+    OPEN db_cursor;
+    FETCH NEXT FROM db_cursor INTO @dbName;
+
+    WHILE @@FETCH_STATUS = 0
     BEGIN
+        DECLARE @sql NVARCHAR(MAX) = N'
         INSERT INTO ##AllTableColumns
         SELECT
             CAST(cu.TABLE_CATALOG AS SYSNAME) AS TABLE_CAT,
@@ -46,7 +56,12 @@ BEGIN
             CAST(cu.TABLE_NAME AS SYSNAME) AS TABLE_NAME,
             CAST(cu.COLUMN_NAME AS SYSNAME) AS COLUMN_NAME,
             CAST(cu.ORDINAL_POSITION AS INTEGER) AS ORDINAL_POSITION,
-            CAST(cu.IS_NULLABLE AS VARCHAR(3)) AS IS_NULLABLE,
+            CASE cu.IS_NULLABLE
+              WHEN ''YES'' THEN 1
+              ELSE 0
+            END
+              AS NULLABLE,
+            cu.COLUMN_DEFAULT AS COLUMN_DEF,
             CAST(cu.DATA_TYPE AS SYSNAME) AS TYPE_NAME,
             CAST(COALESCE(cu.CHARACTER_MAXIMUM_LENGTH, cu.NUMERIC_PRECISION) AS INTEGER) AS COLUMN_SIZE,
             CAST(cu.NUMERIC_SCALE AS INTEGER) AS DECIMAL_DIGITS,
@@ -54,42 +69,49 @@ BEGIN
             CAST(CASE WHEN sc.is_identity = 1 THEN ''YES'' ELSE ''NO'' END AS VARCHAR(3)) AS IS_AUTOINCREMENT,
             CAST(CASE WHEN sc.is_computed = 1 THEN ''YES'' ELSE ''NO'' END AS VARCHAR(3)) AS IS_GENERATEDCOLUMN,
             CAST(NULL AS SYSNAME) AS REMARKS,
-            CAST(
-                CASE cu.DATA_TYPE
-                    WHEN ''int'' THEN 4
-                    WHEN ''bigint'' THEN -5
-                    WHEN ''smallint'' THEN 5
-                    WHEN ''tinyint'' THEN -6
-                    WHEN ''bit'' THEN -7
-                    WHEN ''decimal'' THEN 3
-                    WHEN ''numeric'' THEN 2
-                    WHEN ''float'' THEN 6
-                    WHEN ''real'' THEN 7
-                    WHEN ''char'' THEN 1
-                    WHEN ''varchar'' THEN 12
-                    WHEN ''nchar'' THEN -15
-                    WHEN ''nvarchar'' THEN -9
-                    WHEN ''text'' THEN -1
-                    WHEN ''ntext'' THEN -16
-                    WHEN ''date'' THEN 91
-                    WHEN ''datetime'' THEN 93
-                    WHEN ''datetime2'' THEN 93
-                    WHEN ''smalldatetime'' THEN 93
-                    WHEN ''time'' THEN 92
-                    WHEN ''binary'' THEN -2
-                    WHEN ''varbinary'' THEN -3
-                    WHEN ''image'' THEN -4
-                    ELSE 1111
-                END AS INTEGER
-            ) AS DATA_TYPE
-        FROM [?].INFORMATION_SCHEMA.COLUMNS cu
-        INNER JOIN [?].sys.schemas ss
+            CASE cu.DATA_TYPE
+                WHEN ''int'' THEN 4
+                WHEN ''bigint'' THEN -5
+                WHEN ''smallint'' THEN 5
+                WHEN ''tinyint'' THEN -6
+                WHEN ''bit'' THEN -7
+                WHEN ''decimal'' THEN 3
+                WHEN ''numeric'' THEN 2
+                WHEN ''float'' THEN 6
+                WHEN ''real'' THEN 7
+                WHEN ''char'' THEN 1
+                WHEN ''varchar'' THEN 12
+                WHEN ''nchar'' THEN -15
+                WHEN ''nvarchar'' THEN -9
+                WHEN ''text'' THEN -1
+                WHEN ''ntext'' THEN -16
+                WHEN ''date'' THEN 91
+                WHEN ''datetime'' THEN 93
+                WHEN ''datetime2'' THEN 93
+                WHEN ''smalldatetime'' THEN 93
+                WHEN ''time'' THEN 92
+                WHEN ''binary'' THEN -2
+                WHEN ''varbinary'' THEN -3
+                WHEN ''image'' THEN -4
+                ELSE 1111
+            END AS DATA_TYPE
+        FROM ' + QUOTENAME(@dbName) + '.INFORMATION_SCHEMA.COLUMNS cu
+        INNER JOIN ' + QUOTENAME(@dbName) + '.sys.schemas ss
             ON ss.name = cu.TABLE_SCHEMA
-        INNER JOIN [?].sys.tables st
-            ON st.name = cu.TABLE_NAME AND st.schema_id = ss.schema_id
-        INNER JOIN [?].sys.columns sc
-            ON sc.object_id = st.object_id AND sc.name = cu.COLUMN_NAME;
-    END';
+        INNER JOIN ' + QUOTENAME(@dbName) + '.sys.objects so
+            ON so.name = cu.TABLE_NAME AND so.schema_id = ss.schema_id
+            AND so.type IN (''U'', ''V'') -- U = table, V = view
+        INNER JOIN ' + QUOTENAME(@dbName) + '.sys.columns sc
+            ON sc.object_id = so.object_id AND sc.name = cu.COLUMN_NAME
+        ';
+
+        EXEC sp_executesql @sql;
+
+        FETCH NEXT FROM db_cursor INTO @dbName;
+    END;
+
+    CLOSE db_cursor;
+    DEALLOCATE db_cursor;
 
     -- Return the combined results
     SELECT * FROM ##AllTableColumns;
