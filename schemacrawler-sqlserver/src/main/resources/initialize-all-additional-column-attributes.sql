09c5048e2850d57b281119878f1e8e12d6f40c3b
Sualeh Fatehi <sualeh@hotmail.com>
2025-08-27 17:56:11 -0400

    Loop over sys.databases

--- schemacrawler-sqlserver/src/main/resources/initialize-all-additional-column-attributes.sql
+++ schemacrawler-sqlserver/src/main/resources/initialize-all-additional-column-attributes.sql
@@ -24,28 +24,54 @@ BEGIN
         REMARKS NVARCHAR(MAX)
     );
 
-    EXEC sp_msforeachdb N'
-    IF ''?'' NOT IN (''master'', ''model'', ''msdb'', ''tempdb'')
+    DECLARE @dbName SYSNAME;
+    DECLARE @sql NVARCHAR(MAX);
+
+    DECLARE db_cursor CURSOR FOR
+        SELECT name
+        FROM sys.databases
+        WHERE name NOT IN ('master', 'model', 'msdb', 'tempdb')
+          AND state_desc = 'ONLINE';
+
+    OPEN db_cursor;
+    FETCH NEXT FROM db_cursor INTO @dbName;
+
+    WHILE @@FETCH_STATUS = 0
     BEGIN
+        SET @sql = N'
         INSERT INTO ##AllAdditionalColumnAttributes
         SELECT
-            DB_NAME() AS TABLE_CATALOG,
+            ''' +@dbName + ''' AS TABLE_CATALOG,
             SCHEMA_NAME(O.SCHEMA_ID) AS TABLE_SCHEMA,
             O.NAME AS TABLE_NAME,
             C.NAME AS COLUMN_NAME,
             CONVERT(NVARCHAR(MAX), EP.VALUE) AS REMARKS
-        FROM 
-            [?].SYS.COLUMNS AS C
-            INNER JOIN [?].SYS.ALL_OBJECTS O
+        FROM
+             ' + QUOTENAME(@dbName) + '.SYS.COLUMNS AS C
+            INNER JOIN  ' + QUOTENAME(@dbName) + '.SYS.ALL_OBJECTS O
                 ON C.OBJECT_ID = O.OBJECT_ID
-            INNER JOIN [?].SYS.EXTENDED_PROPERTIES EP
+            INNER JOIN  ' + QUOTENAME(@dbName) + '.SYS.EXTENDED_PROPERTIES EP
                 ON EP.MAJOR_ID = C.OBJECT_ID AND EP.MINOR_ID = C.COLUMN_ID
         WHERE
             O.IS_MS_SHIPPED != 1
             AND O.TYPE = ''U''
             AND EP.CLASS = 1
             AND EP.NAME = ''MS_Description'';
-    END';
+        ';
+
+        BEGIN TRY
+            EXEC sp_executesql @sql;
+        END TRY
+        BEGIN CATCH
+            DECLARE @error NVARCHAR(MAX) = ERROR_MESSAGE();
+            RAISERROR(@error, 5, 1);
+        END CATCH;
+
+        FETCH NEXT FROM db_cursor INTO @dbName;
+    END;
+
+    CLOSE db_cursor;
+    DEALLOCATE db_cursor;
 
     SELECT * FROM ##AllAdditionalColumnAttributes;
 END;
