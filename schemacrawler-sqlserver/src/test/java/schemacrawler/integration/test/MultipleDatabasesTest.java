64d01161b3a78506386770a8f1ead1d6e8473b29
Sualeh Fatehi <sualeh@hotmail.com>
2025-08-29 19:42:59 -0400

    Commit transactions

--- schemacrawler-sqlserver/src/test/java/schemacrawler/integration/test/MultipleDatabasesTest.java
+++ schemacrawler-sqlserver/src/test/java/schemacrawler/integration/test/MultipleDatabasesTest.java
@@ -70,6 +70,7 @@ public class MultipleDatabasesTest extends BaseAdditionalDatabaseTest {
 
     // Note: The database connection needs to be closed for the new schemas to be recognized
     try (final Connection connection = getConnection()) {
+      connection.setAutoCommit(true);
       SqlScript.executeScriptFromResource("/multiple-databases.sql", connection);
     }
   }
