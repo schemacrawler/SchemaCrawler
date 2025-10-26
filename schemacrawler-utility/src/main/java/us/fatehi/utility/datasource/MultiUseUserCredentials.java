d3bfc6d8b6e044e06eb3e700c87d7b6090cffc7a
Sualeh Fatehi <sualeh@hotmail.com>
2025-10-26 13:26:00 -0400

    Convert credentials to record

--- schemacrawler-utility/src/main/java/us/fatehi/utility/datasource/MultiUseUserCredentials.java
+++ schemacrawler-utility/src/main/java/us/fatehi/utility/datasource/MultiUseUserCredentials.java
@@ -8,33 +8,15 @@
 
 package us.fatehi.utility.datasource;
 
-public class MultiUseUserCredentials implements UserCredentials {
-
-  private final String password;
-  private final String user;
+public record MultiUseUserCredentials(String user, String password) implements UserCredentials {
 
   public MultiUseUserCredentials() {
     this(null, null);
   }
 
-  public MultiUseUserCredentials(final String user, final String password) {
-    this.password = password;
-    this.user = user;
-  }
-
   @Override
   public void clearPassword() {
     // No action
-  }
-
-  @Override
-  public String getPassword() {
-    return password;
-  }
-
-  @Override
-  public String getUser() {
-    return user;
   }
 
   @Override
