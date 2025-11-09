e414de40222b66927f544ec27c0311463b052e00
Sualeh Fatehi <sualeh@hotmail.com>
2025-11-09 09:20:40 -0500

    Mark utility classes

--- schemacrawler-commandline/src/main/java/schemacrawler/tools/commandline/utility/SchemaRetrievalOptionsConfig.java
+++ schemacrawler-commandline/src/main/java/schemacrawler/tools/commandline/utility/SchemaRetrievalOptionsConfig.java
@@ -15,15 +15,45 @@ import schemacrawler.schemacrawler.SchemaInfoMetadataRetrievalStrategy;
 import schemacrawler.schemacrawler.SchemaRetrievalOptionsBuilder;
 import schemacrawler.tools.options.Config;
 import schemacrawler.tools.options.ConfigUtility;
+import us.fatehi.utility.UtilityMarker;
 
+@UtilityMarker
 public final class SchemaRetrievalOptionsConfig {
+
+  public static SchemaRetrievalOptionsBuilder fromConfig(
+      final SchemaRetrievalOptionsBuilder providedBuilder, final Config config) {
+    final SchemaRetrievalOptionsBuilder builder;
+    if (providedBuilder == null) {
+      builder = SchemaRetrievalOptionsBuilder.builder();
+    } else {
+      builder = providedBuilder;
+    }
+
+    final Config configProperties = ConfigUtility.fromConfig(config);
+    final InformationSchemaViewsBuilder informationSchemaViewsBuilder =
+        InformationSchemaViewsBuilder.builder(builder.getInformationSchemaViews());
+    SchemaRetrievalOptionsConfig.fromConfig(informationSchemaViewsBuilder, configProperties);
+    builder.withInformationSchemaViews(informationSchemaViewsBuilder.toOptions());
+
+    for (final SchemaInfoMetadataRetrievalStrategy metadataRetrievalStrategy :
+        SchemaInfoMetadataRetrievalStrategy.values()) {
+      final MetadataRetrievalStrategy currentValue = builder.get(metadataRetrievalStrategy);
+      final String configKey =
+          "schemacrawler.schema.retrieval.strategy." + metadataRetrievalStrategy.getKey();
+      final MetadataRetrievalStrategy configValue =
+          configProperties.getEnumValue(configKey, currentValue);
+      builder.with(metadataRetrievalStrategy, configValue);
+    }
+
+    return builder;
+  }
 
   /**
    * Information schema views from a map.
    *
    * @param informationSchemaViewsSql Map of information schema view definitions.
    */
-  public static InformationSchemaViewsBuilder fromConfig(
+  private static InformationSchemaViewsBuilder fromConfig(
       final InformationSchemaViewsBuilder providedBuilder, final Config informationSchemaViewsSql) {
     final InformationSchemaViewsBuilder builder;
     if (providedBuilder == null) {
@@ -53,31 +83,7 @@ public final class SchemaRetrievalOptionsConfig {
     return builder;
   }
 
-  public static SchemaRetrievalOptionsBuilder fromConfig(
-      final SchemaRetrievalOptionsBuilder providedBuilder, final Config config) {
-    final SchemaRetrievalOptionsBuilder builder;
-    if (providedBuilder == null) {
-      builder = SchemaRetrievalOptionsBuilder.builder();
-    } else {
-      builder = providedBuilder;
-    }
-
-    final Config configProperties = ConfigUtility.fromConfig(config);
-    final InformationSchemaViewsBuilder informationSchemaViewsBuilder =
-        InformationSchemaViewsBuilder.builder(builder.getInformationSchemaViews());
-    SchemaRetrievalOptionsConfig.fromConfig(informationSchemaViewsBuilder, configProperties);
-    builder.withInformationSchemaViews(informationSchemaViewsBuilder.toOptions());
-
-    for (final SchemaInfoMetadataRetrievalStrategy metadataRetrievalStrategy :
-        SchemaInfoMetadataRetrievalStrategy.values()) {
-      final MetadataRetrievalStrategy currentValue = builder.get(metadataRetrievalStrategy);
-      final String configKey =
-          "schemacrawler.schema.retrieval.strategy." + metadataRetrievalStrategy.getKey();
-      final MetadataRetrievalStrategy configValue =
-          configProperties.getEnumValue(configKey, currentValue);
-      builder.with(metadataRetrievalStrategy, configValue);
-    }
-
-    return builder;
+  private SchemaRetrievalOptionsConfig() {
+    // Prevent instantiation
   }
 }
