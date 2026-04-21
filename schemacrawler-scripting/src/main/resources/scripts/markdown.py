34869fe7f7969c59ed2a8df19ea29e7dedca8567
Sualeh Fatehi <sualeh@hotmail.com>
2026-04-20 21:06:53 -0400

    Adjust fk display

--- schemacrawler-scripting/src/main/resources/scripts/markdown.py
+++ schemacrawler-scripting/src/main/resources/scripts/markdown.py
@@ -58,7 +58,7 @@ for schema in catalog.getSchemas():
         fk_name = f"{fk.getName()} " if support.hasName(fk) else ''
         pk_cols = support.pkColumns(fk)
         fk_cols = support.fkColumns(fk)
-        print(f'- {fk_name}([{fk_cols}] --> [{pk_cols}])')
+        print(f'- {fk_name}(({fk_cols}) --> ({pk_cols})')
 
     print()
     print()
