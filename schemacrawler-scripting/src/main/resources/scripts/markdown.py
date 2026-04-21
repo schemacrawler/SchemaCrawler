c16734fe5c0d142cc27236bf854df4b8c37c3037
Sualeh Fatehi <sualeh@hotmail.com>
2026-04-20 20:56:47 -0400

    Display multi-column foreign keys better

--- schemacrawler-scripting/src/main/resources/scripts/markdown.py
+++ schemacrawler-scripting/src/main/resources/scripts/markdown.py
@@ -3,64 +3,63 @@
 
 print(f'# {title}')
 
-print('')
+print()
 for schema in catalog.getSchemas():
 
-    tables = catalog.getTables(schema)
-    if not tables:
-        continue
+  tables = catalog.getTables(schema)
+  if not tables:
+    continue
 
-    print(f'## {schema.getFullName()}')
+  print(f'## {schema.getFullName()}')
 
-    print('')
-    for table in tables:
-        print(f'### {table.getName()} ({support.type(table)})')
-        if table.hasRemarks():
-            print(table.getRemarks())
+  print()
+  for table in tables:
+    print(f'### {table.getName()} ({support.type(table)})')
+    if table.hasRemarks():
+      print(table.getRemarks())
 
-        print('')
-        print('### Columns')
-        for column in table.getColumns():
-            if column.isPartOfPrimaryKey():
-                col_name = f'**{column.getName()}**'
-            elif column.isPartOfForeignKey():
-                col_name = f'*{column.getName()}*'
-            else:
-                col_name = column.getName()
-            print(f'- {col_name} ({support.columnType(column)})', end='')
-            column_remarks = support.indent(column.getRemarks(), 4)
-            if column_remarks:
-                print('    ')
-                print(f'{column_remarks}', end='')
-            else:
-                print()
+    print()
+    print('### Columns')
+    for column in table.getColumns():
+      if column.isPartOfPrimaryKey():
+        col_name = f'**{column.getName()}**'
+      elif column.isPartOfForeignKey():
+        col_name = f'*{column.getName()}*'
+      else:
+        col_name = column.getName()
+      print(f'- {col_name} ({support.columnType(column)})', end='')
+      column_remarks = support.indent(column.getRemarks(), 4)
+      if column_remarks:
+        print('    ')
+        print(f'{column_remarks}', end='')
+      else:
+        print()
 
-        if table.hasPrimaryKey():
-            print('')
-            print('### Primary Key')
-            pk = table.getPrimaryKey()
-            pk_name = f"{pk.getName()} " if support.hasName(pk) else ''
-            print(f'- {pk_name}({support.columns(pk)})')
+    if table.hasPrimaryKey():
+      print()
+      print('### Primary Key')
+      pk = table.getPrimaryKey()
+      pk_name = f"{pk.getName()} " if support.hasName(pk) else ''
+      print(f'- {pk_name}({support.columns(pk)})')
 
-        indexes = support.nonPrimaryIndexes(table)
-        if not indexes.isEmpty():
-            print('')
-            print('### Indexes')
-            for index in indexes:
-                unique = ' (unique index)' if index.isUnique() else ''
-                print(f'- {index.getName()} ({support.columns(index)}){unique}')
+    indexes = support.nonPrimaryIndexes(table)
+    if not indexes.isEmpty():
+      print()
+      print('### Indexes')
+      for index in indexes:
+        unique = ' (unique index)' if index.isUnique() else ''
+        print(f'- {index.getName()} ({support.columns(index)}){unique}')
 
-        foreign_keys = table.getImportedForeignKeys()
-        if not foreign_keys.isEmpty():
-            print('')
-            print('### Foreign Keys')
-            for fk in foreign_keys:
-                for columnReference in support.columnReferences(fk):
-                    fk_name = f"{fk.getName()} " if support.hasName(fk) else ''
-                    pkColumn = columnReference.getPrimaryKeyColumn()
-                    fkColumn = columnReference.getForeignKeyColumn()
-                    print(f'- {fk_name}(*{fkColumn.getName()}* --> **{pkColumn.getShortName()}**)')
+    foreign_keys = table.getImportedForeignKeys()
+    if not foreign_keys.isEmpty():
+      print()
+      print('### Foreign Keys')
+      for fk in foreign_keys:
+        fk_name = f"{fk.getName()} " if support.hasName(fk) else ''
+        pk_cols = support.pkColumns(fk)
+        fk_cols = support.fkColumns(fk)
+        print(f'- {fk_name}([{fk_cols}] --> [{pk_cols}])')
 
-        print('')
-        print('')
-        print('')
+    print()
+    print()
+    print()
