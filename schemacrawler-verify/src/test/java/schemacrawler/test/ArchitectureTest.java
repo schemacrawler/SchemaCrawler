7732a172a4fac17d772f788c82dde6727b3ea23d
Sualeh Fatehi <sualeh@hotmail.com>
2026-05-30 15:28:51 -0400

    Update architeture tests

--- schemacrawler-verify/src/test/java/schemacrawler/test/ArchitectureTest.java
+++ schemacrawler-verify/src/test/java/schemacrawler/test/ArchitectureTest.java
@@ -8,7 +8,26 @@
 
 package schemacrawler.test;
 
+import static com.tngtech.archunit.base.DescribedPredicate.not;
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.simpleName;
+import static com.tngtech.archunit.lang.conditions.ArchPredicates.are;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import org.junit.jupiter.api.Test;
+
 public class ArchitectureTest extends BaseArchitectureTest {
+
+  @Override
+  @Test
+  public void notUseJackson() {
+    noClasses()
+        .that(are(not(simpleName("CatalogAttributesUtility"))))
+        .should()
+        .dependOnClassesThat()
+        .resideInAPackage("tools.jackson..")
+        .because("SchemaCrawler-Core must not depend on Jackson")
+        .check(classes);
+  }
 
   @Override
   protected String classesSpecification() {
