a3a7dc99cb1c296d34dc114e1c81dadc6e0a00f1
Sualeh Fatehi <sualeh@hotmail.com>
2026-07-24 22:34:26 -0400

    Add Snowflake test (#2422)
    * Add Snowflake test
    
    * Disable Snowflake test
    
    * Do not install Graphviz for database tests
    
    * Use correct schema name
    
    * Compare Snowflake results to expected results
    
    * Add test results from testdb
    
    * Add Snowflake test
    
    * Run Snowflake test only if auth token is provided

---
+++ schemacrawler-dbtest/src/test/java/schemacrawler/integration/test/SnowflakeTest.java
@@ -0,0 +1,101 @@
+/*
+ * SchemaCrawler
+ * http://www.schemacrawler.com
+ * Copyright (c) 2000-2026, Sualeh Fatehi <sualeh@hotmail.com>.
+ * All rights reserved.
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+package schemacrawler.integration.test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+import static schemacrawler.test.utility.ExecutableTestUtility.executableExecution;
+import static us.fatehi.test.utility.extensions.FileHasContent.classpathResource;
+import static us.fatehi.test.utility.extensions.FileHasContent.hasSameContentAs;
+import static us.fatehi.test.utility.extensions.FileHasContent.outputOf;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import schemacrawler.inclusionrule.RegularExpressionInclusionRule;
+import schemacrawler.schemacrawler.LimitOptionsBuilder;
+import schemacrawler.schemacrawler.LoadOptionsBuilder;
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+import schemacrawler.schemacrawler.SchemaCrawlerOptionsBuilder;
+import schemacrawler.schemacrawler.SchemaInfoLevelBuilder;
+import schemacrawler.test.utility.BaseAdditionalDatabaseTest;
+import schemacrawler.test.utility.DisableLogging;
+import schemacrawler.tools.command.text.schema.options.SchemaTextOptionsBuilder;
+import schemacrawler.tools.executable.SchemaCrawlerExecutable;
+import us.fatehi.test.integration.utility.SnowflakeTestUtility;
+import us.fatehi.test.utility.extensions.HeavyDatabaseTest;
+
+@DisableLogging
+@HeavyDatabaseTest("snowflake")
+@EnabledIfEnvironmentVariable(named = "LOCALSTACK_AUTH_TOKEN", matches = ".+")
+@Testcontainers(disabledWithoutDocker = true)
+public class SnowflakeTest extends BaseAdditionalDatabaseTest {
+
+  @Container
+  private final JdbcDatabaseContainer<?> dbContainer = SnowflakeTestUtility.newSnowflakeContainer();
+
+  @BeforeEach
+  public void createDatabase() {
+
+    if (!dbContainer.isRunning()) {
+      fail("Testcontainer for database is not available");
+    }
+
+    System.err.println(
+        "%s %s/%s"
+            .formatted(
+                dbContainer.getJdbcUrl(), dbContainer.getUsername(), dbContainer.getPassword()));
+
+    final Map<String, String> urlx = new HashMap<>();
+    urlx.put("db", dbContainer.getDatabaseName());
+    urlx.put("schema", "BOOKS");
+    urlx.put("tracing", "ALL");
+
+    createDataSource(
+        dbContainer.getJdbcUrl(), dbContainer.getUsername(), dbContainer.getPassword());
+
+    createDatabase("/snowflake.scripts.txt");
+
+    // Overwrite the datasource with default database
+    createDataSource(
+        dbContainer.getJdbcUrl(), dbContainer.getUsername(), dbContainer.getPassword(), urlx);
+  }
+
+  @Test
+  public void testSnowflakeWithConnection() throws Exception {
+    final LimitOptionsBuilder limitOptionsBuilder =
+        LimitOptionsBuilder.builder()
+            .includeSchemas(new RegularExpressionInclusionRule("BOOKS\\.BOOKS"))
+            .includeAllTables()
+            .includeAllRoutines()
+            .includeAllSequences()
+            .includeAllSynonyms()
+            .tableTypes("TABLE", "VIEW");
+    final LoadOptionsBuilder loadOptionsBuilder =
+        LoadOptionsBuilder.builder().withSchemaInfoLevel(SchemaInfoLevelBuilder.maximum());
+    final SchemaCrawlerOptions schemaCrawlerOptions =
+        SchemaCrawlerOptionsBuilder.newSchemaCrawlerOptions()
+            .withLimitOptions(limitOptionsBuilder.toOptions())
+            .withLoadOptions(loadOptionsBuilder.toOptions());
+
+    final SchemaCrawlerExecutable executable = new SchemaCrawlerExecutable("details");
+    executable.setSchemaCrawlerOptions(schemaCrawlerOptions);
+    executable.setAdditionalConfiguration(SchemaTextOptionsBuilder.builder().toConfig());
+
+    final String expectedResource = "testSnowflakeWithConnection.txt";
+    assertThat(
+        outputOf(executableExecution(getConnectionSource(), executable)),
+        hasSameContentAs(classpathResource(expectedResource)));
+  }
+}
