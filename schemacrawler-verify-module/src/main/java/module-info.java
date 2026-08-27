e370deff7832753c716b6d0f4356229b802484f4
Sualeh Fatehi <sualeh@hotmail.com>
2026-08-26 21:51:51 -0400

    Add SchemaCrawler Core verification module

---
+++ schemacrawler-verify-module/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+/*
+ * SchemaCrawler
+ * http://www.schemacrawler.com
+ * Copyright (c) 2000-2026, Sualeh Fatehi <sualeh@hotmail.com>.
+ * All rights reserved.
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+module us.fatehi.schemacrawler.verify {
+  requires java.sql;
+  requires us.fatehi.schemacrawler.schemacrawler;
+}
